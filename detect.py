7a5344187d5ac7d24fd64f73ebc69920081e7733
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-06-02 23:57:58 -0700

    updates

--- detect.py
+++ detect.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:14aef6153885f3c7de47278f5e524a93bbf6f7724b98e1b13d68e28e01ba95d7
-size 7211
+oid sha256:7bdad04aedcabef9423d6605b7537a0ca931014f9582cf84a5ccf493287acf14
+size 7218
