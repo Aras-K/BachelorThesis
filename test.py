294dcc6960d2793792d2fbf1511f6eeff25f58e7
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-06-03 13:55:33 -0700

    dataset padding mode

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3bca074eae73b9fb6dbde422e6f1c741752e696f3dcf4785cce227c709d9aef5
-size 12069
+oid sha256:ecc8a56b99f507cd7719357d91899abb0f454e193550afbba9231050ad873f47
+size 12397
--- utils/datasets.py
+++ utils/datasets.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d851326b8db624b3242c1e0f008c6197e30644fc0784b1c7f6c8531bd6deaef
-size 34996
+oid sha256:e57965fa00c28ed934b4f1e4e5d3bbab3ef6dc5ccb157819fe528f822336df85
+size 35005
