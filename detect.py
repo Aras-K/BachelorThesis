6813624037996b156d53b71472398ca409fb0e3c
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-06-04 13:30:14 -0700

    model.names for dataParallel

--- detect.py
+++ detect.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:da41f795eb9a115d1fb4f4b20bbabb8de639b6a05aa0a0af269c6213f81dfcc4
-size 7210
+oid sha256:0c788e8567ec3d396f3e435e8cdf3a02b695730c19d6dae800c8c730046d302c
+size 7262
--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ecc8a56b99f507cd7719357d91899abb0f454e193550afbba9231050ad873f47
-size 12397
+oid sha256:ea15ee38308a0154ab4e9fcff9f0687a2abeb3446e0c6deb1b9b6a7ac902d29d
+size 12492
