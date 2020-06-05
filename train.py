59ebb99fb478bd7942f606f5df7cf34b7ecbe3b5
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-06-04 17:21:22 -0700

    assert equal model and dataset classes

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7f49acacd845205a46a0a44cae78754089d56d7d4a2f677a2fb831958e335cf2
-size 21257
+oid sha256:17acebbadee824e488b1402576ed2da950c27c6695539eccc2cbfed98860b2fe
+size 21372
