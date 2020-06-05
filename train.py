dd1ff818228763a6f12c0db9edca1031450c78bd
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-06-05 13:07:09 -0700

    speed-reproducibility fix #17

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:de2b2da65a8d0f0f62563fc32f4069b37e7b3116bba5624a7dd1c272ca5f4181
-size 21519
+oid sha256:d1fb99d29934c825bcf2bdc2ffdb01fed4bf6aa1888e23656bf66eb790846a03
+size 21520
--- utils/torch_utils.py
+++ utils/torch_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5f9bd3e959d1a8c9dafe7f4878fda8bedb8d3676a7f8ebb7bd0d68e59ff7c4f9
-size 8337
+oid sha256:c8a747b07ea45d9ccdac1409a6daef98bcd0ca2ab266dd6ee81ebdf4a0bcf87c
+size 8453
