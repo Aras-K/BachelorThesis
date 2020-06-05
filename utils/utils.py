7861ecdffc6853404c58eca199bb0b09a4a3b907
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-06-05 13:29:26 -0700

    --classes bug fix #17

--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c4eca68c29e0b50a7a4fdb97c6083417f60e1e7996b79e94dbab22243505f84e
-size 45732
+oid sha256:d91d72f55ffa061135ae301552e7cec99b8d746bf07b0d336a4ca9a93f2dbfaf
+size 45729
