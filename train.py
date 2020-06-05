5b4432f1f5daedfb0de76466e5efd485b936d692
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-06-05 12:57:16 -0700

    multi-scale fix #16

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:17acebbadee824e488b1402576ed2da950c27c6695539eccc2cbfed98860b2fe
-size 21372
+oid sha256:de2b2da65a8d0f0f62563fc32f4069b37e7b3116bba5624a7dd1c272ca5f4181
+size 21519
