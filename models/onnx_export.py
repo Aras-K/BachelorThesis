32d7b7e2b7a4f08d73c4f775b27331396d4f0f27
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-06-05 20:03:21 -0700

    update

--- models/onnx_export.py
+++ models/onnx_export.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9b3928c7139234ccd6ce72488571fde125b581a9add4ae8038458c6b86ebaeeb
-size 1490
+oid sha256:f47b6640d361348e9065ca83ffe4b2758fad641c74cf5e7efd04162113fab128
+size 1500
