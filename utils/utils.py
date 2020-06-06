54f99869e73419053820e1d751fda2b7be331f1d
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-06-05 18:44:39 -0700

    NMS updates

--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d91d72f55ffa061135ae301552e7cec99b8d746bf07b0d336a4ca9a93f2dbfaf
-size 45729
+oid sha256:be2bf896d39b5a172e5cbcec0e52d75d1b5ffa3b66f6172ecc78cf740ffca51d
+size 45685
