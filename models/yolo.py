6753cb647915c4b134357c9c3946d81c9f1a56a9
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-06-05 21:14:15 -0700

    augmented inference

--- models/yolo.py
+++ models/yolo.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:35f0d1b71c707e512697a8fd6632b769eab656209ba1fca6764f67bdddfee401
-size 9605
+oid sha256:da36f77ee11cb33611d48ca64616afc7797a5d1967e475592c5dba597d220739
+size 9569
--- utils/torch_utils.py
+++ utils/torch_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc3406d3342dcc98dad6d8c97e4c2c9411d5938ef3779f1d4cd14298c6efd7bc
-size 8453
+oid sha256:6efaf9e2c498f159442b89f0d1ae6bd6228f28194c1359d5246044a85679cdc5
+size 8454
