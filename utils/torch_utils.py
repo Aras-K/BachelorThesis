a090de0613c3caed434d45b4af97ecc8bb060bdb
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-06-05 20:50:43 -0700

    updates

--- utils/torch_utils.py
+++ utils/torch_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c8a747b07ea45d9ccdac1409a6daef98bcd0ca2ab266dd6ee81ebdf4a0bcf87c
+oid sha256:cc3406d3342dcc98dad6d8c97e4c2c9411d5938ef3779f1d4cd14298c6efd7bc
 size 8453
