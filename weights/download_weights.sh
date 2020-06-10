1db0a168e3f5d6b471824b73c85b1aeff0327661
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-06-10 00:44:34 -0700

    bug fix #30

--- weights/download_weights.sh
+++ weights/download_weights.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d41a1a6e7ed84b7a9edcd1617f46d811768a2cb4ee50702e60485a0c32481dd0
-size 192
+oid sha256:3fc88ec1d9fe91a20fbf34aaf7b954eb9c35e4589418c92c476faa64532b93fc
+size 204
