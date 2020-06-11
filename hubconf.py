c07262fc73589d2890cb2f119d897c1af33af952
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-06-10 19:13:04 -0700

    PyTorch Hub update

--- hubconf.py
+++ hubconf.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a1f37ea1457f6eda9ea0719150f99c60b8cd79fb1ff52c2f302078d732dbe5a
-size 2919
+oid sha256:97e51cd4fda2090f81de5db5bc2f5577bea7dcc7aa10b71e5e33be685a93afc8
+size 2917
