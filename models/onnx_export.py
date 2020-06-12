dc1f2242f2684b65a43b24b8a658c3d72740cb9f
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-06-12 13:54:00 -0700

    ONNX export explicit cpu map_location

--- models/onnx_export.py
+++ models/onnx_export.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a08f5259ae6bf7bcefb89aed6cbed949c3ff07fdb4414cb417cc312466144d8
-size 1598
+oid sha256:f6513bf039c6faa62b5ed9b189d5aeda218b11a2c9a9eced1f8105c0f5f90aa1
+size 1632
