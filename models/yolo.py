91c87dabc1f968cd599a20e5d231516c06839901
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-06-11 21:55:22 -0700

    cleanup

--- models/yolo.py
+++ models/yolo.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:21f657a2972d7e5676fad055817e3b68ec26399900978b2606e2165d5f34882b
-size 11615
+oid sha256:58e213ab57156fef4eebb6976fe3a5fc9e32a5a0d49e375b4e5f7d2d1bce549e
+size 10321
