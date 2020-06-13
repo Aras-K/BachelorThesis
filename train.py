a24f18d6c29eade8a088f0b3d76efdeccb9836b4
Lauritzen Kasper Primdal <klaur@dfds.com>
2020-06-13 14:44:23 +0200

    Ensures weights/ dir exists
    Allows train.py to be run outside of yolov5/ directory.

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:172ee9a3ce2a8244ac10314a73fba9c4c1166f5622f620911d6813f4f08f89a3
-size 21366
+oid sha256:39d0ba33b932bcf49efa67091f4918976347c7a39743ccae18dc283c8e9493be
+size 21399
