744701a56749b25659ae2ce843c2b7051fbf7ebb
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-06-14 19:25:39 -0700

    switch default inference to FP16 on GPU

--- detect.py
+++ detect.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:05e7a344f8b9a774040ae8a44273ad8588a8c0371745d97508f786fb53bd9ae7
-size 7136
+oid sha256:183984a1fd6d373b721b29a296bc59e47140c3a78469eefe075b6970f13ce05f
+size 7026
--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:41d496f8bae3a8a459b94ceaa15f0603af9db241d3ce6a35de786abde04b246f
-size 12897
+oid sha256:08265fca8e257c1f59267874aae5c9f21b0729bfed15f21d3c131996f13f6c07
+size 12743
