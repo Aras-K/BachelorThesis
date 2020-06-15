27687c93a44cbb285e422f79a4ab83009ed73f73
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-06-15 00:43:57 -0700

    half() fix #66

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:08265fca8e257c1f59267874aae5c9f21b0729bfed15f21d3c131996f13f6c07
-size 12743
+oid sha256:b414fa920afb8935534c3f5408dd253b4c748dcb3efbac0f1a0192ce06879727
+size 12764
