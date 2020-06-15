07bc9ba04686800ebfbb1f075aa7d83ff04ee8fd
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-06-15 15:26:29 -0700

    FP16 test loss bug fix

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7c376fa74aca92671f21c9d62e59e403c70bd077f9d0a946b71dc2a91fa813e0
-size 12870
+oid sha256:d86ac5a332f2f65e03e08d4709e45fb0ed550895985382c9375560aa257e80dc
+size 12891
