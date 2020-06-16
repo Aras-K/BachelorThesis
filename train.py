e81e91af561d6c9ca86ef343569dbdb67a1f990d
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-06-16 16:05:28 -0700

    tb_writer bug fix

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:471b9f8c763ddcb1d593b16bb45f5bb52942c5e6cdc31ae1a31703de6d445314
-size 21492
+oid sha256:4bd742ad22efef28b1f8fba232cf98793a458876823c8678966281f1ba44fb68
+size 21518
