bb57e97c646a4cb5b6cbf225d463b76c4bc392f6
Alex Stoken <alex.stoken@gmail.com>
2020-06-16 16:30:12 -0500

    add logic for resuming and getting hyp for resume run

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:af8c0a7d53f2c91385e7f5701677236cb7b9009bf438691b8c2f14470bf39f40
-size 22470
+oid sha256:61f2e597f946d0ae4213411f309aabe1c910e578f0b6c47933e2810b8e3a9fe0
+size 22986
