18cef8f24103745d7e439ec61621327e54968eac
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-06-16 14:42:30 -0700

    Multi-GPU disabled in test.py

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d9990e94d8220e5bb756995080fbcc7605f25da2cda6f31cbf23ef369892a66
-size 12898
+oid sha256:a34cee3910e166f4477c0d3b89256854078415ea630594ecd82ff44a26e4277a
+size 12946
