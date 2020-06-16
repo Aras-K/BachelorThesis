872e498a64359576ede36eee71d79f96667e1be1
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-06-16 15:54:20 -0700

    AutoAnchor update - improvement check

--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4aba78893c730e6a40c760258f12ecd1f1471563e8c3e30935f26796fe477749
-size 48387
+oid sha256:29089c5f9565950bf4da9afaee141b4549df378dd4d049cbfa85b5897f11d869
+size 48926
