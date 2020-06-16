32599565705abac28ebd1ddd9688ca721054e933
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-06-16 16:19:58 -0700

    AutoAnchor BPR to 4 significant figures

--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:29089c5f9565950bf4da9afaee141b4549df378dd4d049cbfa85b5897f11d869
+oid sha256:1ae3eddeda8e5d031de1dadd5b69a1c7d9a017085b4859399cac37dccb5486c3
 size 48926
