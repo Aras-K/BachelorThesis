4fdfc0412a183308f4a88b02a15dc25979436ef7
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-06-16 16:48:01 -0700

    AutoAnchor n, imgsz bug fix

--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1ae3eddeda8e5d031de1dadd5b69a1c7d9a017085b4859399cac37dccb5486c3
-size 48926
+oid sha256:a3a96e66d6755b6d4432418e2aeb01b3bf85b186bffa13ff2ff0c9da683b4891
+size 48947
