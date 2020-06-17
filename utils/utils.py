b5b9cdc1c4b40439169c997bb9720ced247573b2
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-06-16 17:08:45 -0700

    AutoAnchor fitness to screen

--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a3a96e66d6755b6d4432418e2aeb01b3bf85b186bffa13ff2ff0c9da683b4891
-size 48947
+oid sha256:5d4b054f7e6a4748d0efdcae116a2119175d0b2f9536b43a2b2045ce6ee782da
+size 49065
