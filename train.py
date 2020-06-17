38b4b3adbeab81191d00963e17774ed2b9b7c0ba
Alex Stoken <alex.stoken@gmail.com>
2020-06-17 16:32:13 -0500

    Syntax fixes

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:056861530aeaba5fd8c747bd9437882bbe480edcabc8c71eefacc8a0d9e416ee
-size 23165
+oid sha256:ae836d536ba10c905b43936448fdafeeba19244995b410291a8d0a9ffb84831b
+size 23175
