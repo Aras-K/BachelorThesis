a529347160ddaa42cbef5964e55a5efcc7bc0264
Alex Stoken <alex.stoken@gmail.com>
2020-06-17 10:34:37 -0500

    Fix get_latest_run to search 'search_dir' recursivly

--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:79902f56e7c6ef16456a2680e1cd90f59fde10bba574444e85f30ddad0b75c15
-size 48986
+oid sha256:8ce563583f31c339fc98ef768a6f58c344342a616b928d1edefdb70042adff68
+size 49003
