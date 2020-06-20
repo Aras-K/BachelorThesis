d5c2fa09de20fc4970b015d74537902be7000567
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-06-20 09:26:07 -0700

    vid_formats bug fix #146

--- utils/datasets.py
+++ utils/datasets.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bdcc391a647db949b1918d4e143b40dc0b5c0000cb42352e2610ca4cbd3e0fe4
-size 35010
+oid sha256:1b22f320fba9968ba872e908bccb376c2aa7b6378015bc7932e046b16199bf00
+size 35018
