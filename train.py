0cacbf18441d37bdbc2abc9979f574696c5b1872
Alex Stoken <alex.stoken@gmail.com>
2020-06-24 10:07:43 -0500

    Default optimizer SGD

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a1a131858a96308b82993dfe530d09a9b8c7e1cae65ffbb4c1d8b944a86c78c9
-size 22489
+oid sha256:caea552557b8e8aefd7279254914c82d435696641b187193228f293af34f0658
+size 22511
