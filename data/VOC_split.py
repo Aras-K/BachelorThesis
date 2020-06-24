cd620b951acd0d44822615f52f79d2b776cee8e5
ChristopherSTAN <497592613@qq.com>
2020-06-24 15:53:34 -0400

    Add files via upload

---
+++ data/VOC_split.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7f9acbeedc84841a6220477eca59215d25545f4d8779f4833a81a656ed9605d8
+size 2279
---
+++ data/organize_VOC.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6b0b2014090e8c2d15eae808d4ec20cfe889a1576c9e500307abf708b9414d74
+size 1406
