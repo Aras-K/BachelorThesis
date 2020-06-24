6bed41967394b956097b8eab31ab78c42a9b2c0f
Alex Stoken <alex.stoken@gmail.com>
2020-06-24 17:21:54 -0500

    Fix yaml saving (don't sort keys), reorder --opt keys, bug fix hyp dict accessor

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f2a4af9828b20ae3b19e0bc51ee78bb2a111cc2cbdb8d1d8433d3a32745a4617
-size 22518
+oid sha256:a8defd3f861f1c1f22902bcc69b9ecc6931f6387d4fd236226fb4353eb9dad03
+size 22554
