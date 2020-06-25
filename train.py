47d7484c1007e9d98e35486c5c8b756903b15a2d
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-06-24 22:22:13 -0700

    model.names multi-GPU bug fix #94

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a61ea58e1c27bd71c744cb1ada3469ea912bef448ec3313dde005bdd3fc8411a
+oid sha256:3b9bbf4421413c0a80f36cd513ddc0b79e700ee6937c55e43fe141b8fdf5b744
 size 21873
