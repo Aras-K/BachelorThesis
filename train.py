e9325382b69244086b50a57faa48df7488503e45
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-06-25 17:52:56 -0700

    small dataset bug fix #140

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3b9bbf4421413c0a80f36cd513ddc0b79e700ee6937c55e43fe141b8fdf5b744
-size 21873
+oid sha256:ff450facee0c08568c6ac910c07f43d25e433d3bedb617ba77cf3ffe5e2273ad
+size 21903
