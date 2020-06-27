a4d1e5853348a48b418b3ce7cad209d0a1e006dd
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-06-27 13:02:01 -0700

    update mosaic border

--- utils/datasets.py
+++ utils/datasets.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e3b0d53a12db148270481a97791faee274c09efb72b29d2cb782b138b434d213
-size 36282
+oid sha256:642c7f907ee9cb00aa008a25ec299596fd53897b9f6b58ed92ceae58f1f356fa
+size 36450
