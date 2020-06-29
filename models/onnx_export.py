312587aa345aa890bf2a4079fe544e4d1306432a
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-06-28 17:07:03 -0700

    onnx_export.py bug fix #224

--- models/onnx_export.py
+++ models/onnx_export.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bc0359a51ab7a5535b5fbe956da0263dec1f830486f73680a983b300fff691c0
-size 1654
+oid sha256:5445a69d51eab689a7011f3d3d1b13577ecfc6c1e402715a4fa945829dcd8096
+size 1709
