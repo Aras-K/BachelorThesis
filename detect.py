aff64093a62592dcac2eb08f25f8a7e8621b43b0
Russ Ferriday <topiaruss@gmail.com>
2020-06-29 12:56:40 +0100

    Remove non-ascii that breaks pdb single-stepping

--- detect.py
+++ detect.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a3140368911e12ba68547e6ef0223a5402d25f69c9b508e45bd90c75f6c71fe6
-size 7354
+oid sha256:a75f7d272a2456160c5200d5cde6b3850c06f90e2bea1b42b37e13b2f33d5c91
+size 7352
