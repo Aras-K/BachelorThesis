1663d76d3e19f82831b340c81d3a293deb25be8c
yxNONG <62932917+yxNONG@users.noreply.github.com>
2020-06-30 19:06:28 +0800

    Unify the check point of single and multi GPU
    save the model.hyp etc to checkpoint when use multi GPU training

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:be15fdb6e75effe39285bd415ac3cd22324c50941122b2fca2b61816289abe15
-size 20797
+oid sha256:cb46050c2d6948c16e1c82f98abaa59a2bb2fd812d5743b86af7c47fe1f43b49
+size 21330
