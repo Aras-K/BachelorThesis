fd5b07716ff53ab34543d862509f1e72ff53b8fc
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-06-30 16:41:26 -0700

    check_img_size() int() update

--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1708d57206d5bcbd6f55f1fa0fefdddc407580ca0d9351fe0a6de8a894003b05
-size 49825
+oid sha256:0d329f790ef16a0387adb6ceb5e047f2bf596396c8cee4c3c9d848dc7cc2dd91
+size 49830
