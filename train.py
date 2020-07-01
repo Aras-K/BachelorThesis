1f7d99264b1a66d5ed14730dc0027e032a55ec79
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-06-30 21:43:53 -0700

    --resume bug fix #252

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:08235ea8f49ab2497957cf9d4a9aa31d89eb53e9948966ac59578db8b5c0e7ff
-size 21056
+oid sha256:eeaf605d93fca35adb31a93c1bff8864ac1f1c6bbeae707a15aee048b5f759d8
+size 21076
