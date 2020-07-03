072ecee18dcb8985f2face04c2efdf1091a44028
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-07-03 16:57:08 -0700

    EMA FP16 fix #279

--- utils/torch_utils.py
+++ utils/torch_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7e4a9e89ac0fc6ffa0eb351406064c7a156d60040ad062197bb6c18e89af4a78
-size 8568
+oid sha256:0769686dabb167b6864777f59c27eee0fc58094e4870fb95a2221e368400b955
+size 8554
