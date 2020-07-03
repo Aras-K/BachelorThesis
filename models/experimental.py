db4720bf40b40b88daaa17eb7794c429c3fbba46
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-07-03 13:46:12 -0700

    module updates

--- models/experimental.py
+++ models/experimental.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bf205d6ea144de9ee62197e0ac47416f80ac061e26f1a6eba99aea5f7c37f7d6
-size 4724
+oid sha256:6aa09161518db2b63fe83c429d8b7153153b07604a8560267dd4b7feb0f9d238
+size 4352
--- models/yolo.py
+++ models/yolo.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9991da2e38e83ef6f3dc6d9e17340788e2c07c5e754ce01cd9d2dafc2d7fc53f
-size 10347
+oid sha256:f80d26d33dc537e0165c70df26c8d918c2b5b84968e1d70e5d6051c9219938c6
+size 10348
