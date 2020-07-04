4e6199e70ef16b91e5fe9b48f66c5f79b77c8e08
NanoCode012 <kevinvong@rocketmail.com>
2020-07-04 23:12:57 +0700

    Update to remove "module" attribute check

--- utils/torch_utils.py
+++ utils/torch_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:90525cb4ed23b2720f0cff7c6e378699f41d4770d4c9381fc036c278b9d856f0
-size 8588
+oid sha256:708fa248b52d260c6cb8108acc1cc59ec0bab769efb14bd0e90f2a907d83028f
+size 8578
