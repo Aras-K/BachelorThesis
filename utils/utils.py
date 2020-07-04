869c5a48d595d66048d59463e965f93d07d30ed8
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-07-03 18:56:07 -0700

    update compute_loss()

--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ccdbd11bbe1209ac333ba36ef3ab79790e94f2284e963432d5afefa7ef2c6307
-size 49891
+oid sha256:070fef672e66d1f320d58d492fb7606bbe62fac2329eb5d748f773871265d770
+size 49932
