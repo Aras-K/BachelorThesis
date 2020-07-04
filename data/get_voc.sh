ab85044d54e5494f83761e509ae699ea9a973239
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-07-04 16:42:12 -0700

    remove verbose tar unzip

--- data/get_voc.sh
+++ data/get_voc.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8967d270141c4ba990278d061ee63836c57eaaea2715ee155ef8a2d9a1cd65e7
-size 6021
+oid sha256:07083c6f507ea52b355ea7e53e569eac693ad409828da373958dc4c40fc5e127
+size 6018
