1f77488d85d554fcba9738be8596752572e78c80
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-07-03 20:05:50 -0700

    update export.py

--- models/export.py
+++ models/export.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:92a9f77dced55c647ac261fc6c2020796273e5a24a38b6b08db6cd5b56e09bf2
-size 2338
+oid sha256:cda78cbbf6d08d4d5e85963e4a9854253aa000f6e95d79192c0d6612b1b0cd41
+size 2342
