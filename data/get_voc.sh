20542282504fb5bffc7cd1e2d1605fcb3e37a259
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-07-04 17:36:53 -0700

    update get_voc.sh

--- data/get_voc.sh
+++ data/get_voc.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b60b0103b92d918fa6d7188a4ddc423d6545dbcfc2a2670ab406beddaddb5d57
-size 6013
+oid sha256:d6ec6f3017f0db2fe78cf1f37d5085c3eb028102fcd2130a232fd21c7893250b
+size 6049
