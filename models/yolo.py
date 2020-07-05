8bff003bd57d9b16c8c9509a0f36aaca9917bd10
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-07-04 17:51:54 -0700

    model.yaml nc inherited from dataset.yaml

--- models/yolo.py
+++ models/yolo.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f80d26d33dc537e0165c70df26c8d918c2b5b84968e1d70e5d6051c9219938c6
-size 10348
+oid sha256:6c8609185644ad91103e515f8dd06d9670c92b076fbdfcce5efc492d94db4a41
+size 10457
--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a3357263693fdd1dd57fdd84cd1cacbffc6853d1efe30deed2a1ae225c3628ae
-size 21036
+oid sha256:d04223d1fee4c22d3278512fbe4ec7492432faaf8da728a3447f70ab297952e0
+size 20941
