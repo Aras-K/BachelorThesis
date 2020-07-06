1e48a2f0374ce7f82511ca64e348ab124ddd6217
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-07-06 11:46:10 -0700

    update fuse_conv_and_bn()

--- utils/torch_utils.py
+++ utils/torch_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c030ae3d1c9a544ec9a04b810f4f8aebed4273cedd77da37f06d154d4199d3f3
-size 9184
+oid sha256:465ac108581002ac60e71ec2264a9ed23fbe9f2b2a85060fc8e5aa6633e56520
+size 9106
