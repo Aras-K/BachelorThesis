63d8d3ead2f6c33b6095d12eef214a334f9c2e14
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-07-05 23:24:53 -0700

    .half() bug fix

--- detect.py
+++ detect.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9526b9117b4cfdf8718cf07bbeaed3a5a1b183b65d20d1c0979fe5d2a8ba6475
-size 7333
+oid sha256:4397e9107fabd1f531ed338d5ff8f70a419f4275ebd6c68c615fd6346a6012e2
+size 7332
