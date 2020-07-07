5e0939ea7af1c0b25c0f519341af99ada707d1b0
Alex Stoken <alex.stoken@gmail.com>
2020-07-07 10:42:28 -0500

    Add in --resume functionality with option to specify path or to get most recent run

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b0b79342f1f337b6372c77f31b36af1c0e0c9ae826d3a189d8fde1bedae5b7bd
-size 22167
+oid sha256:ab7ea3a3550c1d349df7b430053c37ad64315705810c0361f605be2bd8cd2ac9
+size 22465
