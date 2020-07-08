1d03dbbe9c8fe3cecfa2b4c7248f57198507ef70
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-07-08 16:29:31 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f0d0038c210e15b7e0529ed3f7d2bd25feec76e52728f82fd5504deff7f362e7
-size 12463
+oid sha256:bc25149dc125c062f527e90a6260618d87f14e148cc01c1e08c3c596ff4476bd
+size 12450
