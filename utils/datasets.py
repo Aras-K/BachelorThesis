5c9d9c18b0db073c7aedbd43ab1d8aebb16ae19b
lorenzomammana <lorenzom96@hotmail.it>
2020-07-09 19:39:28 +0200

    Refactor code to reduce duplication

--- utils/datasets.py
+++ utils/datasets.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bf32adde3626d3d2c1002ba114eeceb2d64a102ba5e686f2a5ef560844885bfc
-size 37882
+oid sha256:fdd57d5a034c0ce360c975dcdc901ac75e3a4b1d0a0b032d1e4e0d3466cc35f8
+size 37761
