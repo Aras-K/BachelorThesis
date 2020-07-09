ac0c006938b4452ca25d02a730c9c9dd71393dd0
lorenzomammana <lorenzom96@hotmail.it>
2020-07-09 11:52:12 +0200

    Handle multiple datasets

--- utils/datasets.py
+++ utils/datasets.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c42e4c919dc67c011e47954273b97271a9479b6d0207e776043a458e1c3d873e
-size 37209
+oid sha256:bf32adde3626d3d2c1002ba114eeceb2d64a102ba5e686f2a5ef560844885bfc
+size 37882
