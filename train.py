20629f1e2020cb8add852a281c3a1ae477896345
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-07-09 17:03:12 -0700

    new nc=len(names) check

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a00ca741896790c4cce5176539be23d21ad2e3af2eba524d75398b2b5816a30c
-size 21890
+oid sha256:4dd4227b89586385879b1f814f2ace53db5506f4db9ccf5ad36caeb97af13149
+size 22035
