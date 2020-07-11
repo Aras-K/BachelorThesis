4ff6aca3882e5a067a07bb704b07279d734a970f
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-07-11 12:39:27 -0700

    multi-gpu ckpt filesize bug fix #253

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b9fdd7b81d34056cde543e98e64ccac56461ecee7d076dae5bae08c44f984c5c
-size 22094
+oid sha256:48310bd4f875b7f4c14ac20575fd476fd2327fd7602050e796c1207dd2f4283e
+size 22100
