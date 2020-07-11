7a1868600398848a95f06179b921345c8bf93625
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-07-11 15:04:53 -0700

    expired pytorch hub cache bug fix #365

--- hubconf.py
+++ hubconf.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3f2e120c1c593b7482483eb980dfd80c62645aa27b79341483d3d6522b34e051
-size 3310
+oid sha256:3ac49c94acc6f3d4dc87f972d6941dce76ef0a09d2aa3cd0d38b2a7e9d13bf3d
+size 3312
