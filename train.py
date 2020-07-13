3ccb1d7971ec9f7f5dd4f0c37915f23658cd1cc3
Janko Ondras <jondras@cisco.com>
2020-07-13 12:55:43 +0200

    Log hyperparameters in tensorboard
    Log both hyperparameters and command line options in tensorboard.

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:48310bd4f875b7f4c14ac20575fd476fd2327fd7602050e796c1207dd2f4283e
-size 22100
+oid sha256:9804061fcce16c27d1c995bc41449347bca2b2860b0b4f2214f2add1e2246bf7
+size 22422
