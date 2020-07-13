62d6a8ceded5228a7f6903ed8565927767dc20b7
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-07-13 10:53:34 -0700

    update detect.py --class argument #387

--- detect.py
+++ detect.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f0727b20e0886db30d9d30b65034aa9bd62360ec395fd8c3d807fc349941b513
-size 7276
+oid sha256:c6d4101e6a8ee60331a0f40f8593412ea3f62746f1772b81c3d42f829dacbea8
+size 7303
