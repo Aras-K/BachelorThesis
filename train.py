5186f27952e78227629bbfe3ffac79a3bf8581d9
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-07-14 12:32:08 -0700

    Update train.py
    This updates the PR to a one-liner to minimize additions. Perhaps we can include opt in the future but let's start with this for now.

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:416ac1179c5cfe1f89b3094fced07d7aa3442c40f1453c17de2eff2fe16a2402
-size 22429
+oid sha256:eff374e5e93dadde4a046375d95f1f47cfdfaada371ca960aaf90b216848b07b
+size 22139
