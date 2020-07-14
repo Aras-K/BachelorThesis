6747d88239f3c7e14badb977b706e616270717fa
Janko Ondras <jondras@cisco.com>
2020-07-14 08:48:46 +0200

    Fix img_size naming in hyperparameters logging

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9804061fcce16c27d1c995bc41449347bca2b2860b0b4f2214f2add1e2246bf7
-size 22422
+oid sha256:416ac1179c5cfe1f89b3094fced07d7aa3442c40f1453c17de2eff2fe16a2402
+size 22429
