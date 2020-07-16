aaf9136c045c11b3933d2916683a9802768be989
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-07-15 18:25:34 -0700

    test.py --task test updates

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eeedeac44fa3b026775d34372b0874109067981b5f0f148d0ac1c17c3a8657b3
-size 12311
+oid sha256:fd4d53b2af9430e151893aa79ac50a400848b8bf4873b062fa17448a390809d8
+size 12330
--- utils/datasets.py
+++ utils/datasets.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dfcf710db118347a0c122f2ff4a365d7a163e2e932756378772f97388e2e9288
-size 37775
+oid sha256:2e7cbb2f50cbce54ba15cc0f075155124f7e19ad5a19ff73d39c4b24789a260d
+size 37890
