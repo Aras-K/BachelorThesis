fcb8eb1fa5f6924674758415cd1320e55413e82f
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-07-16 12:31:37 -0700

    Update eval.py

--- eval.py
+++ eval.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db51f2c9a07669fcb814d54dee060bdbe987f08a864d13f7c4670246a8711984
-size 13200
+oid sha256:25d8d10b7d9706e0ace0165547314531204aea3bc736a3dfc365dc48d9ff938e
+size 13140
