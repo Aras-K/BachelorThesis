1630ffdb49a3ca21357013ca047d3f8a87db0563
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-07-18 14:34:12 -0700

    update train.py ckpt loading

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:96e1c63c9cb3f34816c098c4a81c1887dd47b50c6de76f66b6f0c756e3e1e069
-size 22164
+oid sha256:75e79e404dc2676ddaa5af101ca260130e46dcdff4b37143b7f7928aa3531d92
+size 22101
