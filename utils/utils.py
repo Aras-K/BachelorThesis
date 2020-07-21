c41c3e94be4431d7718c3b6e10f49bc45b260c63
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-07-21 14:07:11 -0700

    plotting improvements (#471)
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5f404c2f819645de0c4bd01f3e2a09bc7a33b1eb502a1bc34dd46a6f19c8f148
-size 51253
+oid sha256:f749acfce1251f10d040044087f5d3f0b5039cfa001c862a40d0ad0d2652a7a1
+size 51325
