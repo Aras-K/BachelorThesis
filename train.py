c6e78eb4cd11538bebdc48d8d87e53dc4d263433
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-07-21 23:25:33 -0700

    update train.py gsutil bucket fix (#463)

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b8a51dcdaa18c5c5e6eef7a86f0c963d7bc5671b149b59151628f8acd1ba7d60
-size 26119
+oid sha256:e8869df3b2a1c948118d1bf14948bd20619b4c1f15d236336256191e74968d3c
+size 26129
