e589abdfc5bca7a7e0ed5ebdc83098861fa76860
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-07-24 19:26:56 -0700

    Update utils.py strip_optimizer() (#509)

--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8e13bafa68c87d23644d9e2abb45ac5803912862448283eb8af96caf071a4e0e
-size 51446
+oid sha256:d3508ceac2dfa4cb84952e5ac4b990d9a8fbc88002b2a49350fb1074659b8114
+size 51068
