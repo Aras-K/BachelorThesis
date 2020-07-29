40d814491381646dfbdfb6ccabdce16508b8953e
YuriRibeiro <32506180+YuriRibeiro@users.noreply.github.com>
2020-07-29 16:14:11 -0300

    Fix utils.py nonzero() warning (#551)

--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3345ced9bd5b70bcede7c33e334ebb91aaf9b2de92f94a3987a0b738bced3306
-size 51077
+oid sha256:61791a642400283f19aa0a9dacd0528ec45245d6d56a8d97c2eab1bf5f3bc2c2
+size 51091
