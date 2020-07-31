d0ebccf796c3d297f09c0fd17d600d2cdcc1e53f
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-07-30 17:38:15 -0700

    Funnel ReLU (FReLU) @staticmethod bug fix (#556)
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- utils/activations.py
+++ utils/activations.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:38796a22cb61238e319f9f289d223fb2cd8c9fce24b5add9c0805be2048dd1b2
-size 2021
+oid sha256:630b3e1b6cecd7574f71e25a8753cb0d05e6b4d5a8436ebea34948ff823ec736
+size 2003
