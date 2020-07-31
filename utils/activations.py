a668b9c773d0d507cd0d6eb7cf952aa10b05ba67
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-07-30 17:35:34 -0700

    Funnel ReLU (FReLU) (#556)
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- utils/activations.py
+++ utils/activations.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ad19609b9eee043673cecee3c34c3cb588931bc26a3dc177d1f7d0c4239a942
-size 2067
+oid sha256:38796a22cb61238e319f9f289d223fb2cd8c9fce24b5add9c0805be2048dd1b2
+size 2021
