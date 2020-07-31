12a3e061b2d4b6bf9463ddb43b415f75d7c943ab
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-07-31 15:53:52 -0700

    replace random_affine() with random_perspective()
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0dd3ea81530f27de3384e3e7f456bae76607fbdca8bfe10d880ab2a9280f4240
+oid sha256:9f0de815cb774c81927d350e8ab91e4786307dd1b5741804e400f48fcd0d5663
 size 25663
--- utils/datasets.py
+++ utils/datasets.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d407bbba8e7690574719bc19ab272ad8d4001183212b79f2fa7f02518a3abff6
-size 37359
+oid sha256:e025df152ea524c56d74a03d1f89d66eb45d2ca4a6f2f2f6edaf2db7700db57e
+size 38094
