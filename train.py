a49df73cd3abf2100c356ca7681bfa6e69e05bf1
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-08-01 15:07:40 -0700

    hyperparameter evolution bug fix (#566)

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e1ee4817a4e2a108b5faf78fe3b1a702f81d4b1f71208f172d310393f6305c48
-size 25688
+oid sha256:9c9fcea7ee5bda86c42458b0693ade316932b2ff72a96273cf12369289a9a16f
+size 27226
