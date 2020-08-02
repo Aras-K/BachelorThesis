0eb4dad0e094700e80025169363af31579d4bbe7
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-08-02 14:38:45 -0700

    AutoAnchor update to display anchors/target

--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:188e5759271d64e02763cd863442723ffe73557dd9a518b060cb8adb00a3fcfa
-size 51457
+oid sha256:68582a9b95508c9f7b24f6fd4da4d91f29ab15d5885503cdc59bd723d5acd858
+size 51596
