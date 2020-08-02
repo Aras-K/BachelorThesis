15cfb563999efd8ed81457c33e3797de124b16b1
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-08-02 13:18:03 -0700

    hyperparameter evolution update (#566)

--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ca6facc0ebc74fc026ef55a0ef2bd72778447db10be12908cdbd59c5f50ee1a3
-size 51454
+oid sha256:2e808e56b75b9801150035a8078235abd763ae663ab375d19d0019db7c9dac94
+size 51461
