5895194e51a1262aa85f7e1a3beed8a4de0fcc7e
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-08-03 10:52:59 -0700

    import random bug fix (#614)

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a218fa49f32e2dcdb8275375db4bbf6d590ceef2b0bf5a6dc0963f95d593dfeb
-size 28239
+oid sha256:81cd41e35b9e5e56cc31a98f47e077a378620c2c5b36a7940332a09210de17de
+size 28227
