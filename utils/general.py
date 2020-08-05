78c6f56d882a864f7d79f4b349af76b7f2204bb6
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-08-05 13:35:31 -0700

    zero-target training bug fix (#609)

--- utils/general.py
+++ utils/general.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:378cfa4e2513f902662dde015aaca694b8794f87a6e87a1d85293634dc9c2caf
-size 51567
+oid sha256:dd8e3509a44ad456eb857e8a763e836f9d603bd22f1053b3cfa6bd31cbdd5b81
+size 51595
