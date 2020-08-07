4033dce6939d1fa77aafbddb60b7e717810cdae9
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-08-06 22:26:38 -0700

    train.py --logdir argparser addition (#660)
    * train.py --logdir argparser addition
    
    * train.py --logdir argparser addition

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e83c4b8fec307d3de07c4bdb6aa08ef8cdb7b84a67030b5159aaa864fd03d3c8
-size 28392
+oid sha256:ce671eda96f2ee1d5f848153ba11defe90a48f3f8ca1d8529a9c57f70315444f
+size 28514
