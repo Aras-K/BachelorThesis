dce7c22f676530e6efc14e976d6b6f1b4d3a60e1
NanoCode012 <kevinvong@rocketmail.com>
2020-08-08 02:10:59 +0700

    Fix curl download on Windows (#669)
    * Update ci-testing.yml
    
    * Fix windows download
    
    * Fix cookie and curl download issue from gdrive
    
    * Revert "Update ci-testing.yml"
    
    This reverts commit 7389d2238df4dd8bd3b6ddded0f12e551c8fd0b1.
    
    * Update google_utils.py
    
    PEP8
    
    * Update google_utils.py
    
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- utils/google_utils.py
+++ utils/google_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3f8e5298fa2544122f8bd274c2da5175e9eb194a364efc2e3005d9c65ac79d07
-size 4186
+oid sha256:7f256d9270f65b5787df927e2e2ba412a9b8f6be8bdbff60e51a6de9bff9b934
+size 4418
