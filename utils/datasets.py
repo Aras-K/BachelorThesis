475321f8c0917da8ccee620c5ec8f125a945c93e
tkianai <49858938+tkianai@users.noreply.github.com>
2020-08-11 02:23:48 +0800

    the value should be a list to unpacking (#688)

--- utils/datasets.py
+++ utils/datasets.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e66743e3a4e9ec9cced754ab3e79b28fab77c1a170de4199958829c3f397b1d0
-size 38157
+oid sha256:3db716e557ba0bcdbc5a5a790cca0d76633b8008b4342aa4e71c2bfdb2fc9fa5
+size 38165
