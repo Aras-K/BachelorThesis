4ddd6afe0354ab75d7c318e5493c28128a9785e5
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-08-09 21:24:40 -0700

    update train.py remove save_json final_epoch

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f5d34b7a4258dbd5826c45e769bc14fdc31b2ad8c5187b173e5a414749f00e4c
-size 26500
+oid sha256:28a3f13cadb8b970de63c9da7faf471c4e0bdd2d3472a96c1f91aec1c9b12a36
+size 26384
