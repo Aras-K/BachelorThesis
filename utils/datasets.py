9477a03e4782386e20bf17ccad7c407e4c1fe036
tkianai <49858938+tkianai@users.noreply.github.com>
2020-08-12 02:15:48 +0800

    Filter the None labels (#705)
    * the value should be a list to unpacking
    
    * filter the None labels

--- utils/datasets.py
+++ utils/datasets.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3db716e557ba0bcdbc5a5a790cca0d76633b8008b4342aa4e71c2bfdb2fc9fa5
-size 38165
+oid sha256:8d96370399489a9c3cd5b750abb4f4ab3bf13eb0adadc8a52ce1c501e5d6581c
+size 38183
