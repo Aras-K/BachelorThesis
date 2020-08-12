9e9ca72650fcba4c11114ae6cc60e0574e77a5a5
Marc <marcpaquet2@hotmail.com>
2020-08-12 16:57:36 -0400

    max workers for dataloader (#722)

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7ca2e2ea0cd11902f8a31d02083029bc100bf9189cd85810f767f3db1442c0fc
-size 26787
+oid sha256:dc36f81f4faf411b40f57dc7e3164e9339a27635de5d983d53a8acc10d0a0aac
+size 26975
--- utils/datasets.py
+++ utils/datasets.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e3be686c4c0acd2e79bf41f6c5811553f4dcea1e466e5f76d657add588c176e5
-size 38314
+oid sha256:e0d1cafe6d363fbadb7e34e24fbbbc7889e0b39775c07fc58e0c2661de153339
+size 38331
