be7597a6e382ef2f7d05d7f187d60e5d3404999c
Marc <marcpaquet2@hotmail.com>
2020-08-11 20:17:24 -0400

    Github release assets model autodownload (#711)
    * assets autodownload
    
    * Update google_utils.py
    
    * Update google_utils.py
    
    * Update google_utils.py
    
    * Update google_utils.py
    
    * Update google_utils.py
    
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- utils/google_utils.py
+++ utils/google_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1afeef13934c006a8e74cea5bfe7f37cad14a1b4f8f9a1afeba3a3b05fea890e
-size 4404
+oid sha256:76598e2da46b20713c4168b4877c2b36211aba71569478b1aa57c69a57843ca4
+size 4880
