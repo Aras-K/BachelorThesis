417394281fad99aa6ab34da5db1d0990326bb6d3
Hatovix <68970384+Hatovix@users.noreply.github.com>
2020-08-13 03:53:38 +0200

    Fix list paths (#721)
    * Add list paths on check_dataset
    
    * missing raise statement
    
    * Update general.py
    
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- utils/general.py
+++ utils/general.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5c07ebfc8fabc5733b440d0e248051a15bb7272924f8d65c4d00c10980a3854c
-size 52701
+oid sha256:dec81e87d6d972efc9ea4c3f507def9f76c5ea0a529c8e676d477054962110c4
+size 52983
