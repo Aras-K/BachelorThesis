c1c0ae6a13e7b386007607645cee2a19bdf6532a
Francisco Ingham <fpingham@gmail.com>
2020-08-16 17:58:59 -0300

    Update torch_utils.py (#751)

--- utils/torch_utils.py
+++ utils/torch_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:333f693a89266959c91ad4a7de1474f6a212f710df574e0764b230180dba4242
-size 8931
+oid sha256:b0488a69f59d1e624dfafd421ec87837498f28567e0c020ac6eece441a0abfab
+size 8941
