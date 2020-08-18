3b4be6527e57ec2cb7a248137eaa80d937e5eba6
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-08-17 17:14:07 -0700

    --save-txt bug fix (#763)

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bbc7049931238492cde945cb680f80f8bea133704f94d401234a2cdeef10210c
-size 13511
+oid sha256:0b3ba68a2f595e346367dafd5cb01a53bb94d7066e975d0e6b0f5be4552f63dd
+size 13499
