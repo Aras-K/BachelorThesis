15327a0f7821c90a0d137ba7c0fcd42bd5d02abe
NanoCode012 <kevinvong@rocketmail.com>
2020-08-18 23:52:21 +0700

    Fix ema attribute error in DDP mode (#775)
    * Fix ema error in DDP mode
    
    * Update train.py
    
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:86cab6afc0786002f8f6070522670ff0b793442de9482e1d20d067a86c68df23
-size 27355
+oid sha256:b7d0608b76706b3b928a32a941d8ef7dffda61d1837ecf6642f9b5cafb80f8df
+size 27359
