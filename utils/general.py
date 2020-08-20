8cf6176a4bc20ca10e129c82ff4ed480cbc502ca
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-08-20 10:37:55 -0700

    CIoU nan bug fix (#736)

--- utils/general.py
+++ utils/general.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e67beab07a958050db97911f027eb7fb84bbcb2557def46bb92a8a8bc1f66ef3
-size 53196
+oid sha256:a728844ea3dab5504eaceae225b7ef1db9c203d36e57be683767aeaf5d9ac541
+size 53229
