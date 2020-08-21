71dca339f7b828a4c6c9a073206311112cb59d92
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-08-21 14:44:25 -0700

    FP16 NMS (#814)

--- utils/general.py
+++ utils/general.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:05649138324b8aec09286a114ec4a58fa46f85102caedfebe248abc1890e3f4c
-size 53230
+oid sha256:8f4c7471d5b3b69896374396c7d098871c1bf75d1f7485cf330efad514e86346
+size 53137
