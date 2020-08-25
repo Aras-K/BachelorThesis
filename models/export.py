b12ffb2ca55139d0755c47b5cb7b354b48f4c5fd
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-08-24 21:59:26 -0700

    Update export.py with v3.0 Hardswish() support (#831)

--- models/export.py
+++ models/export.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9a54de0d2a09470ee13a42afddb57ece524cacdb30d6415ecd1c2de0f82ac7a8
-size 3289
+oid sha256:7645e22df5545f59db75e3624f0e2d19cc25233c84ebc6cc3ce6ce083a67351a
+size 3347
