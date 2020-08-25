d5f4ab169bd82c6c33042e551b3adcf031d74787
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-08-25 03:14:17 -0700

    torch.from_tensor() bug fix

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a5d104c56719aebdd4108539e808724dab49d870155051d0b79b7ebaba4db0d0
-size 27436
+oid sha256:410ee0a7e5f4e915d8202b7f5722a1df30ae4da2fae20d11391b63ee879f729f
+size 27431
