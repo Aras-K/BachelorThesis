e9d40f48181f81423297b2f1896243716b48c7da
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-08-25 13:48:03 -0700

    Update train.py forward simplification

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:410ee0a7e5f4e915d8202b7f5722a1df30ae4da2fae20d11391b63ee879f729f
-size 27431
+oid sha256:dcf9ae3561b791b33cdfbf825845ff6e4a75556187b406107b1efc5f1593c65c
+size 27220
