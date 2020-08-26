27a6ad0872cf35831fab93deb07c2d63b82a300b
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-08-25 23:08:14 -0700

    Update general.py plot_evolution()

--- utils/general.py
+++ utils/general.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f10972be5c9aa0d81294f88dc42f00dc06eb134972552e7399f2bdccad5e2a97
-size 53145
+oid sha256:b491e02b3e054a51a929f7ef20974f0f340e1c896ab4a7c34959defff1505de0
+size 53139
