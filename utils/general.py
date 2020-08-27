089e7b4ab78a03ec364d76c604dae8102c691040
rafale77 <anhmantran@yahoo.com>
2020-08-27 11:13:41 -0700

    torch.ops.torchvision.nms (#860)
    Don't load the entire torchvision library just for nms when the function is already in the torch library.

--- utils/general.py
+++ utils/general.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b491e02b3e054a51a929f7ef20974f0f340e1c896ab4a7c34959defff1505de0
-size 53139
+oid sha256:92781f5c88d09575a5864846787b3164f90edd4b8f4f97b56f7140adf02b518c
+size 53120
