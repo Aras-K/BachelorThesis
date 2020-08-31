8aa63f23561b253c96f24b59759366f7c8d1f17f
NanoCode012 <kevinvong@rocketmail.com>
2020-09-01 01:01:25 +0700

    Add InfiniteDataLoader class (#876)
    * Add InfiniteDataLoader
    
    Only initializes at first epoch. Saves time.
    
    * Moved class to a better location

--- utils/datasets.py
+++ utils/datasets.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:15d59a671dfc1dbaac517a2fd552cec9239763a2a6b36cd7b610d38ac735b525
-size 38403
+oid sha256:270ceb2ebd3e0628c43bae1a2ccc1b424d6d1a56edbe29ee7dc372d5bcee22ea
+size 39182
