eb3ad82d70fcb2e006e0f9fc704c5f4db06ae43a
NanoCode012 <kevinvong@rocketmail.com>
2020-09-01 06:06:10 +0700

    Fix relative path for VOC download (#880)
    Cleaned up code

--- data/scripts/get_voc.sh
+++ data/scripts/get_voc.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0a031ac3d1f7e94a9c9406eda3a16dd5cf94213a4a516a50287ea89010f75a47
-size 6000
+oid sha256:fab36032eecdf016548edfc5da153b8e03675cd30b5364e24068554a3233852f
+size 5457
