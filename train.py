974a9ecc6cf3aa8d1268f401a86592710cfe0553
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-09-02 15:08:43 -0700

    Update train.py (#902)
    * Update train.py with simplified ckpt names
    
    * Return default hyps to hyp.scratch.yaml
    
    Leave line commented for future use once mystery of best finetuning hyps to apply becomes clearer.
    
    * Force test_batch*_pred.jpg replot on final epoch
    
    This will allow you to see predictions final testing run after training completes in runs/exp0

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e593d425560bbf92ce027c0c4837606fc60c5ee2c30e3d5a720a5afe27f187d1
-size 27542
+oid sha256:7b032e9446347c8682c7d5bb5b776a00ac5f091c75473c8cc24c476c8bf18dd6
+size 27677
