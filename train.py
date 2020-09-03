587477b4ae92e75c463a0249948818cecb02103f
NanoCode012 <kevinvong@rocketmail.com>
2020-09-03 14:47:50 +0700

    Fix results_file not renaming (#903)

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7b032e9446347c8682c7d5bb5b776a00ac5f091c75473c8cc24c476c8bf18dd6
-size 27677
+oid sha256:1b032adc07b7e1c8221b365b7056449a97f95ad5b979a6abeee5d2643b31c78d
+size 27684
