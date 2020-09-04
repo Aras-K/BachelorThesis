3854c1d5cffb83edb7aa676ea51b893421a66f82
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-09-04 12:25:53 -0700

    reorganize train initialization steps

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2d632cd3a2f5f7089822c742de91c87b9c53f64119388d1368cf3ef7adab62c3
-size 27697
+oid sha256:48ac04eea7fba9198ee37987879f3f7f9f574f8f9fee67e56a49e1f9bed4f694
+size 27651
