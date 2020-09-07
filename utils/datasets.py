e835e5bbf20c03bfc256a9c8dd9ec0e3b2365ea4
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-09-06 22:52:28 -0700

    glob.glob(recursive=True) search (#917)

--- utils/datasets.py
+++ utils/datasets.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2ba8aeb11fc20f1c06c5ad3518995187ddd1e510610150158f4e64e2789fc4e6
-size 38988
+oid sha256:a34f51bd4457017089a78f77ac2027f5c57bf041ca19f78afdae4d503e7c50f6
+size 39004
