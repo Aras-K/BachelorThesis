efc6be6a2bb2196d26b1035f5b9cc61f65d1cd30
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-09-10 12:27:35 -0700

    _RepeatSampler outside of InfiniteDataLoader

--- utils/datasets.py
+++ utils/datasets.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a34f51bd4457017089a78f77ac2027f5c57bf041ca19f78afdae4d503e7c50f6
-size 39004
+oid sha256:3d2f11112af27fada1916b143eeb51bde50f2012ba13d99a539c8cb862405b4c
+size 38993
