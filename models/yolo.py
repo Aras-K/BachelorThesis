f4bb66427a4c8a53993570ee6974b8d3bb1de11e
tomguluson92 <314913739@qq.com>
2020-09-12 07:35:48 +0800

    remove `nn.Conv2d` in default settings (#949)

--- models/yolo.py
+++ models/yolo.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9b1503dabb36540cb8b1c378dd945875b54611b451ce6e242c9ded0e504c6f60
-size 11535
+oid sha256:de805b8c103581cb16538380f13d8eefef483a224ace3d8fecccb408d9b455fc
+size 11524
