4180aa5c7ab0f5fc6573ff73a8c8fb3eca89200d
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-09-11 17:44:11 -0700

    print self.yaml['nc'] override update

--- models/yolo.py
+++ models/yolo.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:de805b8c103581cb16538380f13d8eefef483a224ace3d8fecccb408d9b455fc
-size 11524
+oid sha256:169d83f3a2190229372ff641788a6c7a9995b08dd7aae41ceb47eddc8747c5a5
+size 11527
