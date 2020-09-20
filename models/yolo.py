42dc160e2ce5a3fba2d449b26b24637f7156108e
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-09-20 12:04:20 -0700

    .fuse() additional error checking

--- models/yolo.py
+++ models/yolo.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:169d83f3a2190229372ff641788a6c7a9995b08dd7aae41ceb47eddc8747c5a5
-size 11527
+oid sha256:34927ce6f480555026d88bd067952fea0c4e71b872b6c5ae21f8b5e1c7aa6e3b
+size 11551
