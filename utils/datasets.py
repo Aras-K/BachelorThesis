829c8ee43267b1d8b33838129f80f2aac34d5bca
haoyuefan <35680714+yuefanhao@users.noreply.github.com>
2020-09-24 00:51:33 +0800

    mosaic4 bottom left image fix (#1021)
    fix a bug in load_mosaic

--- utils/datasets.py
+++ utils/datasets.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3bf71a4aa82fdd2a187154574ad3f2a71d37d89e1fea5883507f09fb62acd988
-size 39116
+oid sha256:3b83b52117f056842a90202a508ffd2d024807d63cc9031ec13212abac0cc896
+size 39107
