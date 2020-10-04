76152a061fbbb6037184135e308fa0a88c91773e
PetrDvoracek <pedro.dvoracek@gmail.com>
2020-10-04 17:42:05 +0200

    export.py folder sys.path addition (#1052)

--- models/export.py
+++ models/export.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:225b42e69b0b9795a5c8a92c8b524e9f0ba314bcf870c2b25744f8b0b74f1ca4
-size 3374
+oid sha256:53a0b85f61c26689c19f9d2b90e7ef5a25f1711619840fff0ce1a5a6e93ab4c3
+size 3408
