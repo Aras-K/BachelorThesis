cc73773b34198f74f47b6041b8a505bcae5dc1c9
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-10-11 00:42:54 +0200

    fuse() bug fix

--- models/yolo.py
+++ models/yolo.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fff01e65163a1ddfea25b7b4f960bdcc1b5b76b6c08a84fe218bd7f381136f85
-size 12011
+oid sha256:1ab81ef6c0e3a9508d0a4e6068eefefde644cf46b1d44d69a164d5e1f136cbfd
+size 12008
