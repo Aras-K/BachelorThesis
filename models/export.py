bf6214d116864eb424e8815203f60ef696bda3c8
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-10-11 16:23:36 +0200

    Rearrange export input after checks (#1118)
    img size checks are warnings rather than errors, so current implementation allows improperly formed model inputs.

--- models/export.py
+++ models/export.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c5fb2496ea6aedbb3a4a6678acdc5d7273d26e8a7c882361f7c09113320f3be0
-size 3731
+oid sha256:8b7c33f306c897ac7730f1bcf8df18896572813e389cdf773270ebde2f364641
+size 3729
