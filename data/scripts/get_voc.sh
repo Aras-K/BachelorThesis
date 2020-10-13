4f3166abca55f8fc805770986c6ed9eca9ef79d0
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-10-13 15:58:03 +0200

    Dataset download bash script updates (#1132)

--- data/scripts/get_voc.sh
+++ data/scripts/get_voc.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b0d1dd8534c753bad142b28c3e5b2ab822a4a4b0beae60c32b8e4b1ab6575d08
-size 5427
+oid sha256:3aea8359d55e8ae9375242e0326dc17d4e65e4c30f67d835a44e909a749baba1
+size 4387
