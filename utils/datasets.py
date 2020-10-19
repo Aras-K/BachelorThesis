24ffa14f8d7bca27f84ca065ad1a205b94a24819
Shiwei Song <sw_song@hotmail.com>
2020-10-19 18:22:22 +0800

    fix padding for rectangular inference (#1165)
    Co-authored-by: swsong <swsong@stratosphere.mobi>

--- utils/datasets.py
+++ utils/datasets.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff3fb587a7ac008e577b7c010c009f75dcb8e18b75f4882d1bb1d1d1c4905909
+oid sha256:4376598f9aa5adcaf9ee42a5c7b659837445e7756ba2f8f1817e1bb6141e079d
 size 39107
