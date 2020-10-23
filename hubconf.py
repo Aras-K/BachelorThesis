a97e5da4e554b0130430a747428ffd1efb2545b7
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-10-23 15:13:42 +0200

    Update cache out-of-date warning

--- hubconf.py
+++ hubconf.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:84aff669890473722f74c2ebfc7b6f1c78b524c6522d11f5d58da7bd12ea37e3
-size 3475
+oid sha256:b543c71cc8cd42691659a41c64e6d14f346f38e0c684be30e895445b03f35be6
+size 3454
