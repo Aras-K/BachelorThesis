0ad9f6676b79ac979f18cd6aab4a73eb3c619e6c
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-10-24 15:09:19 +0200

    Improved corruption handling during scan and cache (#999)

--- utils/datasets.py
+++ utils/datasets.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:57a18bdc25cd50223d92bfa1516e61d7af9a59f424b6eb7f32d5aba1a1db78d0
-size 39249
+oid sha256:583a1ee5475c291caf3a4dbbc4622cb86f0cfd349061c77f846c0c0a5802fc6b
+size 39229
