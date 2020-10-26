ab7d3393ebfb5936aaeee50732bb5b15dcec1ace
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-10-26 01:06:54 +0100

    Update hubconf.py (#1210)

--- hubconf.py
+++ hubconf.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b543c71cc8cd42691659a41c64e6d14f346f38e0c684be30e895445b03f35be6
-size 3454
+oid sha256:cf7fa2a3d31bfd7485ae1601d84c47f4fd12ab60d6d8ea8c9a757126e1386e39
+size 3756
