d3f06245e48564e94bd8796b8dace50c6a535c17
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-11-07 02:18:18 +0100

    Multi-threaded image caching

--- data/scripts/get_voc.sh
+++ data/scripts/get_voc.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3aea8359d55e8ae9375242e0326dc17d4e65e4c30f67d835a44e909a749baba1
-size 4387
+oid sha256:ce561dc925026f5d8dd8c4d89ca6c14330a6fd749fc2c7249ea0bb3ef5140cd2
+size 4388
--- utils/datasets.py
+++ utils/datasets.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:583a1ee5475c291caf3a4dbbc4622cb86f0cfd349061c77f846c0c0a5802fc6b
-size 39229
+oid sha256:a509deeb919cc3f2b9954ecd0d2efefda523084ef348c9b046413c9ab4fe6706
+size 39388
