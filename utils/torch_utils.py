f6bcf2e10aab7510e9e3628ab3d12afae819b1d5
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-11-14 14:48:55 +0100

    Improved FLOPS computation (#1398)

--- utils/torch_utils.py
+++ utils/torch_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7575c0f80825c95f15a773a350d485b4520abfc6ca6077220f0419f12c679928
-size 9466
+oid sha256:7d182bdb22755674bdbd4f4570fdc2278dc2003db6783e69938d4adfaa1e7d4e
+size 9479
