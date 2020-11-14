43ee9b3cee8ea5f646687d05b7713d54f1e13124
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-11-14 14:39:46 +0100

    Improved FLOPS computation (#1398)
    * Improved FLOPS computation
    
    * update comment

--- models/yolo.py
+++ models/yolo.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:82272be633c548dc1a6b28b2a07803b34484a2d1e1b765edcb1c45e67e5aa842
-size 12295
+oid sha256:7218d1f9ce0534546bc8281d0c22408a6c313d47e6bc1c78b3a0dc21dc7ae6e7
+size 12319
