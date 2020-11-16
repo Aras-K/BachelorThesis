f25aa4741b11e6419507b26148c7826d48ff136a
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-11-16 23:15:43 +0100

    PyTorch Hub and autoShape update (#1415)

--- hubconf.py
+++ hubconf.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d36c1dafede21673ddf07f79ab3c44b837b9b395770ffe91dae5f2a88aa90684
-size 4008
+oid sha256:a5ce4fcbdba8c4ed5d602b019fcae1a58882b3e320dbc028c6523a838f9fda2c
+size 4001
