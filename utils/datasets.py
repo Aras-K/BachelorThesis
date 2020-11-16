88ef42067eddae9949b08d4a8450eaedd62e926e
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-11-16 16:24:57 +0100

    Recursive directories dataset capability (#1408)
    * Recursive directories dataset capability
    
    * x.split('.')[-1]
    
    * f += glob.glob(f"{p}{os.sep}**.*", recursive=True)
    
    * f += glob.glob(str(p / '**.*'), recursive=True)
    
    * remove . from image and vid formats
    
    * .txt to txt
    
    * str(p / '**' / '*.*')

--- utils/datasets.py
+++ utils/datasets.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:21a82cc02848295745e5f3fab7fa25629cc8fdddf1bc809d4212790ec4e79ea0
-size 36862
+oid sha256:26b20468d262b35f208d450df4cb93ce38defef36de8d05208d885eeaa79fb9e
+size 36850
--- utils/general.py
+++ utils/general.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba762da28b5075e9c030e551c53dd9db1272ad847aacb28a181ebe6cc5182b79
-size 17852
+oid sha256:24eee5767142ae1d636a5ddd881d0cc8dc9cbccb53eb01216389a967a791d255
+size 17877
