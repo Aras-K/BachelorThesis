e6ed6e3846401ec2d775273298d187c1f316b82e
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-11-16 16:34:07 +0100

    Matplotlib AGG backend (#1413)

--- utils/plots.py
+++ utils/plots.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6372cfb0ffec67e92f9ed97cb7b4e71bdf0b7c530c5d8d35196cdcfc8fe8f081
-size 15462
+oid sha256:ed1236e040148cdf189fe65e8a07a9757eca2bad0a7f75f83c1b747d2ff12896
+size 15525
