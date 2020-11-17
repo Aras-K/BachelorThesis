6f70035ef03c5b45b95c2b7d9a10189fe0780dd5
asim266 <74567784+asim266@users.noreply.github.com>
2020-11-18 03:47:27 +0500

    Rename precision_recall_curve.png bug fix  (#1431)
    * fix error in train.py line 400, changed precision-recall_curve.png to precision_recall_curve.png and fix error in test.py of {wandb.init() before wandb.log()} by importing wand and initializing it before wand.log()
    
    * Update test.py
    
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0bcc65bde630e1f016925ee378fdeb99f8bec26a1088021bd4827e7c1948baf5
+oid sha256:c01b997b114dda246eeeef42669b61f49650d2058b8d90a07388593e3aa15128
 size 29849
