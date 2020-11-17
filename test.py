480e6018881d558bb1602e0cf9f324d840964d4d
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-11-17 19:17:57 +0100

    W&B logging bug fix (#1399)

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:44a5513c79fbba1e49942f25067f6883c79e29ac3afe798b884f0ca0be827344
-size 15386
+oid sha256:afcbed9f772bcc0fd1737f3ccc0233cef832e4932bc949b3ea32ee861d9ce6a9
+size 15400
