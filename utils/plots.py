9692bd54951c0c7b806385a586dce4b1e0c504ba
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-11-18 12:27:30 +0100

    Update labels.png with rectangles fix (#1432)

--- utils/plots.py
+++ utils/plots.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bedbc487fe787417794a8da27b0974f2912dd1f8ffed4de69c8e252f874649b6
-size 15839
+oid sha256:8c8a5b1de5ea6186adbf192cdd3e43b0ac78dab4cd6536319cea99ca160985c6
+size 15849
