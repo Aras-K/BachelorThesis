8c7af8249f48847abc273c24da855404c760134e
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-11-19 13:45:36 +0100

    Detection() device bug fix (#1455)

--- models/common.py
+++ models/common.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5c8c10997b61ca8e63c02bde5c6dcf65f29f66ff563574fa0a9bbd86f190c8c9
-size 10186
+oid sha256:2493ff0b93db50466bf10072a41c15a735f67bda15f5efb42099ef54e2ac1882
+size 10228
