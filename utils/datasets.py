1db44dba927f1d70c2ceaf7ae4686157c8a855ff
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-11-23 17:18:21 +0100

    Autosplit (#1488)

--- utils/datasets.py
+++ utils/datasets.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1fa0928111d844ad89307e219fbf1c9abd37ee2cfc968ae6db66fd68daa0ce62
-size 36857
+oid sha256:a6722b24a7151e21d7f6bfd7a48c5d59e9d6c8569283c2512b970f924cfdbba5
+size 37709
