efe4969cfa5c1f4924d26e0d3eaafb337a657b75
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-11-24 00:46:01 +0100

    Prevent PR plotting (#1489)

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5e9381a11a4fe088672844af1225368db3a303c196f7ea0b1404908976a5a7ce
-size 8043
+oid sha256:04b9360da275332b6b1fc901dcffca360ced9be737c513c425d977abdc5a038b
+size 8054
