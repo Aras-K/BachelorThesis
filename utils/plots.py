bcae7a6ebd1d6c6b47fa3d1b64678c021ebc0927
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-11-26 14:02:22 +0100

    Mosaic plots bug fix (#1526)

--- utils/plots.py
+++ utils/plots.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c7e00f237ba5416ff7ad2f74368d8a6a62f4fe9627b4ce7ec4b24936e5f0c407
-size 15432
+oid sha256:61091402fe6b4e3f472fe7bbfb8458fabe61d6aa88ff0f5d200ea4d182f14c96
+size 15571
