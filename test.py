a8fcee5f021a996fa74aafcb1e105109e0ea8e83
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-11-26 18:33:28 +0100

    Targets scaling bug fix (#1529)

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ee84c69476387ab53c310120d07a68666dfb076618588601c872c577355219ac
-size 15748
+oid sha256:0816694ae8c468fd3fefe7fcc795a24e28bf1df916bf448ed5151b1b3bbc3825
+size 15811
--- utils/plots.py
+++ utils/plots.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:61091402fe6b4e3f472fe7bbfb8458fabe61d6aa88ff0f5d200ea4d182f14c96
-size 15571
+oid sha256:aa0804331b6d7285b5e13912f595f8461148bc2587a9b84d50909021ae56922a
+size 15608
