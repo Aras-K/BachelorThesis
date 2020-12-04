f2ed72c90027ec52bdeb432c02829b9820c26241
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-12-04 15:06:33 +0100

    Add bias to Classify() (#1601)

--- models/common.py
+++ models/common.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2493ff0b93db50466bf10072a41c15a735f67bda15f5efb42099ef54e2ac1882
-size 10228
+oid sha256:2c98f1203bb187d5918c916b0a5b8b6aef3d1495e3d29b2fb09caaf49a79d514
+size 10215
