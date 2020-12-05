d6c65714f2423aec816be6aff6e2095fe204104b
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-12-05 11:41:34 +0100

    Increase FLOPS robustness (#1608)

--- utils/torch_utils.py
+++ utils/torch_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f5d80c1eeaf0499e39de80f3efe6771d77f3159d56fb98b20c8d3a44f5122fdf
-size 9564
+oid sha256:af5fbde01d05135f1147dcadf0fc647d8ab8b1491960b2f4e90c2b9c6d1a100f
+size 9600
