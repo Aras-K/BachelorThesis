a2f737a8724537d21888349293a6e7fddafa4fc4
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-12-06 17:53:38 +0100

    Hub device mismatch bug fix (#1619)

--- utils/general.py
+++ utils/general.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:618981bb52a720809bdcd2125095f44e70d4acde0bffaa99c63363a9d9fb41bb
-size 18199
+oid sha256:679c7521f9b50c5b4522c7129d578b4c1bd4125a10032991ed385523e8f556a8
+size 18224
