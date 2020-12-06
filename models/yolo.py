d6cd646578e0a5c71b8fecbff37ad94251839516
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-12-06 12:41:37 +0100

    Implement default class names (#1609)

--- models/yolo.py
+++ models/yolo.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7218d1f9ce0534546bc8281d0c22408a6c313d47e6bc1c78b3a0dc21dc7ae6e7
-size 12319
+oid sha256:e469e6901d14d08c0439331af6a8501f828f3159fbecd2aca60024d8b386e02a
+size 12398
