57e01442942af49daaa1abf330ac448dfaabfada
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-12-11 15:45:32 -0800

    Streaming --save-txt bug fix (#1672)
    * Streaming --save-txt bug fix
    
    * cleanup

--- detect.py
+++ detect.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eeae54e83759abbdbbf6799e27df578f6720b07652d3f932160043e04d333307
-size 8055
+oid sha256:faf88a0ead7bb46dc49a8e2550f4d091c1c1dafe6bd5eb78a59db66a7dc60a32
+size 8098
--- utils/datasets.py
+++ utils/datasets.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3f9aa3e21beb5046d0cbd92de8842320e759b62e8ab86969ef8bb72f96d928ac
-size 37995
+oid sha256:62ffe3d0257b739da07f3d9c5c09925516b7d83679f9ce79bf6570da42b61e4d
+size 37994
