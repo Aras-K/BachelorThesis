0bbdf759f7d3e627998b4947b99eb6ca5a28b2ad
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-12-12 11:16:57 -0800

    Simplified PyTorch hub for custom models (#1677)

--- hubconf.py
+++ hubconf.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:60f692996f53362245721068534e67e3a908f1adbde6bfe464be9bad995898dc
-size 3989
+oid sha256:6eaec4b7334f8117605f9393c7a0b472f14f19a795c962073a9cc5da7bf9d35e
+size 4804
