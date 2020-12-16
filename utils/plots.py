3cffdb3af2d446240ad5adf5a72c6a0b3c3f60fc
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-12-15 18:35:47 -0800

    Add idetection_profile() function to plots.py (#1700)

--- utils/plots.py
+++ utils/plots.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a79e51fa2d1bb6512086ba5d112134964b5b1bae7ff1fa398e3ba436abab4008
-size 15974
+oid sha256:47dafe36a14650cc0de2d5d0d6e396d8b736e8025b8130035f2c6dc53fdb493a
+size 17528
