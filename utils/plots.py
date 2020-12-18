83066bf8bbe0bc7f167f0102f9ad4214f41c1a82
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-12-18 01:19:17 -0800

    Update profile_idetection() (#1727)

--- utils/plots.py
+++ utils/plots.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:47dafe36a14650cc0de2d5d0d6e396d8b736e8025b8130035f2c6dc53fdb493a
-size 17528
+oid sha256:5cc2c2db5b9a8e4d57190ab53cb927ed637981f85fa1d3082d2152394601e0c2
+size 17535
