8912c5981a7bc62cf4cdd37ad1b3303e47174fb9
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-12-19 10:54:01 -0800

    Input channel yaml['ch'] addition (#1741)

--- models/yolo.py
+++ models/yolo.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bc165500f7a412ab7708a3eed7201fdd8b2c007b5d1e07ec6bc981f5524f8c92
-size 12398
+oid sha256:ba2d08db563d7017d6363ae6bfdd40c06517c59f8e050c5513ec53f0d06980ba
+size 12463
