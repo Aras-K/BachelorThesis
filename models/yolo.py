aad6c75dcc21305555c10bb423eb55385337df5e
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-12-22 17:27:40 -0800

    leaf Variable inplace bug fix (#1759)

--- models/yolo.py
+++ models/yolo.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba2d08db563d7017d6363ae6bfdd40c06517c59f8e050c5513ec53f0d06980ba
-size 12463
+oid sha256:2e6db82b9b14391b2de68af8c42fadf7270182d048f8fb12d57fd371d3463ab0
+size 12473
