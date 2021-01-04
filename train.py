24a28a5f28ecb2886ea4a14cd191056ddabead3d
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-01-04 15:49:08 -0800

    Create one_cycle() function (#1836)

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:29b5129c0ed435646ed4e041cfa5a4ae32af20da634787caa782417b4680ee1d
-size 31073
+oid sha256:d1ca188acb3787c7485e3da1f8fca53f1f2e7dede8fa1db84b9ded00f16aee02
+size 31066
--- utils/general.py
+++ utils/general.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0c413fab5c9f66a1b8b4dca7dd697f7e099695b013f1c06c6ffe37f5c3207997
-size 18414
+oid sha256:cc71bddda159a8f46b3b66bd1c2a8516b3c369e673870635ae9dfb870514587c
+size 18594
