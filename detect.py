748b9053cde8bb7c12bbaaf62c62b24445f6558e
Rohan Dubey <48961845+rohandubey@users.noreply.github.com>
2021-01-05 02:34:13 +0530

    Remove unused code (#1830)
    * Add files via upload
    
    * Add files via upload

--- detect.py
+++ detect.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1e67e8239af02b62f609f96c75061b87e28eb7fe72e167bf7ecdd01119f9ad8f
-size 8143
+oid sha256:26358d87005d4efcf9316b8bdb2a42b9a4ce7e1c85a51cdb8afa125d7d3c3b39
+size 8043
