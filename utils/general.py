3fc1897d0d4174bff87e5e042347348965eedd1a
Tommy in Tongji <36354458+TommyZihao@users.noreply.github.com>
2021-01-07 08:27:22 +0800

    W&B ID reset on training completion  (#1852)
    * Update train.py
    
    Fix the bug of always the same W&B ID  and continue  overwrite with the old logging.
    BUG report
    https://github.com/ultralytics/yolov5/issues/1851
    
    * Fix the bug of duplicate W&B ID
    
    fix the bug of https://github.com/ultralytics/yolov5/issues/1851
    If we had trained on yolov5s.pt, the program will generate a new unique W&B ID.
    If we hadn't, the program will keep the old code, we can still use --resume aug.
    
    * Update general.py
    
    * revert train.py changes
    
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- utils/general.py
+++ utils/general.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:019dcf7c5f8cf8ee48f2f6d0a28b3b441783b4e78820cdab685ed3e315a5727a
-size 18773
+oid sha256:26ac26dc1a0453100c237cef4002630030c23e61aa7b57b9f7462b4dd21b6ae3
+size 18796
