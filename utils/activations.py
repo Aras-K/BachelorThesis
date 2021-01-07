6ed5cd392ea175a2481626b2595fa725116abbfd
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-01-06 20:58:41 -0800

    nn.SiLU() citation correction (#1713)

--- utils/activations.py
+++ utils/activations.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b5d03b0411aac38dad2212cf9edabe4aeb98700285ee14e43c7bcea0ba247e76
+oid sha256:0b9b091d4a84054d2e1cde2938c0458b5c65c65d354d5da5d6216ed1c47f6387
 size 2248
