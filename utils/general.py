27e10b7612fad70b0f543b71f2789fd9361db8c4
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-01-16 01:15:38 -0800

    prevent check_git_status() in docker images (#1951)
    * prevent check_git_status() running docker images
    
    * Update general.py

--- utils/general.py
+++ utils/general.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a2acbb9d3b04723025be5d30bdf525cc937c2b6aa46be35a2a0c374c573c103b
-size 21045
+oid sha256:a3679efbbea62d0051cc97193ad1786d2bec2a68c196d1ef928acd94913bf060
+size 21082
