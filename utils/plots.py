ac90189d499fc348615b96b45cb5d09d6d09dfac
Abhiram V <61599526+Anon-Artist@users.noreply.github.com>
2021-01-19 00:16:20 +0530

    Update plots.py with yaml.SafeLoader (#1968)

--- utils/plots.py
+++ utils/plots.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c6c1519301a4564f2302b566ae960cb90675cea65ff5b6a4a331c6f8affca795
+oid sha256:51232173d7815d597c4767d687a0a9b41a84a379f4da63c1c41fcde3d689577b
 size 17301
