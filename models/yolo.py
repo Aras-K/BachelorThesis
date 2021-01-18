6c384054bb1d3d9a4611b1649c7f4bc6fc0c88fc
Abhiram V <61599526+Anon-Artist@users.noreply.github.com>
2021-01-19 00:17:00 +0530

    Update yolo.py with yaml.SafeLoader (#1970)

--- models/yolo.py
+++ models/yolo.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e923d1f4ef632403e88b44abe664f29ef99e64e1fe1660396c125640a8b8f917
+oid sha256:c781ee6d94a4ed650d9385225bce4b3581cbcecb4f4c1fd29ac4cf701a1391e4
 size 12562
