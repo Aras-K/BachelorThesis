407be72326bcbbbbc339e807d9f254750a4b98c6
Abhiram V <61599526+Anon-Artist@users.noreply.github.com>
2021-01-19 00:16:46 +0530

    Update test.py with yaml.SafeLoader (#1969)

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0eadfc059227c1c6877bccef1b8498fae4e1fdc6eecb74b7e00fe4f6bdf70350
+oid sha256:ca19e91b2c3260101e84ca2b1201547f2a85af0c3a32acb77f0d362c40b31bd5
 size 16274
