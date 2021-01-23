ed4c7a6c99c31f1dc5ae379ac6e35e0aa2188be7
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-01-22 16:38:06 -0800

    Update autoshape .print() and .save() (#2022)

--- models/common.py
+++ models/common.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fb5cbceb48ba1da8cc02e58f9c9af4d3eaa632545e0d24be1c73c4d3931e0a81
-size 12575
+oid sha256:1470771d682a1268d21465a8bf71d6d177f217c039c759a9f8f757f3a0dfac1d
+size 12642
