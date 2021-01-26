249cd6f3b30808b4499fc3513b01a448b0a1eaf9
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-01-25 20:55:35 -0800

    Check im.format during dataset caching (#2042)
    * Check im.format during dataset caching
    
    * Update datasets.py

--- utils/datasets.py
+++ utils/datasets.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a8a30395a1bdd5e1e834b8150142d3dc44ad0022f2219bbabfe1249c735fc881
-size 41615
+oid sha256:9049937dc19b8cb372dfadb3e38272cdeb43f5828d148dfc07e23bc9564ac16a
+size 41717
