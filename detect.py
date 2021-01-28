357e4022add9bf3c9e759a8ab05cc053447cc7d1
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-01-27 16:01:24 -0800

    Update run-once lines (#2058)

--- detect.py
+++ detect.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e713786cf23b759c102c3da42bc660ed8225b2c0918c792431bb3d4a810ac233
-size 8102
+oid sha256:5cb6cca43663de29b9f8f82653405f73c5cbbeabaaebb6327b8632caafcb3bed
+size 8075
--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1cb1e116a18ee8a992f13a5e7014ab32dff1a0ef456e57036feb999a1d3ad08d
-size 16301
+oid sha256:c8443c834ff410e8c69a1e0efe2ace12ae30d6372a9e940e057e73910bad2f21
+size 16274
