afffd2f410b5f18f87616894f4f92af9421e446f
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-01-31 13:58:49 -0800

    Update test.py --task speed and study (#2099)
    * Add --speed benchmark
    
    * test range 256 - 1536
    
    * update
    
    * update
    
    * update
    
    * update

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:278782d765fe7e23656fe6dc8acc241f752b0a3ffc39481a3d10784d07bfabbc
-size 16242
+oid sha256:7d91c2252120b982f858f0d4f87c42f2e0368ab091691d45e5b06d89dbbe087f
+size 16367
--- utils/plots.py
+++ utils/plots.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2364da03d35b165bb54c8de8b4681765a0934edb5d7b33c0ed3caa0d7bffc16c
-size 17349
+oid sha256:a90b185bed44c1864ea1d17d3e7aa6353089c63f6791139847e841e9139091f6
+size 17343
