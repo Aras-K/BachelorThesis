0408cdb7719c258b62aeafad1ac88fe77cc1dade
Transigent <wbdsmith@optusnet.com.au>
2021-02-10 18:16:38 +1000

    Update utils/datasets.py to support .webp files (#2174)
    Simply added 'webp' as an image format to the img_formats array so that webp image files can be used as training data.

--- utils/datasets.py
+++ utils/datasets.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8d7fbeb4beee69ad4645b44dae9fe59c5eda38cfcd4e9f0370dccdd25b0b0430
-size 42533
+oid sha256:41cd6113a4ac4a916ef9ff88b5714c39f77d0d5becf5b8e4deec9f649ab264ef
+size 42541
