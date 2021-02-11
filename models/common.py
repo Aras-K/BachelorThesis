b658efc7960a09540c2bb6c21647c0031d5423ee
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-02-10 16:10:43 -0800

    PyTorch Hub results.save('path/to/dir') (#2179)

--- models/common.py
+++ models/common.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1470771d682a1268d21465a8bf71d6d177f217c039c759a9f8f757f3a0dfac1d
-size 12642
+oid sha256:f893334b37f236f334ba25a24b93f9e2864f081d057cf096eda5c1330b59a7ec
+size 12729
