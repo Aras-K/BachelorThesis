614aa0c76bf9f13c271fb137503ff3bf332cc6c0
Yann Defretin <kinoute@gmail.com>
2021-02-19 21:38:05 +0100

    Unified hub and detect.py box and labels plotting (#2243)

--- models/common.py
+++ models/common.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e1efc251ae129942b4f1ff98c4cbcdfe1192c44e359f4871bb503d9128398757
-size 13031
+oid sha256:e6a649a5253edc9b3a3f3314113f486d4fd37e92020d379c0c0dd68d24649c77
+size 13008
