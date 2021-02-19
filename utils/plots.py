fd282d2b691d830188eb0fa48fbd6581b1e30c39
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-02-19 15:20:41 -0800

    reset head

--- utils/plots.py
+++ utils/plots.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e3cb939a3d04ee22e45e6803d109223307865860aced830debca479f1d530c52
-size 17427
+oid sha256:a0aeedb36ff35cccd701ea03c029f8d776711557fb2e50b0de2e5b4bd035dde9
+size 18129
