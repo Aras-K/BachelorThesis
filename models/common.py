4162b7ccf4d52aef73cdfbdd2cafca82e5308815
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-02-19 11:22:42 -0800

    YOLOv5 Hub URL inference bug fix (#2250)
    * Update common.py
    
    * Update common.py
    
    * Update common.py

--- models/common.py
+++ models/common.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:af4a36d958ae098d1882b5e574dd61611f2dd45d2021ce53c7c3e6e85e30913c
-size 12981
+oid sha256:9f3e638792f4865722f5176ab60cb116d1fee3ad0ba87844fca1c939c4583ee3
+size 13014
