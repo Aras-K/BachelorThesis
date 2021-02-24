b69c3085132dfe91d6469e3cacd638dbe004f0e0
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-02-24 01:43:59 -0800

    Amazon AWS EC2 startup and re-startup scripts (#2282)

--- utils/aws/resume.py
+++ utils/aws/resume.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:929ccd8886c1530c4d52229fcbb40f817db42418561431b757a10f165481cfb5
-size 1030
+oid sha256:9cfe4e116b31bac9d00ae4918f6c6b8bc1466f3561903e5811e3442062cb2a66
+size 1114
