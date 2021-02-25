3a0e767598455df5eb11123ee2b5f87ac50f6103
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-02-24 21:03:21 -0800

    Improved model+EMA checkpointing 2 (#2295)

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ca29dcea0302f52bef8e21e2998a3a34be498916419d04311c2bd8931dfd92b9
-size 16374
+oid sha256:b62c80b7a03f9109a4d288de2ceddea9c05e62e847f77dd1542671f0418062f5
+size 16408
--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bc15fd6a5b785f2775064b57e6430ea6e1371c3c1e378fef547d5c687e9b4068
+oid sha256:aa81a45a0e1feef0b7c8e722ed35918c8531f6d259f35f776b96bcc029b68ab9
 size 32088
