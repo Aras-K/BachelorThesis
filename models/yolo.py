fc8cdf41ff603c15adac1bfabaf47919461e89af
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-02-28 21:14:08 -0800

    remove TTA 1 pixel offset (#2325)

--- models/yolo.py
+++ models/yolo.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7ca7b872e4daded4745501eb640edc94da91e47db023156a73a9be66546b30a4
-size 11886
+oid sha256:fd5e6b1baf8777109f9a71c1838e082d7ba426cb83d7e96d32822f5941a47795
+size 11878
