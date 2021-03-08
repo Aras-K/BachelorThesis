65e5195326f27a017af2d097391d82f4d1290a16
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-03-07 20:18:30 -0800

    AWS wait && echo "All tasks done." (#2391)

--- utils/aws/userdata.sh
+++ utils/aws/userdata.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ceb74bc49c062757bb5463dab9be7176e3258433567557a1fb543ede7d01ae29
-size 1143
+oid sha256:f47ec29b34ea356337899032fd5e8303a6df31f553ea40b0949c99c4588bb25f
+size 1200
