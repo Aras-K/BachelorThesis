e0aec87397bbee4667e84797dc7a453bb6ff7a7e
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-03-09 21:07:27 -0800

    CVPR 2021 Argoverse-HD autodownload fix (#2418)

--- data/scripts/get_argoverse_hd.sh
+++ data/scripts/get_argoverse_hd.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fc4e7bbe41f61afac090923d47518c3fba221e8094a3358e6cde4401cc64318c
-size 1914
+oid sha256:290841d1c817813fb27d902e198b069bc4928d44106cffe28c80e45088261215
+size 2015
