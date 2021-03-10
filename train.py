d7712aa968451ff9ce3837f5708f7a4e6e3f1402
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-03-09 23:43:46 -0800

    DDP after autoanchor reorder (#2421)

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9a9eb50632199d6390bcb3c99165fb85e576fefc36b1732237620dbbe5f3e9c2
-size 32263
+oid sha256:1802f465d315270fd277d2c04a0e86308cca4a0eb85f4b5992acf29188ea55ff
+size 32262
