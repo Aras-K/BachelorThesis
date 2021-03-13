a30cbc1b332ab911263bc24332e5c44a89d8056d
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-03-12 22:08:42 -0800

    Update test.py --task train val study (#2453)
    * Update test.py --task train val study
    
    * update argparser --task

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:64002ae731a80ec2d09a7c484d5ff2ea84ce71f9cb2dc125369ea18bdf811e0d
-size 16539
+oid sha256:8f891c135fa3186ae6f04f691d61dce9c9644fec4fa16a9b892b3cbaa2d9f590
+size 16546
