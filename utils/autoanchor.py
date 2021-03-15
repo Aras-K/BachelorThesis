2538d8e343cf996239bb3e3a0b972385c3fb20f7
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-03-14 21:58:12 -0700

    Scipy kmeans-robust autoanchor update (#2470)
    Fix for https://github.com/ultralytics/yolov5/issues/2394

--- utils/autoanchor.py
+++ utils/autoanchor.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1565d82a13ffacf9c008253427e466f63c8f40472b3daf66dbdafd9bbd088ced
-size 6940
+oid sha256:e9a0f1017b018aafe498833e484bf30637ebda426be50374ff3b822ecf468c35
+size 7152
