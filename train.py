5286f992fc63b0ef6993de8466b29a2c705f0786
Ayush Chaurasia <ayush.chaurarsia@gmail.com>
2021-03-23 21:24:34 +0530

    W&B DDP fix (#2574)

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3cf0ccc9f939bc38bf570e1083657b92aa6e16e843e79ad5c9e685686fb41766
-size 33297
+oid sha256:d5340a0ecbc67f2feeda3bb61b00ad8561f89277e112b379698c4bdcdfcb1ee0
+size 33342
