58d839703ad56965e88aeb4e853fe9164b7ca8f1
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-03-24 15:42:00 +0100

    YOLOv5 PyTorch Hub models >> check_requirements() (#2577)
    * Update hubconf.py with check_requirements()
    
    Dependency checks have been missing from YOLOv5 PyTorch Hub model loading, causing errors in some cases when users are attempting to import hub models in unsupported environments. This should examine the YOLOv5 requirements.txt file and pip install any missing or version-conflict packages encountered. 
    
    This is highly experimental (!), please let us know if this creates problems in your custom workflows.
    
    * Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4050aa8121a4732abad73183921da0ee506e6056540db71a5792a7b5078bd9d7
-size 5432
+oid sha256:8450d92f3fcaaa20e41aec2290338bb481e27972ab38a2cc2a139610be7f7884
+size 5489
