961ff314269fc93bc08ecddefa8dff15f5983afb
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-03-24 16:23:54 +0100

    YOLOv5 PyTorch Hub models >> check_requirements() (#2588)
    * YOLOv5 PyTorch Hub models >> check_requirements()
    
    Update YOLOv5 PyTorch Hub requirements.txt path to cache path.
    
    * Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8450d92f3fcaaa20e41aec2290338bb481e27972ab38a2cc2a139610be7f7884
-size 5489
+oid sha256:babda9a03b77cb418dddfc41d7936dcb4bd5958ff61e73ed4192bb830bd615ed
+size 5533
