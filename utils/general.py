259be650c2cf17c301cfa12ad39d1a9dcbc3c1d3
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-03-24 01:05:59 +0100

    Enhanced check_requirements() with auto-install (#2575)
    * Update check_requirements() with auto-install
    
    This PR builds on an idea I had to automatically install missing dependencies rather than simply report an error message. 
    
    YOLOv5 should now 1) display all dependency issues and not simply display the first missing dependency, and 2) attempt to install/update each missing/VersionConflict package.
    
    * cleanup
    
    * cleanup 2
    
    * Check requirements.txt file exists
    
    * cleanup 3

--- utils/general.py
+++ utils/general.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5ec0f7b88cbe9cd4c2e79d1172ec76c776e80a252e8493dab0bfc7522f92f897
-size 23921
+oid sha256:0b027e4838367325bd1e3c530989eccff6a2c2c345d54a9e21ce2a1e95509f1d
+size 24327
