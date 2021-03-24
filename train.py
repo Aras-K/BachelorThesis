b53714472953083ae219032129b85e4551d20cb8
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-03-24 15:43:32 +0100

    W&B DDP fix 2 (#2587)
    Revert unintentional change to test batch sizes caused by PR https://github.com/ultralytics/yolov5/pull/2125

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e66dccccd266d9185f17510e4e5ae6dcb60091a4253a05b86ea6b5ea67976364
-size 33347
+oid sha256:0e8f3dc864f15321f5f9cc547eecb6121009693041da2feef81be2b404b299d4
+size 33345
