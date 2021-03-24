0e08b4847ee435cd67ac32086a80889c7455d9cf
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-03-24 16:42:54 +0100

    YOLOv5 PyTorch Hub models >> check_requirements() (#2591)
    Prints 'Please restart runtime or rerun command for update to take effect.' following package auto-install to inform users to restart/rerun.

--- utils/general.py
+++ utils/general.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b027e4838367325bd1e3c530989eccff6a2c2c345d54a9e21ce2a1e95509f1d
-size 24327
+oid sha256:a95047c40bf90a6e0a89713e7c59aacc1e978452975a8d3f8cebb5afbfe4a2d1
+size 24416
