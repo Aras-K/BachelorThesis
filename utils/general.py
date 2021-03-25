38490c8b6d97fd7c808855412e5efcd6eaaec4ae
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-03-25 15:12:49 +0100

    Update segment2box() comment (#2600)

--- utils/general.py
+++ utils/general.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:faf8aa44b57ba1c076aa4b67487dc9f4d6c8f88d92821a4b21d2a9200863b88d
-size 24765
+oid sha256:6c51127e3462bb8ee6fe5e58f51dfe0cc8bb83498420568c35eaf06d5518d348
+size 24760
