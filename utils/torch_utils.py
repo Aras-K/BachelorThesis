9e65a368d1975d21a1e67ea2dbe980f8cbbca9a9
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-03-26 12:52:47 +0100

    Create date_modified() (#2616)
    Updated device selection string with fallback for non-git directories.
    ```python
    def select_device(device='', batch_size=None):
        # device = 'cpu' or '0' or '0,1,2,3'
        s = f'YOLOv5 🚀 {git_describe() or date_modified()} torch {torch.__version__} '  # string
    ...
    ```

--- utils/torch_utils.py
+++ utils/torch_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ca5b64f77cda72be08f2469343b00bd27fc68595229eb38faeb88518a23ab3cd
-size 12105
+oid sha256:ed6edb4c94dbc58dccb16fb5df5dbb590d41c40ad0c29c31ec336e7e767c13f5
+size 12389
