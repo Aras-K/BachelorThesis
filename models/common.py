ac256b38b4e55812b141bd96117f7d0e6b349197
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-03-28 20:23:40 +0200

    PyTorch Hub amp.autocast() inference (#2641)
    I think this should help speed up CUDA inference, as currently models may be running in FP32 inference mode on CUDA devices unnecesarily.

--- models/common.py
+++ models/common.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:10336c2d7d9de3d75fff22f88f83303471d9b17f18edbc5a3aefeccd63bde3f0
-size 13454
+oid sha256:fa37712d3c4fe98614b110ef9b9e2ba82ad379d7d7838de22a7e88db725f7dca
+size 13551
