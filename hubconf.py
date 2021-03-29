9b8ed15a555f9e084ec7cbe9672efef02a1c38ad
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-03-29 15:19:07 +0200

    Speed profiling improvements (#2648)
    * Speed profiling improvements
    
    * Update torch_utils.py
    
    deepcopy() required to avoid adding elements to model.
    
    * Update torch_utils.py

--- hubconf.py
+++ hubconf.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e949d360b2270b0b4dbf285d1e545ee5bdf271063ab96f0ae60ba569f24ac3b0
-size 5709
+oid sha256:9441382fee7107583eee4c13b0867ee385825c478c7b381a1a50eb173c0f38f7
+size 5746
--- utils/torch_utils.py
+++ utils/torch_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d1824a9305c6fbaba14d5616f99c4b8945a81d409d7a2430d0cb673e603eb0cb
-size 12439
+oid sha256:8469a3c2ad8411b2d824e929863c8713142fb8bfe7684ed8da9405594da8728c
+size 12438
