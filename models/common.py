c286490d98ee05f25161a6d80fa9b458dd9c5add
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-04-06 13:18:56 +0200

    Updated filename attributes for YOLOv5 Hub BytesIO (#2718)
    Fix 2 for 'Model predict with forward will fail if PIL image does not have filename attribute' #2702

--- models/common.py
+++ models/common.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e6cf99e511e52eb58e89c3167f533d57191a144d74d262e8fc1ae1dbd24b53cc
-size 15477
+oid sha256:b0096f6b6e5c58208f5e17ed2ba3ed0535868e3c4d778d1d6592e96122018105
+size 15482
