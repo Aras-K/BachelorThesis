e975722199e66a6610336e4c713b41b8eb1fb614
Robin <robmarkcole@gmail.com>
2021-04-15 12:26:08 +0100

    Flask REST API Example (#2732)
    * add files
    
    * Update README.md
    
    * Update README.md
    
    * Update restapi.py
    
    pretrained=True and model.eval() are used by default when loading a model now, so no need to call them manually.
    
    * PEP8 reformat
    
    * PEP8 reformat
    
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

---
+++ utils/flask_rest_api/README.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2b6100f634c1d9d799204ced862ecc9cd3cfd64167220814a60f85000c9e03b0
+size 1436
---
+++ utils/flask_rest_api/restapi.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ec7578d0d59641916cf72bc8bcb9349b759d8fa1323c12d72ef222440a3fdd5e
+size 1070
