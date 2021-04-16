51460c76441833cf619e9cc6fab7e48ebc97ad45
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-04-16 14:03:27 +0200

    ONNX Simplifier (#2815)
    * ONNX Simplifier
    
    Add ONNX Simplifier to ONNX export pipeline in export.py. Will auto-install onnx-simplifier if onnx is installed but onnx-simplifier is not.
    
    * Update general.py

--- models/export.py
+++ models/export.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aa4fb38d17f86ccda6fcd83ea3918291f2410fd61c8ff9044a4ebb11d1d6f54f
-size 4436
+oid sha256:e914390642ad3ce247df2514617cb22d0f176560fd3b9272651cfbc1c58f3bfd
+size 4956
--- utils/general.py
+++ utils/general.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1a4833daa40e6968c1e696cf0bc4e5b861a06c870640b85e079096740a42f880
-size 25125
+oid sha256:d62a5759bf3f76117315b4d1c0b9f70bdbee202df868db4e8845494a94c4edfa
+size 25123
