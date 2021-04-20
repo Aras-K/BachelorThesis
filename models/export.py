7966a3e1af2600a7d01a512e7e7d5eb777045ddf
Tim Stokman <41363+timstokman@users.noreply.github.com>
2021-04-20 13:54:03 +0200

    Fix ONNX dynamic axes export support with onnx simplifier, make onnx simplifier optional (#2856)
    * Ensure dynamic export works succesfully, onnx simplifier optional
    
    * Update export.py
    
    * add dashes
    
    Co-authored-by: Tim <tim.stokman@hal24k.com>
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- models/export.py
+++ models/export.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e914390642ad3ce247df2514617cb22d0f176560fd3b9272651cfbc1c58f3bfd
-size 4956
+oid sha256:5b0cfbca56eaf8650b622088b57e63267b6ef0fd5210dbfc92fbc48c60493b67
+size 5613
