0284ec6592528cd56eff7df253dbcded219e71aa
JoshSong <Joshua_Song@outlook.com>
2021-04-21 23:50:28 +1000

    Cleanup load_image() (#2871)
    * don't resize up in load_image if augmenting
    
    * cleanup
    
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- utils/datasets.py
+++ utils/datasets.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:df2ce5ce068208de82517ce0d19407e414cd07fb78d76dfa8d10739cccfe9af5
-size 44594
+oid sha256:963b718b6096ef8b1c406e2b5bd6d58e471fa36df1591834510ee36edcf91927
+size 44534
