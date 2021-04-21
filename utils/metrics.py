02777f2b001f46fdf8dc5e14a85974836bed59b6
Michael Heilig <75843816+MichHeilig@users.noreply.github.com>
2021-04-22 00:49:55 +0200

    bug fix: switched rows and cols for correct detections in confusion matrix (#2883)

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e2baef50d5c4cad51517d9283c3bf021910840b10afc19b4f09ae1ec323aa395
+oid sha256:abe85ed919da850dc027b069fb6b6b6fc8c59d284379cff1a717fc246cfafedb
 size 8969
