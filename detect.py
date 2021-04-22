bd29b22e220ec3c2b4c1202f127c29ab4bc4fc29
fcakyon <34196005+fcakyon@users.noreply.github.com>
2021-04-22 19:17:30 +0300

    Explicit opt function arguments (#2817)
    * more explicit function arguments
    
    * fix typo in detect.py
    
    * revert import order
    
    * revert import order
    
    * remove default value

--- detect.py
+++ detect.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9b747f7f580a9a385cd8c38be3f6aeda18614f326b4a2d112cf6be2cbd1d9d32
-size 8895
+oid sha256:460dd4df9e24fdae56ac3cb7976f4dbb276eaad2a5d7df32c623c510f68206f1
+size 8912
--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:853e91c4802e7d38f4612cb989862030cba51a46c5e30e52dea014788c798905
-size 16951
+oid sha256:2ab7a75b5533af0108f0ebb5dabfdfd609f85960f8c4b549e0818bf975bda3cb
+size 17009
