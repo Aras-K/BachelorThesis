091bdbbeda78bc9101863a0a31a6dbe83f6044e3
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-04-23 21:21:58 +0200

    Default optimize_for_mobile() on TorchScript models (#2908)
    Per https://pytorch.org/tutorials/recipes/script_optimized.html this should improve performance on torchscript models (and maybe coreml models also since coremltools operates on a torchscript model input, though this still requires testing).

--- models/export.py
+++ models/export.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5b0cfbca56eaf8650b622088b57e63267b6ef0fd5210dbfc92fbc48c60493b67
-size 5613
+oid sha256:79bd5fe938acda4f248df78790b215143b1780e9c5cb79da394eb99d9a7894ac
+size 5774
