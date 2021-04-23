e246b2003dea4e165760b5756785c9ccccc7518c
Maximilian Peters <maximili.peters@mail.huji.ac.il>
2021-04-23 21:07:48 +0200

    Add detect.py --hide-conf --hide-labels --line-thickness options (#2658)
    * command line option for line thickness and hiding labels
    
    * command line option for line thickness and hiding labels
    
    * command line option for line thickness and hiding labels
    
    * command line option for line thickness and hiding labels
    
    * command line option for line thickness and hiding labels
    
    * command line option for hiding confidence values
    
    * Update detect.py
    
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- detect.py
+++ detect.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:460dd4df9e24fdae56ac3cb7976f4dbb276eaad2a5d7df32c623c510f68206f1
-size 8912
+oid sha256:40178170b2d32a262848e181e12aaa2dc8ffc0480475b4669dd161db0f63db0c
+size 9292
