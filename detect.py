b7c8b969206361c5f0ba243255ed0582c5381f16
albinxavi <62288451+albinxavi@users.noreply.github.com>
2021-04-24 23:28:02 +0530

    Change default value of hide label argument to False (#2923)

--- detect.py
+++ detect.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:40178170b2d32a262848e181e12aaa2dc8ffc0480475b4669dd161db0f63db0c
-size 9292
+oid sha256:fa723856e6777caa242091526f5dc4de9bd2d43167bd1f29bda89e4d0dd755f6
+size 9293
