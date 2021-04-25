478166215d164fbf1c1313807b7d8d2ced0c15ed
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-04-25 22:54:07 +0200

    ACON Activation batch-size 1 bug patch (#2901)
    * ACON Activation batch-size 1 bug path
    
    This is not a great solution to https://github.com/nmaac/acon/issues/4 but it's all I could think of at the moment.
    
    WARNING: YOLOv5 models with MetaAconC() activations are incapable of running inference at batch-size 1 properly due to a known bug in https://github.com/nmaac/acon/issues/4 with no known solution.
    
    * Update activations.py
    
    * Update activations.py
    
    * Update activations.py
    
    * Update activations.py

--- utils/activations.py
+++ utils/activations.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1abf0d3b688cfe539fd10e4055e33b12e18d77bbe81b048041185ec1d7ef9b02
-size 3528
+oid sha256:fbfeb09a5e4de0eb2d6df1e7582ea31b5a8da8ed2c4aec55764a1ac2f7c994ff
+size 3722
