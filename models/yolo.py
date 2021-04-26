d023617d9497944f8d86cf6ed34403b37329ddcc
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-04-26 23:58:21 +0200

    Improved yolo.py profiling (#2940)
    * Improved yolo.py profiling
    
    Improved column order and labelling.
    
    * Update yolo.py

--- models/yolo.py
+++ models/yolo.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e7790e0d917c0edcaad9bec75219a8dc1c9a124c3cb11e3d2b5ff16897b14f87
-size 12257
+oid sha256:dd5e58c56bbaf530c4d97c18bab705de4a3da9829b6fe07608e0c276afcb151a
+size 12406
