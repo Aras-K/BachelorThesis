547a7627bbc700ba0597cf9306eed8b54ceddb66
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-04-30 14:59:51 +0200

    PyTorch Hub load directly when possible (#2986)

--- hubconf.py
+++ hubconf.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:897c508b8df6cad2d6dbdf70a94840915b37a8ce5bf62f8fb3b092675f5b28bb
-size 5878
+oid sha256:b0b9bd34fafc47137d29d8942700df0e7792a2073970bec3b0c06aca6a83ec51
+size 6238
