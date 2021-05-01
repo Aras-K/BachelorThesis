ac403e79fc12ea1036c04d1ef7cc2a1e63f8ef86
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-05-01 12:37:41 +0200

    Add unzip flag to download() (#3002)

--- utils/general.py
+++ utils/general.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:884c12263b98fdb761125f9766ad574176425d0647e573d6a8cb5cf3aa740aed
-size 27368
+oid sha256:bdbdb35334d9cbdf1e2e662712ca006a67ae228d3e44646d4321e9787c3e8f59
+size 27390
