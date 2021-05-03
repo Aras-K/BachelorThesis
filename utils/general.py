f42e2700922eb618dcc3481f7b1710df6e2cb782
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-05-03 12:37:50 +0200

    Add is_colab() function (#3018)

--- utils/general.py
+++ utils/general.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:07a77ad68d9c0b5cec0b8ab634e61183de330573c3d9cf97d1c97401d20ca4c9
-size 27564
+oid sha256:154d633613cbd989c841558ca33de4d37d5e430ec8ca458bff94dc8ca5eb7eb1
+size 27822
