118d46049a8d79a0641e82fa079b7a4029b552ef
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-05-04 23:37:31 +0200

    download() ThreadPool update (#3027)
    * download() ThreadPool update
    
    * update train image count
    
    * cid + 1

--- utils/general.py
+++ utils/general.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc763ea5411eab5ee08107377594c81c71a47eacaaa09ebbf1594492a50b7461
-size 28152
+oid sha256:b8ebb354ca6a950a03530f0ea366b90ea1d97c7a8cdc2f49f6fb49dbb745b90d
+size 28213
