45ff8c68787dafba8f1fdb934c06599f69405ed9
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-05-04 02:25:14 +0200

    Objects365 Update

--- data/scripts/get_objects365.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:3364395cf5d5d12ce5de3f3ca05f5155be9b631d0f80bb2ff83dad1bfaad5308
-size 1947
--- utils/general.py
+++ utils/general.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f9d4598cd743d33800ee1877817a8f15ca202ef8abfd831f6ebb3c29fd6cf09a
-size 28049
+oid sha256:cc763ea5411eab5ee08107377594c81c71a47eacaaa09ebbf1594492a50b7461
+size 28152
