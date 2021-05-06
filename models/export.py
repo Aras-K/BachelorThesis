3e80fe4d663fa067999520f10ebe5df274dd03f2
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-05-06 17:23:33 +0200

    Do not optimize CoreML TorchScript model (#3055)

--- models/export.py
+++ models/export.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3b400551e0bfbe572a786548db7b77d607c2a3f89bd9f94360150837fb39fef9
-size 6258
+oid sha256:4b1b0c7c683606e7f5b30fd424b4249d9563c865db30a145745e1259804b1f2d
+size 6242
