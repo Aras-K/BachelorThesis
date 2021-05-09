01f862053b56908a41527b0d2fb32b192563a312
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-05-09 15:33:38 +0200

    Add check_python() (#3088)
    * Add check_python()
    
    Checks python version against minimum version of 3.7.0.
    
    * remove packaging dependency
    
    * refactor import

--- utils/general.py
+++ utils/general.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b8ebb354ca6a950a03530f0ea366b90ea1d97c7a8cdc2f49f6fb49dbb745b90d
-size 28213
+oid sha256:dbe5fd223c38e1e70ce6daa6a128e29322b3dc4780a00d299971423bf9b6620d
+size 28619
