2df9e4bb770a0c2b952e2c9b971a2d1a00c4a777
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-05-09 13:01:58 +0200

    Update detect.py (#3087)
    * Update detect.py
    
    * Update detect.py

--- detect.py
+++ detect.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:28cf688d1212c4408a0e713a54a246d09859d0a8386785fb796ae0d097d9420c
-size 9241
+oid sha256:fba8f806583d49bfaf1eef7918cdb20d97f94513dc7a6bb1c4f4e7fac2dd57a8
+size 9213
