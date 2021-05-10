ed6ebc2edc388de4dc8b9a61838e0a7e2ccd6529
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-05-10 16:39:37 +0200

    detect.py streaming source `--save-crop` bug fix (#3102)
    * detect.py streaming source --save-crop bug fix
    
    Possible fix for #3100.
    
    * () parenthesis

--- detect.py
+++ detect.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fba8f806583d49bfaf1eef7918cdb20d97f94513dc7a6bb1c4f4e7fac2dd57a8
-size 9213
+oid sha256:9386c31868417c4b1a6235ab8a2dcd846809371c60d732f139759d43dd105ef5
+size 9288
