d2649092b5233c48e7709ef73d1279008339fea9
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-05-12 20:18:32 +0200

    Scope imports for torch.hub.list() improvement (#3144)

--- hubconf.py
+++ hubconf.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:55c5b02c4425ed83ed773c005263b6b5984f3d30bece502bb73d3175658badb9
-size 5337
+oid sha256:e2ef3ecbff8158490e58573e40be55fdfb758ab934d023c768dcb474bf7d453d
+size 5361
