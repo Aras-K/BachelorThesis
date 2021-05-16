6a7e8dffbcc16efbd8ea35afc921dd71007c6a83
Christoph Gerum <christoph.gerum@uni-tuebingen.de>
2021-05-16 17:41:26 +0200

    Add `device` argument to PyTorch Hub models (#3104)
    * Allow to manual selection of device for torchhub models
    
    * single line device
    
    nested torch.device(torch.device(device)) ok
    
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- hubconf.py
+++ hubconf.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:802623b8729ea142060bae257bd5beefc7a6c18f51686dfe3a4fe3b8b958d66f
-size 5339
+oid sha256:0af1fd7857b034310c65ebe1e15522dc8b58c592f6623390c1bdf0b83a0bc727
+size 5638
