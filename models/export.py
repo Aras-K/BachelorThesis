7247c4cb12a78acd19f1894abdd513678aa2084f
Cristi Fati <fati_utcluj@yahoo.com>
2021-05-16 17:13:03 +0300

    Parameterize ONNX `--opset-version` (#3154)

--- models/export.py
+++ models/export.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:628a68e701ff74e07c605436220ee3656fe8f2a26b9c3d5096cc7bc7a37eabd0
-size 6840
+oid sha256:d83a38dda3a2dfb8653bb6c9b16b40a5ead1527ba95d73d2e25390025695be00
+size 6960
