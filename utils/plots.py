e03f0dcd99f03883bbbe576a591b2f7f1aa592e4
yeric1789 <76454253+yeric1789@users.noreply.github.com>
2021-05-19 13:47:36 -0400

    `plot_one_box()` default `color=(128, 128, 128)` (#3240)
    * Color can be none by default
    
    * `plot_one_box()` default `color=(128, 128, 128)`
    
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- utils/plots.py
+++ utils/plots.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9d6c6cd65807bead8fb18c4efc682f593cbf1aa3acbdfcc7bf1d5c96313aeb17
-size 18968
+oid sha256:7cace7d1afb10710b4a871023b89a02208c927cb7972f3d3a538de383752819f
+size 18889
