9c754402cb515a6391178d5d7a2cc0f71e9febcb
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-05-21 23:35:31 +0200

    Fix TypeError: 'PosixPath' object is not iterable (#3285)

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4ed77649cb8fe462cd98aed266f6dd832b8483d14a11766610ed2cbe7fe4add9
+oid sha256:fd7dab75df9b90f1ebeb46d42dac584d6e2ee5181d6d6ec7b8c3ebddd9a264b6
 size 33696
