5c2a9f8a7b5a2aa4da220e3979625255298f4821
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-05-21 16:51:07 +0200

    YouTube stream ending fix (#3277)
    * YouTube stream ending fix
    
    Properly terminates YouTube streams on video end. Should resolve issues #2769 and #3220.
    
    * Update datasets.py

--- utils/datasets.py
+++ utils/datasets.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:553d4486badccc49b9d38ce399bed6229a2c6508df17e46d22d0c915824f8479
-size 44877
+oid sha256:aa4a2006c3beddfa8a18e4e3e0b6695c2c655ea9a285fe975922d7b5c5c2dad1
+size 45038
