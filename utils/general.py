b9206ae0a946bf0d07a11b5ba2c54907eecd586e
yeric1789 <76454253+yeric1789@users.noreply.github.com>
2021-05-23 10:00:55 -0400

    PyTorch Hub `crops = results.crop()` return values (#3282)
    * Changing save_one_box 
    
    Made to work with other changes to common.py
    
    * PEP8 and single line BGR
    
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- utils/general.py
+++ utils/general.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f5ce76a99af0d5b420f4d5ffaa91707374567f6138cc6e5f0b5465c4158f360d
-size 28572
+oid sha256:8819e5626ccd101a3c560c90a03660b21d110f1e1ad9c7a9f322aa1a59fbe690
+size 28617
