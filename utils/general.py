6ef96246515ed81ddffd6eec8852e69fec606d1c
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-05-23 18:00:23 +0200

    Improved check_requirements() robustness (#3298)
    Add try: except clause on missing requirements install to catch install failures. Prompted by Ultralytics YOLOv5 API failure on Cython.

--- utils/general.py
+++ utils/general.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8819e5626ccd101a3c560c90a03660b21d110f1e1ad9c7a9f322aa1a59fbe690
-size 28617
+oid sha256:229a39dc6215f6117477fce2dd58fca604bf422b8853312e14c0b8612ee88da8
+size 28712
