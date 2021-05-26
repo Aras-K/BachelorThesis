36601b8ec5670f5649339727ace25b74a7f1edec
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-05-26 15:51:49 +0200

    Add URL download to check_file() (#3330)
    * Add URL file download to check_file()
    
    * cleanup
    
    * pathlib bug fix

--- utils/general.py
+++ utils/general.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:229a39dc6215f6117477fce2dd58fca604bf422b8853312e14c0b8612ee88da8
-size 28712
+oid sha256:2d6c49e8e67a746678846cf0448cd5107a272c695e06f9f18496c7bcd76ac1c6
+size 29130
