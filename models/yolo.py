8beafd43deecf151a7319c13227be483d98f4208
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-05-26 12:06:08 +0200

    yolo.py header (#3347)

--- models/yolo.py
+++ models/yolo.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7855999b1186b7ced151a8a3f6a55e940032458d0e5cb0523417205efde33440
-size 13635
+oid sha256:3d3d9ed97f5f654c6a80acc869cb082c01e5613ed14648dd64c6841dc5b2980c
+size 13698
