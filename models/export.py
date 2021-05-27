5a484c486730ef3eb04226ebfc6e9991c94dfce2
WangChaofeng <syusuke0511@163.com>
2021-05-27 20:10:14 +0800

    ONNX export in .train() mode fix (#3362)

--- models/export.py
+++ models/export.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a3bee3dc9bbdc4891ea7893042bcc73d1379c22fe847f36e488ce37407c366d1
-size 7078
+oid sha256:6cb0c68578702dbec54c6a692b85226c7db97cb9e610496785f4b312be35c00e
+size 7263
