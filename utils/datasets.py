d6147fb1ea4198bd3dad460e81d9bec7e9b5ab23
tudoulei <34886368+tudoulei@users.noreply.github.com>
2021-05-30 03:12:01 +0800

    `cv2.imread(img, -1)` for IMREAD_UNCHANGED (#3379)
    * Update datasets.py
    
    * comment
    
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- utils/datasets.py
+++ utils/datasets.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6353d86d789286b46547bcff192d817e6d3a9c3cd1e5952fd38f6e2371f517f2
-size 45205
+oid sha256:5b35c04dad18c8e69917720753028cc7a9b94e1c9df1907d47b16a588c1ac16d
+size 45234
