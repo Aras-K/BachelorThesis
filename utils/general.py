347142c0a2bf654f231b5031ab02a4d341902d36
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-06-04 15:23:33 +0200

    Add `timeout()` class (#3460)
    * Add `timeout()` class
    
    * rearrange order

--- utils/general.py
+++ utils/general.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:da43d91edcfb9ed62a67b9e1e4abb37ef87f1ca14762439d61979e872efc1b30
-size 29414
+oid sha256:b367fdfca55ee024a5cab2ef00b383c3a6e465a0aedac4f5a1b0e87ee6f1ce2e
+size 30307
