ddff3c683e364ab0c5b4978ccd5a2363fc7f0bfc
Sam_S <SamSamhuns@users.noreply.github.com>
2021-06-05 00:01:58 +0400

    Add `output_names` argument for ONNX export with dynamic axes (#3456)
    * Add output names & dynamic axes for onnx export
    
    Add output_names and dynamic_axes names for all outputs in torch.onnx.export. The first four outputs of the model will have names output0, output1, output2, output3
    
    * use first output only + cleanup
    
    Co-authored-by: Samridha Shrestha <samridha.shrestha@g42.ai>
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- models/export.py
+++ models/export.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6cb0c68578702dbec54c6a692b85226c7db97cb9e610496785f4b312be35c00e
-size 7263
+oid sha256:38edcda1cffad2a68f93f8e58792143d57c229916d1e1d2439ea04570c8349a9
+size 7413
