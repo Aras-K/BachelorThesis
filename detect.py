a19541e13912e5ece2caaab7879f0d6fdfc629f0
edificewang <609552430@qq.com>
2021-06-05 04:28:34 +0800

    Revert FP16 `test.py` and `detect.py` inference to FP32 default (#3423)
    * fixed inference bug ,while use half precision
    
    * replace --use-half with --half
    
    * replace space and PEP8 in detect.py
    
    * PEP8 detect.py
    
    * update --half help comment
    
    * Update test.py
    
    * revert space
    
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- detect.py
+++ detect.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2ee65e1334e3d86e2d2d31bc4e2f61d28d589d92aaf51172dc2cf7a640e2d504
-size 9402
+oid sha256:62d36f9c694595f69dbeb623fc0b735337c37ddc6bf898c92cc60c0dccba48e7
+size 9517
--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7167b863597b7f5a633228cc6e1d98d5b382982c4da7a430130055380c996a8e
-size 17048
+oid sha256:521f8784cfb8d08d17e1741caa7a067e1ad6a78e4eb5aa32f94dd1154e2a0b5b
+size 17188
