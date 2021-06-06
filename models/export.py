a80ac21c18bf7881ff9b78999c29185431caa3d9
Yonghye Kwon <developer.0hye@gmail.com>
2021-06-07 01:39:36 +0900

    Explicit `model.eval()` call `if opt.train=False` (#3475)
    * call model.eval() when opt.train is False
    
    call model.eval() when opt.train is False
    
    * single-line if statement
    
    * cleanup
    
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- models/export.py
+++ models/export.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:38edcda1cffad2a68f93f8e58792143d57c229916d1e1d2439ea04570c8349a9
-size 7413
+oid sha256:b0604bfff10e79f219fdf75776bc7f7f5bda30ad6fe85602fa77d535d0d956e9
+size 7421
