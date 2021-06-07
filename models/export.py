0e92f5e93045e927194f833f01b50a1787d1a7a0
Yonghye Kwon <developer.0hye@gmail.com>
2021-06-08 01:56:41 +0900

    Earlier `assert` for cpu and half option (#3508)
    * early assert for cpu and half option
    
    early assert for cpu and half option
    
    * Modified comment
    
    Modified comment

--- models/export.py
+++ models/export.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b0604bfff10e79f219fdf75776bc7f7f5bda30ad6fe85602fa77d535d0d956e9
-size 7421
+oid sha256:cab493068731bbd0d57d7a13a3dbcc833d8464aa17404b68df6ada912b5d361e
+size 7407
