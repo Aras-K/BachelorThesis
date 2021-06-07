05210a3096a13eacadd53a62a3ddb83d38b40487
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-06-07 23:21:34 +0200

    Reduce test.py results spacing (#3511)

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:521f8784cfb8d08d17e1741caa7a067e1ad6a78e4eb5aa32f94dd1154e2a0b5b
+oid sha256:c960a6c79bc60b8267d1438ceb3bfc95be0b07f82a6fd1d07b1cae3cf44ffeac
 size 17188
