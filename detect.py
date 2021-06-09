38cad7ffaf7100cdf3ead41404fa61ae21880ad3
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-06-09 22:19:34 +0200

    Refactor detect.py arguments (#3559)
    * Refactor detect.py arguments
    
    @SkalskiP @KalenMike
    
    * unused ok
    
    * comment arguments

--- detect.py
+++ detect.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:13500cef22a50b88f7c91af28b078c3e053938f01e4f3c1f8bbb6aaa7cbd43f6
-size 9499
+oid sha256:6d7851d3e3e9ab226c233a90442c2c367ab8938a2e5ff7e92dfaf61ad483e3b8
+size 10412
