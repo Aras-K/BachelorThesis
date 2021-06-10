6ddd219c8fe8da6d889eeae51a23723916a5a9d4
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-06-10 15:35:22 +0200

    Update export.py, yolo.py `sys.path.append()` (#3579)

--- models/export.py
+++ models/export.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3c43509681d7e4a28a096314ed34c8eb15111601aeaf19076cc063c47ba96a1a
-size 7913
+oid sha256:b5a63957c3549c5f733b784472811fb45a6f782448ce4ce6a72be9c6e98b0e6b
+size 7918
--- models/yolo.py
+++ models/yolo.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2d8796f8c3dd84a0320743f58d90968d9ab7e5bfd8840a51606e39f118ae8c48
-size 13698
+oid sha256:78f176e0ad0f49a20d3d33affd729fcd4736dd3809669cfc8c5ac27812ae2bcb
+size 13682
