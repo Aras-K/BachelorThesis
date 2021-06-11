729254658222e911d0535febe7ca198e27869c5b
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-06-11 11:37:08 +0200

    Add ConfusionMatrix `normalize=True` flag (#3586)

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1e02da4dca784960b0105954e266bb0253c9c896b08f051976b8898389134474
-size 9156
+oid sha256:f6aed69d2ac6f9f40c5ee4be4f989f2f00061e654e8d3bac57e65fde46ab5758
+size 9222
