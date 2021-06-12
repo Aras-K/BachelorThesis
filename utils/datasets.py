105cbe80cf32d79d6ce8de7159040a260f2b6626
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-06-12 13:26:41 +0200

    Update `dataset_stats()` (#3593)
    @KalenMike this is a PR to add image filenames and labels to our stats dictionary and to save the dictionary to JSON. Save location is next to the train labels.cache file. The single JSON contains all stats for entire dataset.
    
    Usage example:
    ```python
    from utils.datasets import *
    
    dataset_stats('coco128.yaml', verbose=True)
    ```

--- utils/datasets.py
+++ utils/datasets.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:384bd1dcea50dfe037b6a27c4f8500222ee49063a2c2ab3e2a413a8b3b8caa10
-size 47162
+oid sha256:c59689fc07d178bfbbe8245beeba6a58abab86db01ef3286b985580c1a1f8971
+size 47634
