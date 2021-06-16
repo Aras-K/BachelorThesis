2cbd3190856db4049cbc38e6d29466c60b0927b0
Mai Thanh Minh <thanhminh.mr@gmail.com>
2021-06-17 03:56:16 +0700

    `is_coco` list fix (#3646)

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9b334ef2418781413b75accf4015400bbf2c5c5ae8284d169d05c97b9d4962ce
-size 17595
+oid sha256:c76ff433b69b483ab03ff74c17beaaab0a77d9981d497b2f799de45bd3c48543
+size 17624
