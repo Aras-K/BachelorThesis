59b269f4e1a5340a3c64ff4239a00c114929d999
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-06-16 11:12:15 +0200

    Update `verify_image_label()` (#3635)

--- utils/datasets.py
+++ utils/datasets.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:51fa83519133146f1ba472913c6f22cb4ee6f058502e981ab90ac3d89ca70a41
-size 47628
+oid sha256:c43d1f602a3942395b1d1dc6b31c12a070ed3a73bf677c87fe07f0cce48cce6b
+size 47635
