30fb7ae534d524ae9897206718529cf1f2ed60d7
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-06-17 13:59:52 +0200

    Update `dataset_stats()` to list of dicts (#3657)
    * Update `dataset_stats()` to list of dicts
    
    @KalenMike
    
    * Update datasets.py

--- utils/datasets.py
+++ utils/datasets.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7b7825ea807992837c9969350e2889441b141424f1dd6c4fb4ef2015ecc8ed15
-size 47822
+oid sha256:3c5158c5346f59b9d22ce6835ba57ba7a73ee2a9e7b1717baf3e246e9db1768f
+size 48056
