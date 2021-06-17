2b5ada48231be5c6c8e456544f4fa948307133c2
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-06-17 22:12:42 +0200

    Update TensorBoard (#3669)

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0619d7cea5f8329e76e6383c124ba0b177ad157a8d6d4f2bbca495894b8700d6
-size 33710
+oid sha256:7bf036e1be6c0cd32a46f00f16cf93110b7e569d330607897071bc9cb68184d5
+size 33645
