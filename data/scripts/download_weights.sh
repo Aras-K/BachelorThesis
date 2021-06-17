23621493161a9ef89405e480e52110d9f22b20f7
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-06-17 16:21:10 +0200

    Update download_weights.sh comment (#3662)

--- data/scripts/download_weights.sh
+++ data/scripts/download_weights.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f4e0de560abc2ea9e2d41e50e0d0242813c64b05aa51ed85279e0ca8316af1fb
+oid sha256:0217d6837620aad2781a8eb0a35796099b96145292aa5213c986bcd0c1828850
 size 277
