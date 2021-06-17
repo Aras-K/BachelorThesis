d3e66d7d5cc4fb393a17a811662a20928624c174
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-06-17 22:03:25 +0200

    Update `train(hyp, *args)` to accept `hyp` file or dict (#3668)

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1963a44dc659b096694d866b352a40bef3589b37381f3cb12bcbda155a4b843f
-size 33543
+oid sha256:0619d7cea5f8329e76e6383c124ba0b177ad157a8d6d4f2bbca495894b8700d6
+size 33710
