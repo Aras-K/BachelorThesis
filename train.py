55bff737c493585105c6bf3581403f2f6bd14cf9
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-06-19 19:14:59 +0200

    Eliminate `total_batch_size` variable (#3697)
    * Eliminate `total_batch_size` variable
    
    * cleanup
    
    * Update train.py

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f8064238a39b24aa6162f3b236840f38a5dffb476486b014aaa77fd9f874246e
-size 33692
+oid sha256:0130d5536f88f1a9b825f1420d65c9b17fc94eb676c99208ecec9ab16905fe71
+size 33587
