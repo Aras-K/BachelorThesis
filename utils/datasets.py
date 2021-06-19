b045b4e47e0b788aef30d71875b301ce4f54f129
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-06-19 11:22:09 +0200

    Update cache check (#3691)
    Swapped order of operations for faster first per https://github.com/ultralytics/yolov5/commit/f527704cd32c42bc0bba9cce04601783b8563204#r52362419

--- utils/datasets.py
+++ utils/datasets.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:15949858df443baa6040d2e7089b5e7618083995fd37c16ef1a4cfe15a055188
+oid sha256:57a3dedb577a7e916dacf667ae03dadf42c929b7c79b8081460614de5fc2e887
 size 48348
