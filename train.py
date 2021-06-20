cdebc957ec0894bc4cd605481677844dac09fa98
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-06-20 15:06:58 +0200

    Add `train.run()` method (#3700)
    * Update train.py explicit arguments
    
    * Update train.py
    
    * Add run method

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:08d12e1c8c8c16b05671bc2e8c9bc638fadc4bb85db1e60b4b6c6cacfef02144
-size 33819
+oid sha256:d0eab75c3b4a4082c21894341a49981f8c82d4eac15ee37c4720ccdaeabcf17c
+size 34107
