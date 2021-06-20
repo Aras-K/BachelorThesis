94a842a0cb0db765289bd8d30cb11a144591ad0f
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-06-20 17:15:42 +0200

    Update DDP backend `if dist.is_nccl_available()` (#3705)

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d0eab75c3b4a4082c21894341a49981f8c82d4eac15ee37c4720ccdaeabcf17c
-size 34107
+oid sha256:3fb00321d478a267a4a8903e92978556f4dc90eb51c617f0bb4c407f66691233
+size 34147
