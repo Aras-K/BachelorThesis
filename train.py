c4f5141a3b749486f87122ac047f41e59abfc611
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-06-23 12:49:38 +0200

    Create `data/hyps` directory (#3747)

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8a1ab304402710ad3e520721407d80c7cc3b514fed59ca0f6e8d8a68ab475d75
-size 34462
+oid sha256:17f9f517f981a14348e4a9757fd415ca06e8589ca626642b171c155eceab2cb0
+size 34467
