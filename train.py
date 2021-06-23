9683cc1782c803e6d281ebeb87ebfe3b3b2357f7
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-06-23 12:56:22 +0200

    Force non-zero hyp evolution weights `w` (#3748)
    Fix for https://github.com/ultralytics/yolov5/issues/3741

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:17f9f517f981a14348e4a9757fd415ca06e8589ca626642b171c155eceab2cb0
-size 34467
+oid sha256:99c39f2a34d0ab366b0485bfcb2195759aa93e8af9305ab40cac9c9064ffea2c
+size 34484
