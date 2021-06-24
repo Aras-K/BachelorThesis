c24fb52a60c4df057b3c1bff0bd7055df87e5e1c
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-06-25 01:55:53 +0200

    COCO annotations JSON fix (#3764)

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:63872cbb28ceabc1e3e87ac0c5e9548e9891a8e1167cbc560cdc113720d3ec57
-size 18000
+oid sha256:112f1b144ff971c5095a9e0a048cf54a0e48802ec6d42a8f68441984bd5fe36d
+size 18033
