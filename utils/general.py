7670a5e802eca5ecae2e4147110ff5f46642d5d3
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-06-26 00:49:05 +0200

    NGA xView 2018 Dataset Auto-Download (#3775)
    * update clip_coords for numpy
    
    * uncomment
    
    * cleanup
    
    * Add autosplits
    
    * fix
    
    * cleanup

--- utils/general.py
+++ utils/general.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c6cb36f01ec78e0598b4c53752cb86e4a0bb1cb5b5d17f15ce7422a978212e73
-size 31373
+oid sha256:6c3820696390c903bc62f3a7b827c98aae6fb608eade6982e3522c1306fe7bcb
+size 31792
