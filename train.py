dab184e3584caa661d0f6f4e3c5ef2f4126a1417
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-06-25 12:52:05 +0200

    Remove DDP MultiHeadAttention fix (#3768)

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a612de4f9e3ebea9a7cefffb4a0114825dba654f56381132b72b17ae36c493da
-size 34489
+oid sha256:ecf70b3cf5cf4e25d151ae67a2eca932ce91d9a83298fe34e44e967f177057d6
+size 34253
