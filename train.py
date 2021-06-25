90ff9f5e6e606551672c10bec808aadf4e6154a7
Piotr Skalski <SkalskiP@users.noreply.github.com>
2021-06-25 16:16:18 +0200

    fix/incorrect_fitness_import (#3770)

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ecf70b3cf5cf4e25d151ae67a2eca932ce91d9a83298fe34e44e967f177057d6
-size 34253
+oid sha256:6f85dc498a80acefa07f19380cdaeb0e7ce9cbb36ce31f7a1641fe1052a15f9f
+size 34278
