fba91dda7780dc6ae15fad42bf347b818fef63bb
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-06-26 15:42:40 +0200

    Update seeds for single-GPU reproducibility (#3789)
    For seed=0 on single-GPU.

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6f85dc498a80acefa07f19380cdaeb0e7ce9cbb36ce31f7a1641fe1052a15f9f
+oid sha256:49cafdb72dc5a03c2d1674ce69f24b0f4fc8c45cbc8ceab252bb0098e30dbb2a
 size 34278
