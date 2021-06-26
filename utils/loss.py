2376711fb0365195892db7c38a9021ced83993d1
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-06-26 14:45:53 +0200

    Objectness IoU Sort (#3610)
    Co-authored-by: U-LAPTOP-5N89P8V7\banhu <ban.huang@foxmail.com>

--- utils/loss.py
+++ utils/loss.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e194ee7ce7d392764e77cb2ab2a784468209d8661b8af8dafdefa33b285f653c
-size 9460
+oid sha256:a6e453dcabb138dc83fcb1d7abe95b752d65ff0cadd18ba2f2f67e59e49c24e7
+size 9660
