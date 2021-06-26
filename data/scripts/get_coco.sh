701e99ff42383bb19097aed501902cac061af79f
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-06-26 15:29:16 +0200

    Fix datasets for aws and get_coco.sh (#3788)
    * merge master
    
    * Update get_coco.sh

--- data/scripts/get_coco.sh
+++ data/scripts/get_coco.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ef84016dbc7901755c735b12042b590e6711164186e38b13ca0e2543e5d8e323
-size 962
+oid sha256:fc475ba4b3c2f1af6d0f83afe2b9b42bf51c793c4eb2c877029c76e627abccd0
+size 979
--- utils/aws/userdata.sh
+++ utils/aws/userdata.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8c3f8d6734991233160512f51ed98857d340367cfaac433e489422584acd2ca1
-size 1300
+oid sha256:290b2bb741652eb2128aed15bb9aa8554d4521d6b8b235850beda76cbd63bbae
+size 1247
