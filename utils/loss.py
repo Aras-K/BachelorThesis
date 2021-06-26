8c68c0e4efa94bd4de626b939001e49f8b98bade
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-06-26 14:52:18 +0200

    Update objectness IoU sort (#3786)

--- utils/loss.py
+++ utils/loss.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a6e453dcabb138dc83fcb1d7abe95b752d65ff0cadd18ba2f2f67e59e49c24e7
-size 9660
+oid sha256:33245f8581123430ccb777ab5c46d20aec589d36b9fba2f8148c920e9d2e5093
+size 9740
