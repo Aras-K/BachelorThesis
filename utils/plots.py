b2cfd83e7233f15817f23ad16ee54d418b816f58
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-06-28 13:48:14 +0200

    Update `feature_visualization()` (#3807)
    * Update `feature_visualization()`
    
    Only plot for data with height, width > 1
    
    * cleanup
    
    * Cleanup

--- utils/plots.py
+++ utils/plots.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:29a9febfc3c1cfc8154c74fc51ea8a3f9e7a939eaa13cc4ddf6f3067779a1d6a
-size 19975
+oid sha256:9420f69d01894cd630dea1011d7402a42cfd88cedebf4ae749815a6b4e1af1cb
+size 20104
