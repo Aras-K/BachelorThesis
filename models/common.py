4a693b561449c03016cc5686084d231be9a30190
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-06-29 16:03:10 +0200

    Concise `TransformerBlock()` (#3821)

--- models/common.py
+++ models/common.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:79607ab82a95544fdac9dd15c7f3475144afc444fddba212f1c2a5d273cacb19
-size 16870
+oid sha256:213ac50d6433d139d4508471f980618af34fd984f1049070edd953b134c89616
+size 16743
