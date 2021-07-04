a1ad09d272d68678aa8cdf8c7500750ddd4eae0b
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-07-04 20:12:32 +0200

    Improved BGR2RGB speeds (#3880)
    * Update BGR2RGB ops
    
    * speed improvements
    
    * cleanup

--- utils/datasets.py
+++ utils/datasets.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7b054d71d8b84d7ff1705bace2de7b12f1528b2721524f8fdf71c49d32fc8370
-size 39415
+oid sha256:d97e8eb2e83dd59f8395ffc5d82395efeab29d7d80ef45e789dcaeeafbfd7c33
+size 39372
