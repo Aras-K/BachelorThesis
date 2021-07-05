8d136ed8000977021fd86ad4106c74f17c16a7a7
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-07-05 16:20:46 +0200

    Hub models `map_location=device` (#3894)
    * Hub models `map_location=device`
    
    * cleanup

--- hubconf.py
+++ hubconf.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aa0ef7deb6353e237b6cce7df2792f4bdaf57ce7ed913479ccc1ea724289c59f
-size 5621
+oid sha256:23fd406f6a31765f5a24a7fef07f98ee2405cf5475528bcf124ec0fe89c38706
+size 5584
--- utils/torch_utils.py
+++ utils/torch_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:64057307be6d4a1daa616245c984fd9ab4025abd712ce2121968d00f5014cad3
-size 12736
+oid sha256:e4db61c32f2ba3b70d174e14d991dcdd3d019836cb38a63b1b1631f5216949d8
+size 12820
