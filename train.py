bc71920f90bdaa1c4b1ab0bcd1414db7d23f8062
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-07-05 12:48:27 +0200

    Evolution commented `hyp['anchors']` fix (#3887)
    Fix for `KeyError: 'anchors'` error when start hyperparameter evolution:
    ```bash
    python train.py --evolve
    ```
    
    ```bash
    Traceback (most recent call last):
      File "E:\yolov5\train.py", line 623, in <module>
        hyp[k] = max(hyp[k], v[1])  # lower limit
    KeyError: 'anchors'
    ```

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:55fc20e7e9c8efa8330e6b0114ea065f0a2f5754add2d14b7c73cea747efd0fd
-size 34466
+oid sha256:1a42841b891a76d481b4da3095bfce560358d4bd980fd96d816c19137c7087c5
+size 34571
