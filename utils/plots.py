b5d7af3d4fe8ddd44a68cc346f637ec19f0f7790
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-07-07 16:23:31 +0200

    `feature_visualization()` CUDA fix (#3925)

--- utils/plots.py
+++ utils/plots.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b1cef7a82a8331c075e13e4d560d320d60237a86cae9dd0d1be0cbfb18f42e86
-size 20083
+oid sha256:09b331b5f9fde37efd425d275f5777146eb507c26e326597675edf3a195e128a
+size 20089
