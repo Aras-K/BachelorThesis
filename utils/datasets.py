7b76f8e8e8b87bf219c7e5f2d93704734b8f535c
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-07-08 11:42:30 +0200

    Update `dataset_stats()` for zipped datasets (#3926)
    * Update `dataset_stats()` for zipped datasets
    
    @KalenMike
    
    * cleanup

--- utils/datasets.py
+++ utils/datasets.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2efac2be82b98d5c1d611af1cdfa870434cefa92afc41420f12f6c4ef0ab37b4
-size 39480
+oid sha256:13563ae149372fff8ca1943f12fed2229195809bdae2637a204ce2222b02b481
+size 40283
