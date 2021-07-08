4292eb07182c3f947f4a9db88203460a735cb245
Eldar Kurtic <eldar.ciki@gmail.com>
2021-07-08 15:29:02 +0200

    Fix inconsistent NMS IoU value for COCO (#3934)
    Evaluation of 'best' and 'last' models will use the same params as the evaluation during the training phase. 
    This PR fixes https://github.com/ultralytics/yolov5/issues/3907

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1a42841b891a76d481b4da3095bfce560358d4bd980fd96d816c19137c7087c5
-size 34571
+oid sha256:dbaebe0236e8789553b1dff80bd9d6769ee06d86141fd0a762b3ac31d12272e8
+size 34448
