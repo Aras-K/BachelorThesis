fa740e5e9f1b497d13ac13faf3933a9358dd1e17
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-07-10 19:50:53 +0200

    Numerical stability fix for Albumentations (#3958)

--- utils/datasets.py
+++ utils/datasets.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:25a871d6de5caae0e6490bd7e898ffe28f91e05f34cd7113547f11f919fd37fe
-size 40258
+oid sha256:7e5b09775f95ca2ebbcbaab6cc7e9d4114a947a80e65c9b029bd5212e4261438
+size 40252
--- utils/general.py
+++ utils/general.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e95a908aa8b1f646ddcb2d96906250139e812c9901ae9e1f4fe4b076a9951ab6
-size 28575
+oid sha256:5d4c77f12de8892c883904471a775f96ebbf274929944badc939956af5032ebf
+size 28500
