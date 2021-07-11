b4e010ad66991db8337e47973e498cd6383ffb0c
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-07-11 15:49:30 +0200

    Update `albumentations>=1.0.2` (#3966)

--- utils/augmentations.py
+++ utils/augmentations.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c0b0533f65f5e20510d585e9cfd78cfcbcf5ce4272fb4504fae3d7b0f4f12247
+oid sha256:aa25ecfa8fbca91df6f98dd734029e1159f7696bc20124fc1969270a4ffde5e6
 size 11437
