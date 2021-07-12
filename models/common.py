3e5d051ef77f3b065678ebbb59aa91192dffcc1b
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-07-12 19:48:47 +0200

    Alert (no detections) (#3984)
    * `Detections()` class `print()` overload
    
    * Update common.py

--- models/common.py
+++ models/common.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9073c6e31742db106d84cbdcf13fc3d2b12ff06955d5a5edba2f3a74706dc086
-size 16830
+oid sha256:a3837e8877747054143fd2a23adda323f4cd9199266ec282b1479eeb502c75b9
+size 16886
