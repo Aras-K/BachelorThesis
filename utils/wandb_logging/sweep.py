3d4894b8c96c3b003db1efe1f4de522d4147428f
Ayush Chaurasia <ayush.chaurarsia@gmail.com>
2021-07-14 22:43:02 +0530

    W&B sweeps support (#3938)
    * Add support for W&B Sweeps
    
    * Update and reformat
    
    * Update search space
    
    * reformat
    
    * reformat sweep.py
    
    * Update sweep.py
    
    * Move sweeps files to wandb dir
    
    * Remove print
    
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

---
+++ utils/wandb_logging/sweep.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a4f9d9a012366f01a6b3fcc8a8e4263e52f279673d27d83665295d7b31bc290d
+size 877
--- utils/wandb_logging/wandb_utils.py
+++ utils/wandb_logging/wandb_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a717e7176e3d1cdf00cdc45a3ea97dd295b96c735a007afdb4930c44f68e6c12
-size 18065
+oid sha256:1602a3132867eeed89bb6525c97b60cdcd6b4518818f3073fad4ca7c1ff389a7
+size 18059
