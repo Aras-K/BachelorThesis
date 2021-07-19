8ad15d4632e4a0ab0cb01dc234b0e11e7528e39e
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-07-19 13:10:21 +0200

    Missing `nc` and `names` handling in check_dataset() (#4066)

--- utils/general.py
+++ utils/general.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f1007dff49f76dfb608bff368cb2f8d7197fb7548194d50623221940894a5e89
-size 28503
+oid sha256:3bd8fa2a446e63fc5268d4b70e9310198e0df21db9c1e60117a3f8d721e0d8f0
+size 28677
