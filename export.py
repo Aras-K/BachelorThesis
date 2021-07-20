d589dd2ddf8fb93e32c3142fe8f3bd0f09445b8a
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-07-20 18:42:27 +0200

    Addition refactor `export.py` (#4089)
    * Addition refactor `export.py`
    
    * Update export.py

--- export.py
+++ export.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e5be3fbac02ab5a4b7f5c1e9de00399e162acd14a2711a52d512abea9e3399b2
-size 7825
+oid sha256:10e698dd437e872ac041ec2efd4aa87bbeddc4826f219d5a1c704e38e67203f3
+size 7818
