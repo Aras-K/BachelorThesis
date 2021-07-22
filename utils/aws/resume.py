738181c006d1824fc5364487a2ee8918bd591814
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-07-22 17:22:11 +0200

    Update resume.py (#4115)

--- utils/aws/resume.py
+++ utils/aws/resume.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4c64dfff6947d1500303b28e0f4444eed3c3c132fc325943c6aa66dbc2d85db6
-size 1095
+oid sha256:58c626a6f1ec1a9e8e06e13d619521320df4c6a4981b5041255340d9e2662a0c
+size 1092
