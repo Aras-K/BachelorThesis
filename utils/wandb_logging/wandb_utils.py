91c51152e7d486fa009cc04800484a0dda540fe8
imyhxy <imyhxy@gmail.com>
2021-07-23 20:55:00 +0800

    Fix indentation in `log_training_progress()` (#4126)

--- utils/wandb_logging/wandb_utils.py
+++ utils/wandb_logging/wandb_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ac8b00c29db9ffb18e6957ce164d084df1d07c72fafe3566e81a7982d4e323f
-size 19498
+oid sha256:153631664a58be9f1068b33f194c9bf684d9d711783bea4a16cd527d51e6cd83
+size 19418
