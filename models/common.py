e00ae2259fc97726175acadbaca4dc93bbea18ca
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-07-30 00:37:55 +0200

    Suppress torch 1.9.0 max_pool2d() warning (#4227)

--- models/common.py
+++ models/common.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:95c8dc825491613ac3ba60c6d5460f7dd2cce745d2b717349d74c24baf1d9962
-size 16629
+oid sha256:a06ebad732b855e6a3bd4b3e6625c6ac7d7891dc83afa53d0a7a87ce6b81a1d1
+size 16778
