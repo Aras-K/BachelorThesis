3b6687ee07f7622be5cda4d36faf05d6f68f5d8f
Ayush Chaurasia <ayush.chaurarsia@gmail.com>
2021-08-01 15:36:53 +0530

    W&B: suppress warnings (#4257)
    * Improve docstrings and run names
    
    * default wandb login prompt with timeout
    
    * return key
    
    * Update api_key check logic
    
    * Properly support zipped dataset feature
    
    * update docstring
    
    * Revert tuorial change
    
    * extend changes to log_dataset
    
    * add run name
    
    * bug fix
    
    * bug fix
    
    * Update comment
    
    * fix import check
    
    * remove unused import
    
    * Hardcore .yaml file extension
    
    * reduce code
    
    * Reformat using pycharm
    
    * Remove redundant try catch
    
    * More refactoring and bug fixes
    
    * retry
    
    * Reformat using pycharm
    
    * respect LOGGERS include list
    
    * call wandblogger.log instead of wandb.log
    
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- utils/loggers/__init__.py
+++ utils/loggers/__init__.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f919e85dda624d4efca2902d53878f5ee882346ed35d6e5bce5680ffdc94d345
-size 6254
+oid sha256:a64ce867d85d9f115e66a6fe531c07ae674e8e972d15353a6cb5650e6912b0d1
+size 6339
