c313c20dfd3ec2347931b36053248cde20f6e002
Ayush Chaurasia <ayush.chaurarsia@gmail.com>
2021-08-02 22:11:52 +0530

    W&B: Fix sweep bug (#4276)
    * Improve docstrings and run names
    
    * default wandb login prompt with timeout
    
    * return key
    
    * Update api_key check logic
    
    * Properly support zipped dataset feature
    
    * update docstring
    
    * Revert tuorial change
    
    * extend changes to log_dataset
    
    * add run name
    
    * bug fix
    
    * bug fix
    
    * Update comment
    
    * fix import check
    
    * remove unused import
    
    * Hardcore .yaml file extension
    
    * reduce code
    
    * Reformat using pycharm
    
    * Remove redundant try catch
    
    * More refactoring and bug fixes
    
    * retry
    
    * Reformat using pycharm
    
    * respect LOGGERS include list
    
    * call wandblogger.log instead of wandb.log
    
    * Fix Sweep bug
    
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- utils/loggers/wandb/sweep.py
+++ utils/loggers/wandb/sweep.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:27d7ee8c4feff2cfd88e5dbeec0aadea0a3605baf561dfcf6abc1d3c17db3c52
+oid sha256:74ad4e8128a02bf4b40b0d75f49ca4bd50a0549269b933a19e7b02bd3380b253
 size 866
