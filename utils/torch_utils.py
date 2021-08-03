f6fb20a8f91f303db58cff58e173d58d20a71f41
imyhxy <imyhxy@gmail.com>
2021-08-03 19:11:42 +0800

    Fixed logging level in distributed mode (#4284)
    Co-authored-by: fkwong <huangfuqiang@transai.cn>

--- utils/torch_utils.py
+++ utils/torch_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e4c1ac5d1e39295236440bbbf7de7b61f1cb3e9f76efdb7a2f613fe806f3d160
-size 13396
+oid sha256:5dc71fda36a16570b294b3b3f2c18b3c7d87253ab181443c74cf1e13e0488457
+size 13400
