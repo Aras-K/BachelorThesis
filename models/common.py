1291ebd3567cbefac1800c4eaedbee048e7907a0
Yonghye Kwon <developer.0hye@gmail.com>
2021-08-05 17:54:16 +0900

    Simpler code for DWConvClass (#4310)
    * more simpler code for DWConvClass
    
    more simpler code for DWConvClass
    
    * remove DWConv function
    
    * Replace DWConvClass with DWConv

--- models/common.py
+++ models/common.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:39f47c34a3c1b572f8ad0a5bdb0b54e54fa0d085dcf208797c6110a5bd25123b
-size 17110
+oid sha256:fc2d617b92e6be3a5e1bf4280f3220414f6a7c3fa2af69a74bd83d01be8adb28
+size 16897
--- models/yolo.py
+++ models/yolo.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5926fe6c8a71559fb52a067f855d227ef86ce74d13d8337b9e4d37a87f18079d
-size 13180
+oid sha256:dbbfca0bd2771e9554036a6c77fbf24457274cc8ad06c7b77655318782e49dad
+size 13175
