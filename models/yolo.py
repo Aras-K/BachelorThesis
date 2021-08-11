2a8b31026ab68fd600c10ce3ed644560bb136b90
Yuantao Yang <31794133+orangeccc@users.noreply.github.com>
2021-08-11 23:38:34 +0800

    Fix module count in parse_model (#4379)
    Co-authored-by: yangyuantao <yangyuantao@transai.cn>

--- models/yolo.py
+++ models/yolo.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dbbfca0bd2771e9554036a6c77fbf24457274cc8ad06c7b77655318782e49dad
-size 13175
+oid sha256:a0bd8222d3a4471ad36bb5ac942c7cb51976e8edebc6592863d52c3032c3aedb
+size 13181
