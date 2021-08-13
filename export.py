a5cc50695f3db56a227e1859e82f091184d7672b
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-08-13 13:22:13 +0200

    Simplify ONNX inference command (#4405)

--- export.py
+++ export.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0c1d8ec095d2d41667bd934e7c1d0cb5e6d0a8cbf078aa191dc99154bf8f39db
-size 7960
+oid sha256:b9394a6ec13652dd808f05358da738eecca88b7392e3dcac4b211ddb5c4e9941
+size 7950
