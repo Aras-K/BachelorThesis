5f9580600a1b9a04d7e2aab344b06b0b3c07617d
Huu Quan, CAP <huuquan1994@users.noreply.github.com>
2021-08-18 19:07:09 +0900

    Fix missing labels after albumentations (#4455)
    * fix missing labels after augmentation
    
    * Update datasets.py
    
    Cleanup
    
    Co-authored-by: Huu Quan <huuquan@HuuQuans-MacBook.local>
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- utils/datasets.py
+++ utils/datasets.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f5820b7debed5842a40f0e10c33bac8bae7f1ebecb250d78294a7dac502eb49f
-size 42407
+oid sha256:fb0973cb372cccfe2d053df2a1ec7ba0b27df968d8a5a6403d2bde4efe6b1e70
+size 42466
