35120fda4c80703720ee97cacb3f1dfb8be00b6d
Ayush Chaurasia <ayush.chaurarsia@gmail.com>
2021-08-23 16:40:07 +0530

    W&B: Refactor the wandb_utils.py file (#4496)
    * Improve docstrings and run names
    
    * default wandb login prompt with timeout
    
    * return key
    
    * Update api_key check logic
    
    * Properly support zipped dataset feature
    
    * update docstring
    
    * Revert tuorial change
    
    * extend changes to log_dataset
    
    * add run name
    
    * bug fix
    
    * bug fix
    
    * Update comment
    
    * fix import check
    
    * remove unused import
    
    * Hardcore .yaml file extension
    
    * reduce code
    
    * Reformat using pycharm
    
    * Remove redundant try catch
    
    * More refactoring and bug fixes
    
    * retry
    
    * Reformat using pycharm
    
    * respect LOGGERS include list
    
    * Fix
    
    * fix
    
    * refactor constructor
    
    * refactor
    
    * refactor
    
    * refactor
    
    * PyCharm reformat
    
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- utils/loggers/wandb/wandb_utils.py
+++ utils/loggers/wandb/wandb_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ccf0b838c14db6b4a0c4947096deac4b6dd0a3eb27a1d63b970013c67b8d4cca
-size 25176
+oid sha256:74f1232113d6aa05779629e1a6363ab4f90cca6a13f604825e679210332c1bb7
+size 25324
