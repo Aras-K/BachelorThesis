8364da82df8db08b3097a44b9cacb7248d4a1dcf
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-08-26 15:51:04 +0200

    Auto-fix corrupt JPEGs (#4548)
    * Autofix corrupt JPEGs
    
    This PR automatically re-saves corrupt JPEGs and trains with the resaved images. WARNING: this will overwrite the existing corrupt JPEGs in a dataset and replace them with correct JPEGs, though the filesize may increase and the image contents may not be exactly the same due to lossy JPEG compression schemes. Results may vary by JPEG decoder and hardware.
    
    Current behavior is to exclude corrupt JPEGs from training with a warning to the user, but many users have been complaining about large parts of their dataset being excluded from training.
    
    * Clarify re-save reason

--- utils/datasets.py
+++ utils/datasets.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fb0973cb372cccfe2d053df2a1ec7ba0b27df968d8a5a6403d2bde4efe6b1e70
-size 42466
+oid sha256:fcfcea79c061e3c2bbd9a1590bc9183866cf5c5ff1afb48c28071ce08329eaef
+size 42636
