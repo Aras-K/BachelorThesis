0f2821562d84d0cfdec20802ec8dce2b1f0978b1
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-08-27 13:23:29 +0200

    Fix for AP calculation limits 0.0 - 1.0 (#4563)
    This PR brings alignment in AP computation practices with Detectron2 and MMDetection. 
    
    Problem first noted by @yusiyoh in https://github.com/ultralytics/yolov5/issues/4546

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0c3648ffd3aec2ef846f1b3cef86cceb56b841497762e62b4414f478cf55ca05
-size 13458
+oid sha256:26a6a4f3c1280f59f8b42c49abe23f5c0463d705883a3ac5b820616d07fd5036
+size 13447
