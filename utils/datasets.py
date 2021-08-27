1df6e1dd94134369523a5663bbefcc50dad7e32b
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-08-27 13:01:21 +0200

    Fix for corrupt JPEGs auto-fix PR (#4560)
    Auto-fix corrupt JPEGs PR introduced a bug whereby the f.seek() operation read all of the bytes in the image, resulting in the PIL image having nothing to read upon the .save() operation. 
    
    Fix was to re-open the image using PIL before saving.

--- utils/datasets.py
+++ utils/datasets.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fcfcea79c061e3c2bbd9a1590bc9183866cf5c5ff1afb48c28071ce08329eaef
-size 42636
+oid sha256:ecfaa968d7b63ba231cdcd12f4481ef7670393c81c90d9bd352da1a6e6c1357f
+size 42653
