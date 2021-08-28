4a81f133ca9583628a95a6f64529d6c5df3ecc42
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-08-28 19:17:21 +0200

    Remove `image_weights` DDP code (#4579)
    * Initial commit
    
    * Update

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f5d0c831385b5fd74d2c46e7ef0fbfcfbb715c204b8bdafbf214fa9ebb361206
-size 31849
+oid sha256:950c0d6c140a406151365193968b28cdcd6fa5cef2d383473c483b54e5810496
+size 31506
