9f1fd6d6726f842157425f1babd644313ec79c58
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-08-29 13:49:04 +0200

    Add `Profile()` profiler (#4587)
    * Add `Profile()` profiler
    
    * CamelCase Timeout

--- utils/general.py
+++ utils/general.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3a4a4dcfd156622cd5ca58efdf11951543e8782165d703756dd13d47e4c4a65d
-size 30741
+oid sha256:25d73e5e6e1ff2a757fa714e1194607f7432ea58fbbc53f3eb8f2584e31089e4
+size 31032
