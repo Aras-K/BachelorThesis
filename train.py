44fe087910528bd5e77a8d855182d570240a0495
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-08-30 18:35:07 +0200

    DDP `torch.jit.trace()` `--sync-bn` fix (#4615)
    * Remove assert
    
    * debug0
    
    * trace=not opt.sync
    
    * sync to sync_bn fix
    
    * Cleanup

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:90328f987b528ac9e4b6435ea7ea0e1199ced6d6b9dd0891f6a5ba4b0cf9dc1e
-size 31524
+oid sha256:05af2f72d0bd6749d6d1f77c756ebcffb9a71b76e3f893e614be3babba509061
+size 31417
--- utils/loggers/__init__.py
+++ utils/loggers/__init__.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:925f6a873743a55af86d11195f20bdc88eecfa7af7b453ed9f526a9389218b9b
-size 6452
+oid sha256:a69878bac08e651929bc901c30242e2824ce85a6e9e404bfcf0eb80940b4a6fa
+size 6592
