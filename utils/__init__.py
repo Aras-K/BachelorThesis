d7b32231e2715552c5e1ffbfdcac091834df4c15
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-08-31 15:01:41 +0200

    Fix for Arial.ttf redownloads with hub inference (#4627)

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dce65882d764ab1cf9371e9ae852ed45291cc93ae7783157d38ce9eed3d898e0
-size 428
+oid sha256:f8c75c3b8082671def961846b725f77c95a486c0746c8dd0e4b55c5afc80eeb3
+size 520
