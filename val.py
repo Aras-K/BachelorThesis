c264cca0c6e53388d2b126e70e34157adf4a930b
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-09-01 16:30:14 +0200

    Fix `is_coco` on missing `data['val']` key (#4642)

--- val.py
+++ val.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f862cda871ccc9492ed9e4b2f2fb549cebd29a400a3a589154d61d4ea1a261c4
-size 16785
+oid sha256:a505cc64bbc1d48118e0539073dc4f4d1cece3b07a89fc213bf30f412083a461
+size 16793
