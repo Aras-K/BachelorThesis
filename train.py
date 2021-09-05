5d65a2d788d12ea85541a960266159beacaf9c4e
imyhxy <imyhxy@gmail.com>
2021-09-05 23:43:09 +0800

    Fixed 'meta' and 'hyp' may out of order when using evolve (#4657)
    * Fixed 'meta' and 'hyp' may out of order when using evolve
    
    * Update gitignore

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:05af2f72d0bd6749d6d1f77c756ebcffb9a71b76e3f893e614be3babba509061
-size 31417
+oid sha256:6e2b932415d1034286c8568673f2b2c7c9c7eaeec3e6cbf06f35ac1790b8318e
+size 31420
