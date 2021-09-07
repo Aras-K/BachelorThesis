5b2d99fa2df6ff68c4ad8d4615f964a51e2611eb
Kalen Michael <kalenmike@gmail.com>
2021-09-07 18:32:15 +0200

    Optimised Callback Class to Reduce Code and Fix Errors (#4688)
    * added callbacks
    
    * added back callback to main
    
    * added save_dir to callback output
    
    * reduced code count
    
    * updated callbacks
    
    * added default callback class to main, added missing parameters to on_model_save
    
    * Glenn updates
    
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- val.py
+++ val.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a505cc64bbc1d48118e0539073dc4f4d1cece3b07a89fc213bf30f412083a461
-size 16793
+oid sha256:783805a8c07cd112a94bcb93b7311eb6e86cc6b6d0b339df2cc99b42512a8bbf
+size 16805
