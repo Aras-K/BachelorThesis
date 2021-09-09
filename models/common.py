c0ba563ca5834fe68a219ee9bb67ce338626f13a
Jean-Baptiste Martin <ultrapowa.exp@gmail.com>
2021-09-09 15:32:04 +0200

    Allow `multi_label` option for NMS with PyTorch Hub (#4728)
    * Allow specifying multi_label option for NMS when using torch hub
    
    * Reformat
    
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- models/common.py
+++ models/common.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cf14b190d6837f4ac4af034251bf7536f64cd19333b2edf078fc7d9c9788bc20
-size 19646
+oid sha256:5270224f3ac15ce8659923afedae918a844e97a22f7211ca26f43a885d28d06e
+size 19767
