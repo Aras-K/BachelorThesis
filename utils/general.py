49694349d005d210fd57530b944fafabaf8c76eb
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-09-10 17:06:22 +0200

    Fix `is_writeable()` for 3 OS support (#4743)
    * Fix `is_writeable()` for 3 OS support
    
    * Update general.py

--- utils/general.py
+++ utils/general.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d4b2c3c81c97c01f9256c08031179f8a7fbb6b9f73cf2db5a302645a4b684103
-size 32301
+oid sha256:bf51fbb2e9a4ef2b1aec701a84ad56f3c9f9953ff443071ea7fda86503a3af3a
+size 32444
