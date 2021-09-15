79e25585ee9b59faca45df02f360df048fa35cc8
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-09-15 13:13:33 +0200

    Evolution `--resume` fix (#4802)
    Also disable `/weights` dir creation when evolving as no weights are saved and empty folder causes user expectations of weights.

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f0fc446d02e0d7a2530a4d948423787b66f20cc9648c047c1e13b6b87356f164
-size 31631
+oid sha256:e88c1e05f895c84f30132dd7177917e5d614ac4033b3cc90ac7c23da7533ea0a
+size 31722
