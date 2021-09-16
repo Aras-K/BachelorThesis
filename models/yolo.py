68c5ddd8e29fca59c78545cef3141b67d7d0113e
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-09-16 09:47:34 +0200

    Refactor `forward()` method profiling (#4816)

--- models/yolo.py
+++ models/yolo.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:56b557c6f49a45c65fc9bce6a9e844a6379c5bbffffd863ca9835db718331406
-size 13424
+oid sha256:b976f72944037050a5ea9030ce3f1398d87647bb2dce8d3093c44847c028d707
+size 13337
