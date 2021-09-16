f9a660aa6267bc5de857687153c2695559050d6a
Jiacong Fang <zldrobit@126.com>
2021-09-16 21:27:22 +0800

    Multiple TF export improvements (#4824)
    * Add fused conv support
    
    * Set all saved_model values to non trainable
    
    * Fix TFLite fp16 model export
    
    * Fix int8 TFLite conversion

--- export.py
+++ export.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7f008b1fc8826ee60d41a2afd4d77f41aaad4a715d462afe3c8ffd0d833c714a
-size 14772
+oid sha256:37c8eefc491f1e983965c65601659beaf6881d73d53097800bb646410aa55b80
+size 14924
--- models/tf.py
+++ models/tf.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ef12955c6a1aae675052928b51869fed18a48caabd507917b43f6be3f4a08a45
-size 20001
+oid sha256:d4a40d27fd1edd7ab2cb8b43ba8d067503a705012ba9143a4bcd3d47e2a593d0
+size 20143
