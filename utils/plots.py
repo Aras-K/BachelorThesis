5bcfe2b7640c701f13442bd24d30ad901ac7d15d
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-09-16 17:55:58 +0200

    Fix val.py study plot (#4831)
    * Fix val.py study plot
    
    * call plot_val_study
    
    * Rename plot_study_txt to plot_val_study

--- utils/plots.py
+++ utils/plots.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d45cc3a77359f7d8703155f5d419fe5d18dbaa8a8422f78b0f7f2ef530d11aa7
-size 18424
+oid sha256:96526a583e280d91e9f8b2625893a8cdffb5be760b5bc46816c9253e820ea470
+size 18554
