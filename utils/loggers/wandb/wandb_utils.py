52ae1324ea3b707f8aceb2dc834dd489bdfb3724
Ayush Chaurasia <ayush.chaurarsia@gmail.com>
2021-09-17 22:29:34 +0530

    W&B: Enable login timeout (#4843)
    * evolve fix
    
    * Enable login timeout
    
    * fix pkg

--- utils/loggers/wandb/wandb_utils.py
+++ utils/loggers/wandb/wandb_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:96f36484c9c72c47206d33271f39d32de8473ec2a5c36ca20950cb5b4f6ce4ad
-size 25328
+oid sha256:aa6c79e48c3237201d838d13e30205db97c785224b7a7a205386ecdee86dd6c3
+size 25464
