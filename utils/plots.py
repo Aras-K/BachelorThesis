6dcb05e676e02e8aeb5f1467013665b460375d2a
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-09-20 13:57:23 +0200

    Annotator `check_font()` RANK -1 remove progress (#4864)
    * Annotator `check_font()` RANK -1 remove progress
    
    * Cleanup

--- utils/plots.py
+++ utils/plots.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ea8615c45ca2b2baed4ffeb1de90c63e19343abc74642cccac75100e0c3d2709
-size 18663
+oid sha256:04404eb31dec585f4f9b250096d062d7e153d0727bd1d5b20cc73245861d30bd
+size 18751
