5d8d1114edb1b5e90e6552df3ddaf70a6281bf32
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-09-26 17:11:46 -0700

    Scope `check_file()` search space (#4933)
    `check_file()` is now limited to searching opt-in directories: /data, /models, /utils. This prevents large non-project directories like /.git and /venv from being searched, which may cause `check_file()` to slow significantly.

--- utils/general.py
+++ utils/general.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ec02da57031d7bf781efc10cb67e7a8f666c2f0929f52ff6fe48f68888154008
-size 33814
+oid sha256:28816eece442cccc0cc4567a567c1b66524a8d4285fd12a795cbd934a3b7a371
+size 33922
