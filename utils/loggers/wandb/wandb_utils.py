863f8138729ff49e814403d47c3134807ad947db
Diego Montes <54745152+d57montes@users.noreply.github.com>
2021-09-28 15:22:31 -0400

    Update wandb_utils.py (#4953)
    `is_valset_wandb_artifact` and `is_trainset_wandb_artifact` were referenced before assignment causing wandb to be unusable.

--- utils/loggers/wandb/wandb_utils.py
+++ utils/loggers/wandb/wandb_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b4b129dfd8e9c574ad71e77460bdb0f4b91c2e81d42e76006416084058114709
-size 25750
+oid sha256:9e5ff8a465d08e2ca8f7b9921a4ee0bb967dc85bb51889bf46e7453c50c69178
+size 25796
