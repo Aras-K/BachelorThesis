c608e95278956642be290fcca80a10c718b23f7f
Diego Montes <54745152+d57montes@users.noreply.github.com>
2021-09-27 23:16:23 -0400

    Fix `isascii()` method calls for python 3.6 (#4958)
    * fix isascii for python3.6
    
    * update comment with python 3.7 note
    
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- utils/general.py
+++ utils/general.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eaeb08b231c2c02a3874a405cbae670d63ccf8a1104cc438ca53ab5b3ccb7edb
-size 33852
+oid sha256:300ea63fdf67840ffa8a2eedef7a344afd3ff47f873019e7c20013ad99888f51
+size 34100
--- utils/plots.py
+++ utils/plots.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1ed6089388a793adbb975814850104f75efdbe50aaaadeec139d788f10ad6a12
-size 19259
+oid sha256:f0451044d7986e6c85ce8d5f6af6cf0b5e7276be5dcd4fda06dfc091df5a67d7
+size 19269
