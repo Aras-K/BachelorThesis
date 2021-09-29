d961371eb7aa6d61931bda5100dafb58f68f26e2
Kalen Michael <kalenmike@gmail.com>
2021-09-29 19:48:45 +0200

    Fix URL parsing bug (#4998)
    * added callbacks
    
    * added back callback to main
    
    * added save_dir to callback output
    
    * merged in upstream
    
    * removed ghost code
    
    * fixed parsing error for google temp links
    
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- utils/general.py
+++ utils/general.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:300ea63fdf67840ffa8a2eedef7a344afd3ff47f873019e7c20013ad99888f51
+oid sha256:222fe3aef582c46ef8cc75b783e245233e84cc069689d8acf1c8b8ab353b2aaa
 size 34100
