64fe87f66da9b70d00af7b3a9ba5152ca2237e83
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-09-30 13:57:53 -0700

    Update train.py (#5014)

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a611b9a3f4e5603f541a1c8e4134fc359a5c74af4d69b00249b7cbc4e84d8922
-size 31723
+oid sha256:c0a20ff5a6bb266c32d5573abf485fc1d361bcde65687b9bfe49f89e5ee3d22b
+size 31726
