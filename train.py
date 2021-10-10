ccbbab981a9d3b4ca0b2ad71bc62c248d594a068
Nan <nan@blinkfire.com>
2021-10-09 20:41:50 -0500

    Pass `LOCAL_RANK` to `torch_distributed_zero_first()` (#5114)
    Co-authored-by: qiningonline <qiningonline@gmail.com>

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4ee56a75dec6a7dc5b8eebfc1ebe6bc5f722d9b92c8cba3ef07001bba384f64b
-size 32005
+oid sha256:c2342abab6e02382eca9d9bca3835d2ee375f66a306e05ef67f8a4c2c58c193f
+size 32023
