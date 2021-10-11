7e8a2f8ab6958531aad30f55dfebf3480db8e834
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-10-11 01:59:22 -0700

    Update plots.py (#5127)

--- utils/plots.py
+++ utils/plots.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f0451044d7986e6c85ce8d5f6af6cf0b5e7276be5dcd4fda06dfc091df5a67d7
-size 19269
+oid sha256:cd91870be493e0cff17ab500c0972b6c019e19f95872cf3b238d1d9c39a5e301
+size 19279
