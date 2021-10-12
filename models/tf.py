8ac00bf07bd8c8c62953e9ebbf00fdff848b3ecc
Yoni Chechik <chechik.yoni@gmail.com>
2021-10-12 18:38:54 +0300

    fix `tf` conversion in new v6 models (#5153)
    * fix `tf` conversion in new v6 (#5147)
    
    * sort imports
    
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- models/tf.py
+++ models/tf.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bba4b2c11038ebe2feff3d4fec55c2de57b413b5089cbd5177da64d3d9f9416a
-size 20226
+oid sha256:6f1ac6aa7871d28c1175651a0e2355d0c7899e054dd2efb6d8c972c635dac8e7
+size 20789
