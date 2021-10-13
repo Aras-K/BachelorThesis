2c0b89a259262c7b3144d4be983e5df2c37d6d20
Ayush Chaurasia <ayush.chaurarsia@gmail.com>
2021-10-14 04:47:15 +0530

    W&B: fix DDP with wandb disabled (#5163)
    * fix dpp with wandb disabled
    
    * PyCharm reformat
    
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- utils/loggers/__init__.py
+++ utils/loggers/__init__.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a69878bac08e651929bc901c30242e2824ce85a6e9e404bfcf0eb80940b4a6fa
-size 6592
+oid sha256:e4afbd8efb85ede10eb983476d94dbdea6c1fd044693733276407ef47ac57257
+size 6875
--- utils/loggers/wandb/wandb_utils.py
+++ utils/loggers/wandb/wandb_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9e5ff8a465d08e2ca8f7b9921a4ee0bb967dc85bb51889bf46e7453c50c69178
-size 25796
+oid sha256:bff024ec209f17dd307b7e20e62b5352e1e7c32d245bf19eb0c7cebda4dc823b
+size 25508
