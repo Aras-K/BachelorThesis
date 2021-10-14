795b940320735f02a54ac5a035efcb83aab92422
NauchtanRobotics <davidmacnaugh@gmail.com>
2021-10-15 04:57:00 +1000

    Adjust legend labels for classes without instances (#5174)
    * legend-labels Adjust legend labels for classes without instances.
    
    * #5158 Re-indexed series names: only classes containing data.
    
    * #5158 Re-indexed series names: only classes containing data.
    
    * Cleanup
    
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:137f499b43119bfeb3470de576460b8669b3e93ab74380a1e0d3604cba9b306d
-size 13503
+oid sha256:22989b0a177ace493773d46bb3c48d12a64d3fde04927516e62b4f44f44f235f
+size 13664
