22018f2b8f50fbdf0efef909bb476f825f0b1035
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-10-18 14:28:22 +0200

    Add `on_fit_epoch_end` callback (#5232)
    * Add `on_fit_epoch_end` callback
    
    * Add results to train
    
    * Update __init__.py

--- utils/loggers/__init__.py
+++ utils/loggers/__init__.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e4afbd8efb85ede10eb983476d94dbdea6c1fd044693733276407ef47ac57257
-size 6875
+oid sha256:8429c1ab51a43520336902f03439e116b7f9fb87936bfe61af8046da97e49190
+size 6884
