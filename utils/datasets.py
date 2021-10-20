ed1e963f3125a2e4802a1eac9f57a0ea7dccb2a5
jdfr <jdfr@users.noreply.github.com>
2021-10-20 21:35:47 +0200

    take EXIF orientation tags into account when fixing corrupt images (#5270)
    * take EXIF orientation tags into account when fixing corrupt images
    
    * fit 120 char
    
    * sort imports
    
    * Update local exif_transpose comment
    
    We have a local inplace version that is faster than the official as the image is not copied. AutoShape() uses this for Hub models, but here it is not important as the datasets.py usage is infrequent (AutoShape() it is applied every image).
    
    * Update datasets.py
    
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- utils/datasets.py
+++ utils/datasets.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dc380d669430143677aceb7cc71c942043d14fc8fd6044c6c40ae1251d7dbc4a
-size 44917
+oid sha256:4788ab90b5ab7fd5ca1ac73cd0007facc0499f3bfc481d5f9333b0b1500fcfa3
+size 44959
