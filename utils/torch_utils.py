4996e0a298bfcb5b3d05dfff3202db936732e781
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-10-22 20:02:19 +0200

    More informative `EarlyStopping()` message (#5303)

--- utils/torch_utils.py
+++ utils/torch_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d1adefdb4059aa12a2a1f8cc4e20a4f9abf92ca1bdb556c5ac16b9bff32d4ac8
-size 14025
+oid sha256:29406328e6993d9948a4c99f3bc6d14b9bec4680b856a33e2fa2f7828b8cd80c
+size 14367
