471dcf5df6b64f576df67325471371ccaa744591
Mrinal Jain <mrinaljain007@gmail.com>
2021-11-02 17:16:01 -0400

    Update `check_git_status()` to run under `ROOT` working directory (#5441)
    * Updating check_git_status() to switch to the repository root before performing git ops
    
    * More pythonic
    
    * Linting
    
    * Remove redundant Path() call
    
    * Generalizing the context manager
    
    * Fixing error in context manager
    
    * Cleanup
    
    * Change to decorator
    
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- utils/general.py
+++ utils/general.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3caf262932ee01db26af30db1155215826583f53cf6b66029ce7485d0161b64b
-size 34501
+oid sha256:aa922c1d191c083e83b5c4bdae94b70886c86abb5bf69ae9163eea21df4927b5
+size 34933
