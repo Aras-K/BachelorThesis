2d9cdc7ce2e55ea5845b1ebd614813a67df661dd
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-11-03 14:51:37 +0100

    Remove `check_requirements(('tensorflow>=2.4.1',))` (#5476)
    `check_requirements()` is unreliable for large packages like torch and tensorflow that may have multiple installation routes (i.e. conda, pip, tensorflow-cpu, etc.)

--- detect.py
+++ detect.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:459d1b32d644776cee3b14e51d3e9b4fba3e4e54107c40da8cf21fdcce934124
-size 15430
+oid sha256:75704f3a9e6fc3517867e8188b88ee99db4fe74c5c9d2c8d9d7c9d7d562a7503
+size 15379
