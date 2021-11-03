88fdf48d1f8650135a0d0473398dbf85c665d154
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-11-03 16:59:53 +0100

    Improve GPU name (#5478)
    * Improve GPU name
    
    * Update torch_utils.py
    
    * Update torch_utils.py
    
    * Update torch_utils.py
    
    * Update torch_utils.py

--- utils/torch_utils.py
+++ utils/torch_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ddbaa993d949daddc0888726bf3f57558afee870055d96e5259903c0fbab394
-size 14387
+oid sha256:de6f5e64b6e9a2d668e89a2f4ec12a2f358d2390d0e309ed2cefbb4d69f8ee8e
+size 14392
