2a28783e4bdb6b1bf611c72d7f2a9e6232fbdc10
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-11-03 19:04:51 +0100

    Add tf.py verification printout (#5484)
    * Update tf.py with verified confirmation
    
    * Update ci-testing.yml
    
    * Update ci-testing.yml

--- models/tf.py
+++ models/tf.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9c56b5007f8fffcf058a3dfbe2d2669c806fe5af610cf75b71b193d9bc9ebc10
-size 20546
+oid sha256:78994259e86eedfbe87259a3f6ad6789b9b90f4c7564e2325389a74d99679fa2
+size 20662
