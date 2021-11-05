41d9c42c198ed8e3f07606a8637d13dfd1ab7b79
Yonghye Kwon <developer.0hye@gmail.com>
2021-11-05 19:48:10 +0900

    Write date in checkpoint file (#5514)
    * write date in checkpoint file
    
    write date in checkpoint file
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * isoformat
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:19be6294a69be9d4b9179f40bbf2a51c4c86e2eee4484367f41bd1cf5f44d563
-size 32472
+oid sha256:dc028376a4e805171f0082a39715bcc56bb834be9c5c23544ff0e968a1806d0a
+size 32562
