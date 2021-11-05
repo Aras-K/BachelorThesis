8b3b273cf94caad7a403aa222ce0d200d05575fe
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-11-05 15:46:20 +0100

    Fix `increment_path()` explicit file vs dir handling (#5523)
    Resolves https://github.com/ultralytics/yolov5/pull/5341#issuecomment-961774729
    
    Tests:
    ```python
    import glob
    import re
    from pathlib import Path
    
    
    def increment_path(path, exist_ok=False, sep='', mkdir=False):
        # Increment file or directory path, i.e. runs/exp --> runs/exp{sep}2, runs/exp{sep}3, ... etc.
        path = Path(path)  # os-agnostic
        if path.exists() and not exist_ok:
            path, suffix = (path.with_suffix(''), path.suffix) if path.is_file() else (path, '')
            dirs = glob.glob(f"{path}{sep}*")  # similar paths
            matches = [re.search(rf"%s{sep}(\d+)" % path.stem, d) for d in dirs]
            i = [int(m.groups()[0]) for m in matches if m]  # indices
            n = max(i) + 1 if i else 2  # increment number
            path = Path(f"{path}{sep}{n}{suffix}")  # increment path
        if mkdir:
            path.mkdir(parents=True, exist_ok=True)  # make directory
        return path
    
    
    print(increment_path('runs'))
    print(increment_path('export.py'))
    print(increment_path('abc.def.dir'))
    print(increment_path('abc.def.file'))
    ```

--- utils/general.py
+++ utils/general.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:325ebc6b2864efbed5c5286209ebd950a05793978581b345c6990562dac69f17
-size 34845
+oid sha256:a632fc5ab2d063e774250c2b4a9cc795aa4ad56b8a3ca6fd0160c0faa3d703dc
+size 34876
