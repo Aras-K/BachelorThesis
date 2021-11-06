936fbf40ed5f60748931bae23f61586a9351e06c
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-11-06 13:49:00 +0100

    Update autobatch.py (#5538)
    * Update autobatch.py
    
    * Update autobatch.py
    
    * Update autobatch.py

--- utils/autobatch.py
+++ utils/autobatch.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3b49f7691e5d1ebe6a61f7f7b9787fee1f1860b7d57498e52f328f7d98d55646
-size 2065
+oid sha256:0fed11f6d67d393c71f2342b058d9c3c2be130e40b2959e54a2dc057f163aaec
+size 2137
