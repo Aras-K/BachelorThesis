75ec1b2b11489a0445661bc495ef7f51918f593a
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-11-08 16:04:31 +0100

    Add `--conf-thres` >> 0.001 warning (#5567)
    Partially addresses invalid mAPs at higher confidence threshold issue https://github.com/ultralytics/yolov5/issues/1466.

--- val.py
+++ val.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5fa505aae5af490f8b6af92f16d8c576f13e924e10c95cdbefceb36fe0177192
-size 17193
+oid sha256:762fcb92610581a4c80e9471fc2a828c303a2011cfc83f32432940db0c72a887
+size 17398
