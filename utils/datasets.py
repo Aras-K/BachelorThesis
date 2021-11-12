d1ee2edc83e2f8b1158c353414bbf1bb3165d98f
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-11-12 12:05:25 +0100

    HUB dataset previews to JPEG (#5627)
    @kalenmike per our convo yesterday.

--- utils/datasets.py
+++ utils/datasets.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:38c5d2e5c3b6b6fee40b63f6d7138375375128b1134479ac2c8fcab40af9bd63
-size 45025
+oid sha256:fdcc88ca52b9b33e8b271f3cfb38df9097d5a07222695d777aa5fd31feab17d6
+size 45048
