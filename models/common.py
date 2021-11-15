575b606feeb04745d306fa07ae9c503a28941027
Ding Yiwei <16083536+dingyiwei@users.noreply.github.com>
2021-11-15 17:06:18 +0800

    Replace 2 `transpose()` with 1 `permute` in TransformerBlock()` (#5645)

--- models/common.py
+++ models/common.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c32c773d94e752e90224f0046049153f4d9c5b5eb4bceea0ed08acdc02e7b3d3
-size 27391
+oid sha256:315e69fc5fc7743ca092c9cf54e6796e7795ced756dd1ecfb79dde2d3fd4d4e3
+size 27356
