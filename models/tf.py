8b02069ea7784d60d3805e437b6f361587681c21
Nrupatunga <nrupatunga.tunga@gmail.com>
2021-11-16 17:06:00 +0530

    TFDetect dynamic anchor count assignment fix (#5668)
    * fix tf.py when anchors not equal to 3
    
    * revert the isort fix
    
    * update the fix to use anchor attribute available already

--- models/tf.py
+++ models/tf.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d4f79449db7f55f2cc7cfbaad0fa41a3b58e5da10e75d100da2756753bf20644
-size 20658
+oid sha256:d457e680c894113c0a7e9766118c584215a4571c316e5f834deda07acb9a92d2
+size 20664
