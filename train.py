7aa5cdf7c19e08fbe09cdabf30dbf6f493c9c861
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-11-16 13:58:15 +0100

    Update train.py comment to 'Model attributes' (#5670)

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e741aff13970d0b634c656b89d3e77beb7622e75bfab45f5d857eee55be195c9
+oid sha256:9e57841b3b140ffa558c72d6f34b7c1af177f2fe7b9b8e9e94e929e9d5a29d7d
 size 32643
