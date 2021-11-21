18f01e0e2002103e7608295fc209078010117587
Zengyf-CVer <41098760+Zengyf-CVer@users.noreply.github.com>
2021-11-22 03:21:44 +0800

    Save *.npy features on detect.py `--visualize` (#5701)
    * Add feature map to save npy files
    
    Add feature map to save npy files,export npy files with 32 feature maps per layer.
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update plots.py
    
    * Update plots.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update plots.py
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- utils/plots.py
+++ utils/plots.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:af9c5c830e2d0cdb128c03db41f9b9b79c2bfc8fd4e5f109496da8f4e1f20c13
-size 20497
+oid sha256:7403c7e926ef644fe017cd54f74a97d46817abe220800434c767534c3ccbe822
+size 20566
