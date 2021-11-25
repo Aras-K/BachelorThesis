330e0033c0ff0466ff42fadba5f9c4c17b02a489
rockstarr <41538890+miknyko@users.noreply.github.com>
2021-11-25 18:18:30 +0800

    TorchScript `torch==1.7.0` Path support (#5781)
    * fix path error in export.py
    
    * Update export.py
    
    updated!
    
    * Update export.py
    
    oops forget something

--- export.py
+++ export.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ecabd60426a7fbb90e008d42740c76b871c0f717df8d549db17633d7240c38d4
-size 20212
+oid sha256:b3c28e1dd86865470f5f328be0384d957821981af17fdb345def1a2d90ed53c1
+size 20217
