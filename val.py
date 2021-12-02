3c682e6f56bcb5e671ab2d3d62337f62e8bb312f
iumyx2612 <69593462+iumyx2612@users.noreply.github.com>
2021-12-02 22:49:50 +0700

    Add `--workers 8` argument to val.py (#5857)
    * Update val.py
    
    Add an option to choose number of workers if not called by train.py
    
    * Update comment
    
    * 120 char line width
    
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- val.py
+++ val.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:073aaf3e995773fc4273be7ef471e11a0edbea7162f34fa9cb4a7e991df426b6
-size 17798
+oid sha256:a25b6013c3b0fcf785ea118bd71fb5a0c8e8877c65b4cabef6ffb0c4f5998412
+size 17995
