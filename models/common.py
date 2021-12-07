5d277b8a0385ebb8245e4cbf56bc9e0982198c8c
Li Zeng <lizeng614@users.noreply.github.com>
2021-12-07 16:01:41 +0100

    `Detections().tolist()` explicit argument fix (#5907)
    debugged for missigned Detections attributes

--- models/common.py
+++ models/common.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:96ca2801c4d0575d964b46cd027d7227accf676bf611a444ece5590c630cd369
-size 29978
+oid sha256:cf7ce7c0e8d85095f6a7f09a98a87ad4e4127dafb4ef2b822ae8bcbc68a06ba5
+size 29990
