43bd794b5cbfaac4c6fa8f4288fb4ce819d4d482
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-12-08 14:57:03 +0100

    Add hardware checks to `notebook_init()` (#5919)
    * Update notebook
    
    * Update notebook
    
    * update string
    
    * update string
    
    * Updates
    
    * Updates
    
    * Updates
    
    * check both ipython and psutil
    
    * remove sample_data if is_colab
    
    * cleanup
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a8285992d20706363a28d3c0c905507c225903bf7c76fb0eceb869d390b29656
-size 476
+oid sha256:47abb5c98e80d60842470c075e668da8f4a278f9dcc8faf011715983151ae9dd
+size 1131
