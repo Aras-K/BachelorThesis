d8a4703c33ed5df69214d55c2227344065caf781
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-12-08 16:46:24 +0100

    Revert "Update `plot_lr_scheduler()` (#5864)" (#5920)
    This reverts commit 360eec69101ad9ffada78326b715b724b9b7eb0d.

--- utils/plots.py
+++ utils/plots.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c38fcbe2e2baec3eec91ac91dc6c67fd361479d953356084ef688cbaf8900352
-size 20578
+oid sha256:7403c7e926ef644fe017cd54f74a97d46817abe220800434c767534c3ccbe822
+size 20566
