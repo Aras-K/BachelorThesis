bb891761e7e7f7ea1ecff3d14b7cc8328ab58d3c
Felix You <35478566+youyuxiansen@users.noreply.github.com>
2021-12-10 22:27:20 +0800

    Make `select_device()` robust to `batch_size=-1` (#5940)
    * Find out a bug. When set batch_size = -1 to use the autobatch.
    
    reproduce:
    
    * Fix type conflict
    
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- utils/torch_utils.py
+++ utils/torch_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:decd1dd65483ad7ad9c63b64a89bc4b4a2f2bf6c3269a7e52b799b77882ae62a
-size 13455
+oid sha256:7b0b18cf4d013df9560e8a3afe4d2d6c17b32bb3d39cfb7a87ca2e592749e974
+size 13456
