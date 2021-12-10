0fd18f4baba74864a69ad06af896143fc6e9743e
Diego Montes <54745152+d57montes@users.noreply.github.com>
2021-12-10 12:24:32 -0500

    Add nms and agnostic nms to export.py (#5938)
    * add nms and agnostic nms to export.py
    
    * fix agnostic implies nms
    
    * reorder args to group TF args
    
    * PEP8 120 char
    
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- export.py
+++ export.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:97951251341e3bf5216b2a2249416dbfd51b1e2b8a2e867d27c3c31742976aa8
-size 20306
+oid sha256:1039f2dc27297624ae9c524fba3364ea69455f0d2accbf317cf039bc97c27712
+size 20633
