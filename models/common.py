36f7beaf83eafdde9f615db93d93e6fc10c52667
Yono Mittlefehldt <yono@toojuice.com>
2021-12-11 18:40:37 +0100

    Fix Detections class `tolist()` method (#5945)
    * Fix tolist() to add the file for each Detection
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Fix PEP8 requirement for 2 spaces before an inline comment
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Cleanup
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- models/common.py
+++ models/common.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ce791ab45fa496e7af1f26b54df4eced7c502e35b3f34062da53f17d18a8a139
-size 30276
+oid sha256:ad93b0c4b6c74c946649253f6869683eb467b6a15371cb7c948fe77f01e5fab1
+size 30377
