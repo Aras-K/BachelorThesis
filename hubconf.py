14847944c63803460b94a4508f9ab7e388cb6c47
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-12-13 13:32:27 +0100

    `pretrained=False` fix (#5966)
    * `pretriained=False` fix
    
    Fix for https://github.com/ultralytics/yolov5/issues/5964
    
    * CI speed improvement

--- hubconf.py
+++ hubconf.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f403a09d1ad570da4b467d2ec38ee3256f1547f5991d790b82c731c26413c27c
-size 6371
+oid sha256:e382fd853e0cec5bc78a150bffc58f5dcc2874b39e40a1b2ad831435e1fa26b0
+size 6381
