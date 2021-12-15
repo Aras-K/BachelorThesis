0243031614df03e7488f7fb8f47015119eaded15
Mrinal Jain <mrinaljain007@gmail.com>
2021-12-15 19:12:23 +0530

    Increase `ar_thr` from 20 to 100 for better detection on slender (high aspect ratio) objects (#5556)
    * Making `ar_thr` available as a hyperparameter
    
    * Disabling ar_thr as hyperparameter and computing from the dataset instead
    
    * Fixing bug in ar_thr computation
    
    * Fix `ar_thr` to 100

--- utils/augmentations.py
+++ utils/augmentations.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db20fbfd757d0e960057e42b9c32c6b82fc3fc29c6678eb6ccdc341e4217082f
-size 11732
+oid sha256:edbec4d32e74c313d1692617057f6d2bfd1f2ad7c0edef6a27e8d76a558cd39b
+size 11733
