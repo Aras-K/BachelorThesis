0f2737dd05703dfce4126a5f190658537e6cf209
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-12-20 18:24:07 +0100

    `--freeze` fix (#6044)
    Fix for https://github.com/ultralytics/yolov5/issues/6038

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:38a86afa67d36d372067378e40cca7bbfcb5150dfb8111bcb51f1affc5719835
-size 32714
+oid sha256:112530feaf2159377d6da1fbc41740a43d4a5dffcf4a01af7f0dc6230ae2288a
+size 32709
