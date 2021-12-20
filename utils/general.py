a51b3997312592409d34470254501e6aef712928
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-12-20 17:30:46 +0100

    Kaggle `LOGGER` fix (#6041)

--- utils/general.py
+++ utils/general.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd64f3cf4f4aa7738162ee7f1cb873d31b473e1ca53f3dbd8b1129fc0a882a6d
-size 35058
+oid sha256:a1b892777d260e89d1450df80fa0cc76d6e67e402e7f21611bf4aba3920f0765
+size 35197
