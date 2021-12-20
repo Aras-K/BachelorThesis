33a78fb3d61db561cc465908a07a8e9ae32c26de
Glenn Jocher <glenn.jocher@ultralytics.com>
2021-12-20 17:42:52 +0100

    Simplify `set_logging()` indexing (#6042)

--- utils/general.py
+++ utils/general.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a1b892777d260e89d1450df80fa0cc76d6e67e402e7f21611bf4aba3920f0765
-size 35197
+oid sha256:bbb0426540149afb9ee30d846c0a8a3dfae05219356e2e5bd6a6155984044876
+size 35194
