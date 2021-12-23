ecb6f77f758f8003a4c79acde05dd0507be9d136
JieLi <32835610+jedi007@users.noreply.github.com>
2021-12-23 18:53:00 +0800

    Reduce G/D/CIoU logic operations (#6074)
    Consider that the default value is CIOU，adjust the order of judgment could reduce the number of judgments.
    And “elif CIoU:” didn't need 'if'.
    
    Co-authored-by: 李杰 <360751194@qq.comqq.com>

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ee79197c964986fb15f8f56c25b0e4d55decaeca5b17fcbe357ac7b8e3758917
-size 14081
+oid sha256:bee0ef83b5e937af8c35d24074633a34b4569dcc01f37c69b7c77954164a43b2
+size 14076
