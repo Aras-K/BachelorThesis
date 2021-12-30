41a2450dc5be2b640d864a707c908f3158c05c9a
Awsaf <awsaf49@gmail.com>
2021-12-31 03:47:53 +0600

    Log best results (#6085)
    * log best result in summary
    
    * comment added
    
    * add space for `flake8`
    
    * log `best/epoch`
    
    * fix `dimension` for epoch
    
    ValueError: all the input arrays must have same number of dimensions
    
    * log `best/` in `utils.logger.__init__`
    
    * fix pre-commit
    
    1. missing whitespace around operator
    2.  over-indented

--- utils/loggers/__init__.py
+++ utils/loggers/__init__.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:10eb2f71993b78b3b7edda85fa198186323364be28cc70192dbd90bfcc2ff1a6
-size 7374
+oid sha256:8497c66490661959bd3400b4f09b98161c808a5a598df66c8926611daf7cd3d9
+size 7737
