426b3e2783e534d81bab44d75e7539f2cd0e22a0
bilzard <36561962+bilzard@users.noreply.github.com>
2022-01-03 06:10:19 +0900

    Enable AdamW optimizer (#6152)

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7f90fcc02b2fceb9dac45e44ede9d7414f13160b3a91768537ae792e7f8138c3
-size 32770
+oid sha256:de861d3e620193bfd24451cc4aaba9b7beb669b4a2504c770d0ef436a78dd251
+size 32952
