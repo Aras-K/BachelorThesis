ff9781f79a4160b43db63232d5b304a9a3359bc0
imyhxy <imyhxy@gmail.com>
2022-01-05 03:09:25 +0800

    TensorRT 7 `anchor_grid` compatibility fix (#6185)
    * fix: TensorRT 7 incompatiable
    
    * Add comment
    
    * Add if: else and comment
    
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- export.py
+++ export.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2561b0726ca34f329aeba33deaecd161f697978305443876495c83f4149d2e40
-size 23424
+oid sha256:355fc2c07a6474218adcef7d544e6319915f3f20128ac0b3c2590cbb6d5d1e19
+size 23831
