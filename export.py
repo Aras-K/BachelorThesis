8b488b38802ea4ded11376b77a34ef73e1d4b567
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-01-12 15:48:40 -1000

    Update `export.py` with Detect, Validate usages (#6280)

--- export.py
+++ export.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0e832cd1bb8492ea78ec0056bf7cf94f2ee6dbcb65911cb322f874a0b2c896c7
-size 24756
+oid sha256:d6f9a6f213b05f38760e11ad64bd52aa386ff117b58330475b83fe708050665e
+size 25102
