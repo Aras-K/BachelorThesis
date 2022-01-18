70bcd8d3293e7495d62bdd37c602ff02973e6b87
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-01-18 13:52:25 -1000

    TensorRT `assert im.device.type != 'cpu'` on export (#6340)
    * TensorRT `assert im.device.type != 'cpu'` on export
    
    * Update export.py

--- export.py
+++ export.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d6749cceb26497ec5f571176507e8f69479b6f632f624f8d1dd54154546d5b17
-size 25064
+oid sha256:873b2789410a64bde97e4caddfc56af55d669ba38fab078e5311685d1cbd7dcb
+size 25183
