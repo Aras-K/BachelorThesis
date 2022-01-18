d963f99500175713a60a7647b6d48f012e2696af
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-01-18 10:04:49 -1000

    Remove `dataset_stats()` autodownload capability (#6303)
    * Remove `dataset_stats()` autodownload capability
    
    @kalenmike security update per Slack convo
    
    * Update datasets.py

--- utils/datasets.py
+++ utils/datasets.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e7883bc8dde5106a4275294febfbfcd2958023fcb7bea8d7d861bf06f7f3a539
-size 45238
+oid sha256:6f0218da820f6959bad098e9f5dc8468e9984c578846eab455f5a4200cb78a41
+size 45101
