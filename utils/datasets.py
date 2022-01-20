66a75bed9746c9a91c1717bef52e1fcdf9fe3f1d
sitecao <95668894+sitecao@users.noreply.github.com>
2022-01-20 18:06:26 -0500

    `DEVICE_COUNT` instead of `WORLD_SIZE` to calculate `nw` (#6324)

--- utils/datasets.py
+++ utils/datasets.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:614964f112aad19d34a1c665caf5560bf56e7b90a2db877a10d74731a59067bc
-size 45095
+oid sha256:e54971abd76e9ef0c7e2f029ce469c646a97ee845f265cb3549fefc46f21d2e0
+size 45094
