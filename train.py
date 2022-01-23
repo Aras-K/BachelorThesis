6129f6dae304d5734d9188b114b339c6e0af94ad
Matthias <haimat@users.noreply.github.com>
2022-01-23 03:37:21 +0100

    Add `stop_training=False` flag to callbacks (#6365)
    * New flag 'stop_training' in util.callbacks.Callbacks class to prematurely stop training from callback handler
    
    * Removed most of the new  checks, leaving only the one after calling 'on_train_batch_end'
    
    * Cleanup
    
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:989cd19b022ec2592bc24913a4d37c5865bf318ab0a82de03c16fac1bdefde24
-size 33401
+oid sha256:03627def2e8a88f5047e35986f29a84cf82d347c8facfcced0283ab8831405db
+size 33472
--- utils/callbacks.py
+++ utils/callbacks.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:772449d0efb313d2ef02058db80070cb90ed2852b8bf83a4adee7271b323c88b
-size 2396
+oid sha256:912b92377c5df061650f502b4ba51fc6693993051cd4bf40774bee0bbcb7250d
+size 2465
