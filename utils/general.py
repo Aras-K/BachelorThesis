806231e2852bb25d916360e3eeac0bce68769403
Jonathan Samelson <jonathan@samelson.info>
2022-01-25 16:21:06 +0100

    Rename logger from 'utils.logger' to 'yolov5' (#6421)
    * Gave a more explicit name to the logger
    
    * Cleanup
    
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- utils/general.py
+++ utils/general.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:483cec4dd20e28d6473f9d85e296834bf5b42d0532e71ef731c8203f7bedab25
+oid sha256:0f2d77c392fc47a8a19b7197d43cb45b16c8d3cb92ec764ceed6e1b4cc8595ab
 size 35579
