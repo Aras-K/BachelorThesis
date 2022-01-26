b0793fb44c9f173be0badf46cd6c12087e52f923
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-01-26 23:47:00 +0100

    Fix `select_device()` for Multi-GPU (#6434)
    * Fix `select_device()` for Multi-GPU
    
    Possible fix for https://github.com/ultralytics/yolov5/issues/6431
    
    * Update torch_utils.py
    
    * Update torch_utils.py
    
    * Update torch_utils.py
    
    * Update torch_utils.py
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update

--- utils/datasets.py
+++ utils/datasets.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:56c801786087cfdf9b3d0a8df1ef4a76a738147d1353a4f275cff0c3aba12b3e
-size 45239
+oid sha256:baa0ec4829dc8695a788064d468b1f358accf2aa110c269205dc28d813468b95
+size 45268
--- utils/torch_utils.py
+++ utils/torch_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7d3513cc6b9ecbee1a2080d85c91c2b50a7baa4c193ef1288e4b224283f2c126
-size 13591
+oid sha256:8f10695d086c8c389da7604cbaa530bfcd7a02a88b3e10d650f0837aea3c5150
+size 13912
