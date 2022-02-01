c4491f2395570e652cd23a46fa88db84d1487fda
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-02-01 21:17:56 +0100

    Simplify TF normalized to pixels (#6494)

--- models/common.py
+++ models/common.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f836ca387e48eaed14d39fd7b087e1dbdfc58e8208a0de4cda5344217edc3d45
-size 32146
+oid sha256:163f5532939239d42ea28cf3002509c006f3e45f52a35a9a058eb33fe68549a6
+size 32086
