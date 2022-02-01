69f855e7ec317ef01af369ba0b1065331d7888a4
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-02-01 22:06:29 +0100

    Improved `export.py` usage examples (#6495)
    * Improved `export.py` usage examples
    
    * Cleanup

--- export.py
+++ export.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a4be84adb66f6530bae237d791d67a4833cae290af72cde72197c331e513b6ad
-size 25435
+oid sha256:37985afc97f281110c89943c5d30a8b9dc1e4a93333df4daa0512f0eae2eb96d
+size 25455
