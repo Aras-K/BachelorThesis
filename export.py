25b5f13bbabc24274ce8a88906159c1799be9b75
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-02-01 23:52:50 +0100

    Suppress export.run() TracerWarnings (#6499)
    Suppresses warnings when calling export.run() directly, not just CLI python export.py.
    
    Also adds Requirements examples for CPU and GPU backends

--- export.py
+++ export.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a1df897c8a3f549ccbe0e081f06e98a01b45ca6a8eb976a2694cea3dbdafed15
-size 25624
+oid sha256:21f10fa82eac5b46ff0364ed57331008916281fbdeb34e8b40e42b8ff681467a
+size 25827
