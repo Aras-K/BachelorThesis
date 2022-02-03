794166023173768223d7a12bfe13bb8e6274065b
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-02-03 20:40:09 +0100

    Edge TPU compiler `sudo` fix (#6531)
    * Edge TPU compiler sudo fix
    
    Allows for auto-install of Edge TPU compiler on non-sudo systems like the YOLOv5 Docker image.
    
    @kalenmike
    
    * Update export.py
    
    * Update export.py
    
    * Update export.py

--- export.py
+++ export.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:21f10fa82eac5b46ff0364ed57331008916281fbdeb34e8b40e42b8ff681467a
-size 25827
+oid sha256:bad3beab84768a1b03f2ec6b5e1cf1baa6833ca613b3bd8febc8248d917bedf0
+size 25993
