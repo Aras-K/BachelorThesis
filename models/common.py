062f772fa62fa7339371d3350a7105251b87b5d0
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-02-05 15:45:44 +0100

    Edge TPU TF imports fix (#6542)
    * Edge TPU TF imports fix
    
    Fix for https://github.com/ultralytics/yolov5/issues/6535#issuecomment-1030631526
    
    * Update common.py

--- models/common.py
+++ models/common.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:29f9531c71351ff23607a3e432bb1d900633701d61637afc8ce145cef5753e07
-size 32208
+oid sha256:2417e586391d95b8ffa3cbff677f12f23b4518834da3cbbaf218f1f1ab599842
+size 32257
