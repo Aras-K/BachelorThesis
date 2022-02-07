e3a1a5a6b85a46f1ac8a8f9a9d248a5e25df4c3c
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-02-07 18:59:26 +0100

    Move trainloader functions to class methods (#6559)
    * Move trainloader functions to class methods
    
    * results = ThreadPool(NUM_THREADS).imap(self.load_image, range(n))
    
    * Cleanup

--- utils/datasets.py
+++ utils/datasets.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6f26d5f3f03102a9173c747f12dbb0bc7acc1c2e7556fa4cf1aae593cdd1d19b
-size 45346
+oid sha256:d3b624c8ca99aaa2f644d4b2e6f7ce31cb0d1e58951223e7a204fdfb3cca5a11
+size 45877
