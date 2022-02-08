fdb1efe081ba48a8158274dc63f91cb5b03bc0f8
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-02-08 12:20:39 +0100

    Fix zero-export handling with `if any(f):` (#6569)
    * Fix zero-export handling with `if any(f):`
    
    Partial fix for https://github.com/ultralytics/yolov5/issues/6563
    
    * Cleanup

--- export.py
+++ export.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:91ed1d0dc7d05dd1e508c3a17aa0af936723af1e0ccc724d2813f5648c824f92
-size 26158
+oid sha256:90e0895a6bbf162bda13ab511c2eee19c2b640b4d2b06f0caf3be6d9fd6285ea
+size 26197
