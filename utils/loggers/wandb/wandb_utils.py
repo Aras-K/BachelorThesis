c3bea6a0b0a20362034790f43e27fb9f045e8446
or-toledano <49984106+or-toledano@users.noreply.github.com>
2022-02-10 17:17:47 +0200

    Fix `ROOT / data` when running W&B `log_dataset()` (#6606)
    * Fix missing data folder when running log_dataset
    
    * Use ROOT/'data'
    
    * PEP8 whitespace

--- utils/loggers/wandb/wandb_utils.py
+++ utils/loggers/wandb/wandb_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:40f516efeec20edfb89faf9abf174ba547c9915d164f60c1207ae52afab944cd
-size 27101
+oid sha256:b3bbc42acda6d590a5d44c0c650244b3df71cc474bdfddc808cbd92f8f153448
+size 27102
