d57a60e0789227aab8215c72ff03c97df80d1c32
Ayush Chaurasia <ayush.chaurarsia@gmail.com>
2022-02-12 17:34:50 +0530

    W&B: don't log media in evolve (#6617)

--- utils/loggers/wandb/wandb_utils.py
+++ utils/loggers/wandb/wandb_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8c83a488e5fd273443932b880b42fabed3fe851836dbf4e2ac1e7714e8f7010f
-size 27043
+oid sha256:a52119c0b94a9baae097ccb0b81207097e980c3130b8cce1cc88235ca37fbb01
+size 27142
