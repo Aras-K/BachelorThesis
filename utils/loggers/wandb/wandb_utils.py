67af94085229dfb054fc88bff0b4005a8786c0e4
imyhxy <imyhxy@gmail.com>
2022-02-15 00:08:36 +0800

    Fixed wandb logger KeyError (#6637)

--- utils/loggers/wandb/wandb_utils.py
+++ utils/loggers/wandb/wandb_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a52119c0b94a9baae097ccb0b81207097e980c3130b8cce1cc88235ca37fbb01
-size 27142
+oid sha256:cd88b7bd74eb0e0725ecaac900da582eaa8e9b42c48dfb5e15f6ebec3323201c
+size 27147
