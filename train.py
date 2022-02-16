77964de4b30570eb0f428c07e1a44be424304fa8
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-02-17 00:47:42 +0100

    Add `--cache val` (#6663)
    New `--cache val` argument will cache validation set only into RAM. Should help multi-GPU training speeds without consuming as much RAM as full `--cache ram`.

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:54742fc5da01b3ddf8419a1dcdfd1facee6a80c22ed0d004011fe2ad03cc05c4
-size 33782
+oid sha256:3f99f09a9a6ffa36ba18cee7cb14fcfd7c4182fe35603ba9cdd0248b64404c59
+size 33860
