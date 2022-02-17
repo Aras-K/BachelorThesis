e843bc6cb628b172a5da27d07242224acd4a185f
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-02-17 12:55:03 +0100

    Robust `scipy.cluster.vq.kmeans` too few points (#6668)
    * Handle `scipy.cluster.vq.kmeans` too few points
    
    Resolves #6664
    
    * Update autoanchor.py
    
    * Cleanup

--- utils/autoanchor.py
+++ utils/autoanchor.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:be6a973bd37daf6b51ac9174edd56f01c900f0d2f2ecf13c1a456e7781df9b25
-size 7166
+oid sha256:a71a6804e452000a5ebb34a0a23fcef627fbab0d7442158c26d2408fbab6f207
+size 7318
