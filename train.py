71f36ea2970567999a59f2e8c71db8ed045348d2
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-02-21 14:23:27 +0100

    Switch default LR scheduler from cos to linear (#6729)
    * Switch default LR scheduler from cos to linear
    
    Based on empirical results of training both ways on all YOLOv5 models.
    
    * linear bug fix

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d141bcb07aec63ea25c49b011cb9d2eb8fe27ab35c08276ad5fa4ff8bf030ee8
-size 33862
+oid sha256:c816984d503b114e6de86336637b49f9c6cada99d1d0ed2e29900856ad737cd9
+size 33860
