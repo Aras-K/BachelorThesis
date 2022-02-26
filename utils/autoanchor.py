7c12f9dc01b8e65933943e29a7fe07e4dc19a2a9
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-02-26 19:15:12 +0100

    Update autoanchor.py (#6794)
    * Update autoanchor.py
    
    * Update autoanchor.py

--- utils/autoanchor.py
+++ utils/autoanchor.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a71a6804e452000a5ebb34a0a23fcef627fbab0d7442158c26d2408fbab6f207
-size 7318
+oid sha256:7e8bab1fd41dbad702cc31210f7c27c28b0f2f83e3cc7fa99c0cae61c56ead7e
+size 7335
