5d6577a6371695adc37cad98d935d0bd36187cf7
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-03-04 10:32:18 +0100

    AutoAnchor improved initialization robustness (#6854)
    * Update AutoAnchor
    
    * Update AutoAnchor
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- utils/autoanchor.py
+++ utils/autoanchor.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7e8bab1fd41dbad702cc31210f7c27c28b0f2f83e3cc7fa99c0cae61c56ead7e
-size 7335
+oid sha256:f875c72d41b3143974341fe692ba8402a55223c82e71bb698fd7be8f6dcaf4c5
+size 7368
