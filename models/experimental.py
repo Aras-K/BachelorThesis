fc194a2bd1550825133dea5e84648823d34db145
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-03-10 12:58:41 +0100

    Optimize PyTorch 1.11.0 compatibility update (#6933)

--- models/experimental.py
+++ models/experimental.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:746907f677ccd68918693bc2820e0bcd1243cf16ac02b206b04c2c142b21d908
-size 4617
+oid sha256:bff555d1975af3c1da33d1d8193b14a49a69dd1af6cee09df225fc9845dd8bde
+size 4662
