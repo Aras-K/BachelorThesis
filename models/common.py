a983678f62cc44247797ff8506209e87e9806f57
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-03-11 16:18:40 +0100

    Fix PyTorch Hub export inference shapes (#6949)
    May resolve https://github.com/ultralytics/yolov5/issues/6947

--- models/common.py
+++ models/common.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d8328740d50421ac5a46aacbf077075d49d51570aeff54decd4f5ad29d440d6d
-size 33371
+oid sha256:78890f1af693062ed188eac6ed4102709bd0d7f4c8bf6254cf64905dda0a1169
+size 33321
