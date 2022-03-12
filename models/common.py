bc60d2c5a83ddcc2bf78537e70dd007c34b27904
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-03-12 13:16:29 +0100

    DetectMultiBackend() return `device` update (#6958)
    Fixes ONNX validation that returns outputs on CPU.

--- models/common.py
+++ models/common.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6b5711f1435b040a0a5e5c9d06c5f972d11bbea93835e5cb11bacd9773e48b6b
-size 33529
+oid sha256:ff0c4d1610da3192c4dbdb8bea28295906f0bceb4279e44e02e17e61f99fdd16
+size 33555
