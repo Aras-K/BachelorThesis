7f565de655ad4a9369359dfc22f22c765c867ffc
paradigm <RubenProkscha@gmail.com>
2022-03-12 16:15:09 +0100

    EdgeTPU optimizations (#6808)
    * removed transpose op for better edgetpu support
    
    * fix for training case
    
    * enabled experimental new quantizer flag
    
    * precalculate add and mul ops at compile time
    
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- export.py
+++ export.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c622a47d67612ae393426ed0066359e7ecd60b988ac2aaf6676eef9f2cb9c4a0
-size 27933
+oid sha256:c210c1ea45e74d13a90b72177d6881797be718023d009819609d9fcc8b30e165
+size 27932
--- models/tf.py
+++ models/tf.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7ac91e640075c965d74b7e32865da7e53d1b5fb533788e68b3a56488e7e44eb2
-size 20649
+oid sha256:163f765081055985d72a31d60497e568b7dac885d0f6eb22136be1b161edfdf8
+size 20773
