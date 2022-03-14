edca6b7bedd508e2d0303060c1f0e8fbc483a4b4
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-03-14 10:54:51 +0100

    Model `ema` key backward compatibility fix (#6972)
    Fix for older model loading issue in https://github.com/ultralytics/yolov5/commit/d3d9cbce221b2ced46dde374f24fde72c8e71c37#commitcomment-68622388

--- models/experimental.py
+++ models/experimental.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bff555d1975af3c1da33d1d8193b14a49a69dd1af6cee09df225fc9845dd8bde
-size 4662
+oid sha256:37a967bd006d1f0aca748ad91de2f8fddccc09feb162522357cb5f10568574eb
+size 4666
