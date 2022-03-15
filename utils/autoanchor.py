8ff8927f620d9953e7c4ac614ae37af8fd23d33d
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-03-15 16:32:56 +0100

    Update TQDM bar format (#6988)

--- utils/autoanchor.py
+++ utils/autoanchor.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f875c72d41b3143974341fe692ba8402a55223c82e71bb698fd7be8f6dcaf4c5
-size 7368
+oid sha256:196d327341a6a3ad80ae0b82e01de9d5947a8c6b149c4920268429ca38e75f54
+size 7357
--- utils/datasets.py
+++ utils/datasets.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:30c077b7025b6ec4e936ce04a0eb86a0085c2446f41a38dee8109ef30e5e0a0c
-size 45769
+oid sha256:0486d73b1c660756e11db2f2e8f3c883089b27f28b2d9b9fe852efd26b4377f7
+size 45905
