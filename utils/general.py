acb33589c32ba52778124e18e4bb97ac1d004de0
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-03-16 15:33:54 +0100

    Conditional `Timeout()` by OS (disable on Windows) (#7013)
    * Conditional `Timeout()` by OS (disable on Windows)
    
    * Update general.py

--- utils/general.py
+++ utils/general.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eaddb40719bd0c6c6896337b9c657278305acb289fdb8bd9e17d0e1c2cd329fe
-size 37395
+oid sha256:0f11f0bd063bda06983e8a8d0b3a2b9495a6bce2e13f6bb7e410e28faea3d5a9
+size 37529
