385f7a1d8951f4f3b8e69e489210c0e401b95c30
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-03-20 14:18:05 +0100

    Fix incomplete URL substring sanitation (#7056)
    Resolves code scanning alert in https://github.com/ultralytics/yolov5/issues/7055

--- utils/datasets.py
+++ utils/datasets.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0486d73b1c660756e11db2f2e8f3c883089b27f28b2d9b9fe852efd26b4377f7
-size 45905
+oid sha256:72be17d24cf87d766fdb5796ec2424ef1d0496eab35dff84d15dc3d93ace12ae
+size 45951
