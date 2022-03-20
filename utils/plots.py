7449a13389822097020861739922d5471deea02e
Philip Gutjahr <mail@philipgutjahr.de>
2022-03-20 15:46:29 +0100

    Use PIL to eliminate chroma subsampling in crops (#7008)
    * use pillow to save higher-quality jpg (w/o color subsampling)
    
    * Cleanup and doc issue
    
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- utils/plots.py
+++ utils/plots.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1a3b35b2672370c3b7026751aa25e0156099eb877dcff9088a38d0639953ec76
-size 20647
+oid sha256:960c80fa21df297c1338fa1df5db32f156f3a8c4f4e514d55904a0e05d99fc80
+size 20842
