8a6b317b79068b4d258fd232749d04695e72109e
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-03-21 18:35:36 +0100

    Update loss.py with `if self.gr < 1:` (#7087)
    * Update loss.py with `if self.gr < 1:`
    
    * Update loss.py

--- utils/loss.py
+++ utils/loss.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:97762f10b7214e6960336e53da5a37a2559d5e27b2b8d26b270fe6378a72b108
-size 9677
+oid sha256:a948f9b8ce1e400c1552540617f6af13de49d96878d5defd44fd095c2a7f9f2c
+size 9668
