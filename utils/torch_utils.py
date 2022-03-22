70e6478b05a8895bd336a9ad124220b1b85126cd
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-03-22 15:41:19 +0100

    Update model summary to display model name (#7101)

--- utils/torch_utils.py
+++ utils/torch_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3af28e0c34224f3503add89e97e92bdafdddbe26046c818edc491ccadac68394
-size 13207
+oid sha256:333285d6903522b60169ee2b0a48c7b1bb74c93e42f18d2bbf605fa9d22b8da3
+size 13323
