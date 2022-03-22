be865775595ac0dbc53d8d3ccbdb1e694fc4c7d1
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-03-22 17:36:05 +0100

    `torch.split()` 1.7.0 compatibility fix (#7102)
    * Update loss.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update loss.py
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- utils/loss.py
+++ utils/loss.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:008a23b1c23e90ce94f976f5657831251405fff53142d4f97e353cc96ca685d5
-size 9684
+oid sha256:e137bcb3f620660c2da1fd04f2d16bb4393fedff03b254dc8044f9ef63cde3d2
+size 9866
