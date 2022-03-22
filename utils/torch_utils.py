bbadb1b3d0626c98172ab742a1e734bed953e08b
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-03-22 20:05:07 +0100

    Model summary `pathlib` fix (#7104)
    Stems not working correctly for YOLOv5l with current .rstrip() implementation. After fix:
    ```
    YOLOv5l summary: 468 layers, 46563709 parameters, 46563709 gradients, 109.3 GFLOPs
    ```

--- utils/torch_utils.py
+++ utils/torch_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:333285d6903522b60169ee2b0a48c7b1bb74c93e42f18d2bbf605fa9d22b8da3
-size 13323
+oid sha256:7d3246d1c3cbc1abb04dc7ae530570bdf0ef7143a28caf9291098d05347d37e0
+size 13343
