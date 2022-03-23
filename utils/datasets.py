00246e2fe6e700c1c122236ef174202034fc1a93
yeshanliu <41566254+yeshanliu@users.noreply.github.com>
2022-03-23 22:35:15 +0800

    `np.fromfile()` Chinese image paths fix (#6979)
    * :tada: :new: now can read Chinese image path. 
    
    use "cv2.imdecode(np.fromfile(f, np.uint8), cv2.IMREAD_COLOR)" instead of "cv2.imread(f)" for Chinese image path.
    
    * Update datasets.py
    
    * Update __init__.py
    
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- utils/datasets.py
+++ utils/datasets.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:72be17d24cf87d766fdb5796ec2424ef1d0496eab35dff84d15dc3d93ace12ae
-size 45951
+oid sha256:8a0a9a068c323713c2362597973ec9cb34d558077dd66812796aecccfe6bfeb8
+size 46065
