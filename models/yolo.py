1a635f2ec8c175a46a1f792b1e324119448a93a9
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-03-27 20:24:42 +0200

    `yolo.py --profile` updates (#7170)

--- models/yolo.py
+++ models/yolo.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b3849884dc22f83617725a2781cae2307d14a88a62c636f3c751bab2903bb64
-size 15064
+oid sha256:b0c83e6ff27651cbd6dbc93f94f112fc94fb7eb2a0d82022d15c033ddd96acbe
+size 15073
