4a1345f7cfcc7da56ef4331ad95ea79a373444a4
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-03-28 02:31:00 +0200

    Revert `C3()` change (#7172)

--- models/common.py
+++ models/common.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:82758b46be6562c740a846a949498f6de06ed365100c9eccabb74152d68b4f6c
-size 33693
+oid sha256:7bc6191a805e77e544e61841596b85b60868edcb73f8115f19b8d9a79de12a69
+size 33649
