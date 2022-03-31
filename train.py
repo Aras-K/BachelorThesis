2b30f183f9edf1335b387a5c1987aa6e4964a8ae
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-03-31 13:17:22 +0200

    Add train.py `--name cfg` option (#7202)
    Automatically names run as --cfg argument

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8d532da2e95e4833b42e681efaeeb1e8b5b1d4365a8868544ac939baaad3ace1
-size 33864
+oid sha256:b8f260d90e511b19f0eaeb5b3c8d3174a68a0c6991b2a725a7b4bc3b02f735bb
+size 33962
