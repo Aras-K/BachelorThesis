27302b148235fa3ee0fcf7eee0178548ba03ca80
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-04-01 00:07:23 +0200

    SavedModel TF Serve Fix (#7228)
    * SavedModel TF Serve Fix
    
    Fix for https://github.com/ultralytics/yolov5/issues/7205 proposed by @tylertroy
    
    * Update export.py

--- export.py
+++ export.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6d1a80d955a4a0d2f3592b3377c6369d4fd1883b1a20814f75fbda260776a690
-size 28625
+oid sha256:fe2796de658aef890896abebbead381404978da8f5560029e9aef0518b10502d
+size 28659
