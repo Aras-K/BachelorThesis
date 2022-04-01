8cec55a12cdc958913dd53e06d463a4e512f7068
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-04-01 21:38:49 +0200

    Fix `www.youtube.com` hostname (#7242)
    * Fix `www.youtube.com` hostname
    
    * Update datasets.py

--- utils/datasets.py
+++ utils/datasets.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b03083ba0e30ccd2f012cf801762ae5ac677c22a3151a58eaeaab7795401b143
-size 46462
+oid sha256:b57f13da7daf9288497e6deb47bf2ca9bbbb3efbd0bf6311be273229f4b3c87a
+size 46481
