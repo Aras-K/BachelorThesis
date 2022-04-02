cb33e4ef0fa5d1368518d11f1bdca19d971547fb
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-04-02 15:05:00 +0200

    Update minimum Python>=3.7.0 (#7247)

--- utils/general.py
+++ utils/general.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9c123d307527ec527f7ee5ff5faf4f6fc1cc10338325e5d6cf25fd263844bb21
+oid sha256:b0e4c52670599c04b25c82ce5455f4bee8684709935dfa90677b18577f6f98e6
 size 39102
