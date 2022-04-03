37f68b8fce26e9d0e16df0ee5ddfc9e029809058
Zengyf-CVer <41098760+Zengyf-CVer@users.noreply.github.com>
2022-04-03 19:19:26 +0800

    Fix Flask REST API (#7210)
    * Update restapi.py
    
    * Update restapi.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Cleanup
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- utils/flask_rest_api/restapi.py
+++ utils/flask_rest_api/restapi.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5b6f14f79a5d91ea9a8d393be39e34d0353421f7e4f1a9d5b34aeeac9f62044a
-size 1078
+oid sha256:e5449561616b5edf07c4f3259f0a3e69bf2aa7744b6d2fcc80fbcc40b9ae0c0f
+size 1247
