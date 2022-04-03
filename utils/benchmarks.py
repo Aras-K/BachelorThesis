e0734df65543a616393f81d1cc0948111a31d07a
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-04-03 21:21:55 +0200

    Update

--- utils/benchmarks.py
+++ utils/benchmarks.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b43d763744f068fcbd7bbfd3cf767462125f9e8f8c3ff72f93163350625e90c6
-size 4526
+oid sha256:c497c896e140dcfe04b55f045766952fa2eeaafc84fcc58322966699174d5cb8
+size 4587
