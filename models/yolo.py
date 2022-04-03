b43c46a965dea0fa0e94b0f297957907f731453d
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-04-03 23:40:23 +0200

    Integrate offset into grid (#7262)
    Eliminate 1 op during training and inference.

--- models/yolo.py
+++ models/yolo.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:98f63ec3b079864209cd7499239225ef28b8da39be7b8920231b539c9012fd58
-size 15188
+oid sha256:dd306eec601cca9513175fc701a99a7cbc77f85e21d9dc53d4c0d5a70f90f033
+size 15223
