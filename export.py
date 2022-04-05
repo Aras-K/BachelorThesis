f42c2f2f378916af82e408cc5af82a6fca335745
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-04-05 21:21:57 +0200

    Update export.py (#7301)
    * Update export.py
    
    Simplify code.
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- export.py
+++ export.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bf21c80255f9104394981ff4a424984536f492f69aae8b7b8ddbb34e5c90ed96
-size 28829
+oid sha256:ddce25aea23a2f2b6900dd7294ca4621d1e810025d8a720282a196130998cda5
+size 28553
