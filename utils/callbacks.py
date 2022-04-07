2957d07568f20c23707bff48f4cf65361cc6d8bf
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-04-07 16:15:01 +0200

    Simplify callbacks.py return (#7333)
    * Simplify callbacks.py return
    
    * Indent args (pytorch convention)
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- utils/callbacks.py
+++ utils/callbacks.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3d1b2e4163eeb7fb786227d3879dcab48b6f14e09ba670260f5fc316011a8fe0
-size 2453
+oid sha256:03ea6d78555baa5e6f3ed3027d0ec64fa1cbdb9962b1332272270b9957cf800b
+size 2402
