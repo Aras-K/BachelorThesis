734b5f5b3817f9870294314bcf379766ad1f7ec9
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-04-09 01:32:16 +0200

    Add `python benchmarks.py --test` for export-only (#7350)
    * Test exports
    
    * Fix precommit

--- utils/benchmarks.py
+++ utils/benchmarks.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c497c896e140dcfe04b55f045766952fa2eeaafc84fcc58322966699174d5cb8
-size 4587
+oid sha256:e23da3c2cb3dd3b432638ddd3d80798654f50339724aecd9eb0c228ba64c4dd0
+size 6063
