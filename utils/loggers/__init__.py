8c447c707029c39290051d2f51ed8db6d6b14178
Ayush Chaurasia <ayush.chaurarsia@gmail.com>
2022-04-12 15:16:56 +0530

    Add version warning for wandb (#7385)
    * add version warning
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Cleanup
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update __init__.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- utils/loggers/__init__.py
+++ utils/loggers/__init__.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:75c44213cdb515e03b4342c64c938628dee9fbbf0d759ba78d0908301a0f89a1
-size 7828
+oid sha256:4bc29826c8f40f61e04a2bc35e27768377da191114e71d76ec6cc234a9113f13
+size 8168
