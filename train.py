e14f18d747c0a40ed4d0de6e00d4c24f01d8678b
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-04-12 11:02:11 +0200

    Dynamic normalization layer selection (#7392)
    * Dynamic normalization layer selection
    
    Based on actual available layers. Torch 1.7 compatible, resolves https://github.com/ultralytics/yolov5/issues/7381
    
    * Update train.py

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:317bd2877d60711f0438af64fcb7b9a4ae17ddf586f1acf5f82f6a50d5d1a837
-size 34693
+oid sha256:d959446e274d5c92add563ae5f9b45233f4d9ec5ef7e37f33f745410a85e172a
+size 34688
