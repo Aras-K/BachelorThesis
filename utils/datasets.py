390867aa202cc74c355afa31cd2a90b83fe93eea
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-04-16 15:12:38 +0200

    Disable `pbar` for DDP ranks > 0 (#7440)

--- utils/datasets.py
+++ utils/datasets.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f0c7020582b305064161574fbeca34b9bf155597bd14bb1ced322eea3f9a0c80
-size 46613
+oid sha256:850f2fc81be67742e4f5d409387a73f49dcd95d0e4c7ff1c7ccde0254bbbc457
+size 46637
