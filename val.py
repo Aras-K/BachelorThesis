d15548e813b97cb8612f054b453d9b64fc66f812
HERIUN <ehdrndd@naver.com>
2022-04-20 07:40:06 +0900

    Update val.py (#7478)
    * Update val.py
    
    is_coco doesn't work!! '/' -> os.sep!!
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Cleanup
    
    * fix
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- val.py
+++ val.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:704aa7a943c39e716e251657decbcbe213a0e26bcba0eab67c899472c61815db
-size 19626
+oid sha256:f3c2fed4621dc74ee80c65efbd27e8b9198b0d03683e00f8890789a468c067df
+size 19634
