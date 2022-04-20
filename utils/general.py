e6b97461cc90d0872ef1b8c1df559bf536188ebf
Joseph Kocherhans <joseph@jkocherhans.com>
2022-04-20 12:08:22 -0700

    Added `YOLOv5_AUTOINSTALL` environment variable (#7505)
    * Added a way to skip dependency auto-installation.
    
    Setting the environment variable `YOLOv5_AUTOINSTALL=False` will
    skip installing any missing dependencies as if the user had passed
    `install=False` to `check_requirements`.
    
    * Cleanup
    
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- utils/general.py
+++ utils/general.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:befc863de70405cef21196f6d348deea39ee50ce92857d812309ecd0ec7e2e3c
-size 39854
+oid sha256:0cd4fde57a3d65441c6ac6c5f1beb2fb39175214b360393737c1cae1d0b868ba
+size 40003
