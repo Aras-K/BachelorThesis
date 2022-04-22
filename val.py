153ca0e02a57bc0f302c427b5716daa6365c38f3
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-04-22 12:01:14 -0700

    Empty val batch CUDA device fix (#7539)
    Verified fix for https://github.com/ultralytics/yolov5/pull/7525#issuecomment-1106081123

--- val.py
+++ val.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a464724655f337f724f99430a13f4a150cf61fdb752355f6f26fd7af1246627e
-size 19549
+oid sha256:5aa32ab6a95aa90c07fce1e90413be25371eb40f3fccc10b6722172d9040fb6e
+size 19564
