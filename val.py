35a37670d711d3a565ec41a3d8f94f919aaa1ea5
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-04-21 20:06:57 -0700

    Reduce val device transfers (#7525)

--- val.py
+++ val.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dc74b921fb76826583f15ef75e8982387ff7597e5fda901c0eb304a894f1c7be
-size 19626
+oid sha256:a464724655f337f724f99430a13f4a150cf61fdb752355f6f26fd7af1246627e
+size 19549
