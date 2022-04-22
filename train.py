3bbe9704458f2f7723930e4bda9fbcb791fe3a94
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-04-21 18:21:01 -0700

    Fix val `plots=plots` (#7524)

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2c5b015e5d992fefab301cd626ace8ad082a4f03646aca39c123eca507a63e96
-size 34864
+oid sha256:f28f57ce067c524e5efd0414a17cf71ba34c6a0cc400b00ad0c016cf86d7827a
+size 34865
