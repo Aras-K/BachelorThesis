2422bd97c35723b07cf97e6455ae8f5ba48a53b8
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-04-22 16:07:33 -0700

    Improve availability of YOLOv5 in Russia (#7545)
    * Add redundant weights download mirrors
    
    This PR seeks to improve access worldwide to YOLOv5 weights. Universal access to AI for all is our core value, and we are against any censorship or restriction efforts.
    
    I've uploaded the official YOLOv5 v6.1 weights to a primary backup bucket on GCP and to secondary backup on Google Drive at https://drive.google.com/drive/folders/1EFQTEUeXWSFww0luse2jB9M1QNZQGwNl. Autodownload with try the first two locations (GitHuB release assets and GCP bucket), and point users to the Google Drive folder if the first two fail.
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- utils/downloads.py
+++ utils/downloads.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:25d70f6b5e2f1d74d76324165331537a1ec9996fda14e8cf297572bd2d1438fe
-size 6281
+oid sha256:9ad24d5eed5bd429f6ea2ed8828dd96d2a72b079c4ab1af11dc190b515d42c13
+size 6413
