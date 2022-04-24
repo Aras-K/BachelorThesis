0951c57d539bcacea8cf2da36656234d18b74c31
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-04-24 11:30:40 -0700

    Enable `results.print()` when `_verbose=False` (#7558)
    Follows implementation of _verbose flag for PyTorch Hub models. Currently these are so silent that result.print() does nothing 😂

--- models/common.py
+++ models/common.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ebf5eefa1c1ad715161e3bcb7bb68c6f78225c7ee7b43147c6689b8ced964dfc
-size 34176
+oid sha256:528d4f42d0c00ffa75e963fcceb14310d3880332fdc4b69d08cb76c09694681c
+size 34144
