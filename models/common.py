2e74e2c432569b40005cd14664116612a088c53b
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-04-24 12:07:05 -0700

    Add `print(results)` override for PyTorch Hub results (#7559)

--- models/common.py
+++ models/common.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:528d4f42d0c00ffa75e963fcceb14310d3880332fdc4b69d08cb76c09694681c
-size 34144
+oid sha256:5337d5dc476fd7616e0069359e8d146903d079eb7c4ddbe07a86805f040d3f35
+size 34259
