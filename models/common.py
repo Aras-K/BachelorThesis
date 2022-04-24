c529f1d9fb19309838f9e206860f25cc3f5927d6
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-04-24 12:45:56 -0700

    TensorRT PyTorch Hub inference fix (#7560)
    Solution proposed in https://github.com/ultralytics/yolov5/issues/7128 to TRT PyTorch Hub CUDA illegal memory errors.

--- models/common.py
+++ models/common.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5337d5dc476fd7616e0069359e8d146903d079eb7c4ddbe07a86805f040d3f35
-size 34259
+oid sha256:92cd791a7b3918d4bcf21b6ef73b4a610fd110e38ef2d3ba9f0eb682d59f6e3c
+size 34282
