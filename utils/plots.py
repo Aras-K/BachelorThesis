4fe02c57cc9537d6e0664c9f08b2c77865789b28
SylvanDing <37374560+sylvanding@users.noreply.github.com>
2022-05-01 11:01:21 +0800

    Update plot.py (#7654)
    fix bug in issue #7650

--- utils/plots.py
+++ utils/plots.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0189a1fa24c839b36b2f8aef44bd94f3f758659c73a8e6350c07aa3ad7b8cf94
-size 21027
+oid sha256:a407a224da52019dd1f21909870c5f3ba83d797699c7c9ad6ad00c7fa29ead77
+size 21043
