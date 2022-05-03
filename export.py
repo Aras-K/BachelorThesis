df8c30554e910b979b1c365afeb9693a1883952d
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-05-03 13:47:30 -0700

    FROM nvcr.io/nvidia/pytorch:22.04-py3 (#7680)
    * FROM nvcr.io/nvidia/pytorch:22.04-py3
    
    * Update Docker
    
    * Update Docker
    
    * Update Docker
    
    * Update Docker
    
    * Update TRT auto-install
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Cleanup
    
    * Cleanup
    
    * Cleanup cpu
    
    * Cleanup cpu
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- export.py
+++ export.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0e2dec28ac3fc2e2ed458c9f56fd409a4d0632b819431484218d1d14588a5531
-size 29583
+oid sha256:dd665d9c3652459e7775cbb4a9f53c8b8eb230b382e952e2b0f7a83c9a4d4def
+size 29669
