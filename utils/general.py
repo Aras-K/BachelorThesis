5ffa7421f0e6d005a3f2a0dec99e489a4d234074
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-05-02 20:53:02 -0700

    Update Dockerfile-cpu to force python3.9 (#7675)
    * Update
    
    * Update
    
    * Do not install torch
    
    * Pillow>9.1
    
    * Update Dockerfile-cpu
    
    * Update Dockerfile-cpu
    
    * Update Dockerfile-cpu
    
    * Update Dockerfile-cpu
    
    * Update Dockerfile-cpu
    
    * Update Dockerfile-cpu
    
    * Update Dockerfile-cpu
    
    * Update Dockerfile-cpu
    
    * Update Dockerfile-cpu
    
    * Update Dockerfile
    
    * fix emoji
    
    * reduce scipy
    
    * add libpython3.9
    
    * Update Dockerfile

--- utils/general.py
+++ utils/general.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:636dc5eb359febe130e4919252fe782b733d648b063ec625db0082e2db471c3f
-size 40406
+oid sha256:4928769ab342ebd355d3edce63c13563a9b65b2c498ebf31938cd2fd66fb3c73
+size 40403
