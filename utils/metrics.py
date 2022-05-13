d2e945f84ac0073d207f28daf94ac43f166e540c
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-05-13 13:53:21 +0200

    Add smoothing to curve plots and max F1 index (#7798)
    @sergiossm

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cdf4fa30379f2557c848043ba4431b80ba40ecfc2548f704a1effb36498e1c1e
-size 14108
+oid sha256:646a0e62cc53c5d44c84d5f2eb1d86198ac87406644c9281ec6d6c6215cbbfdd
+size 14453
