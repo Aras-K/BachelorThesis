0732418e9515653aaec82ab7023bda7e14accea8
Yonghye Kwon <developer.0hye@gmail.com>
2022-05-13 21:26:11 +0900

    Add random interpolation method augmentation (#6826)
    * add random_interpolation option to make model robust to interpolation methods
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Fix precommit error
    
    * Update augmentations.py
    
    * Update augmentations.py
    
    * Update augmentations.py
    
    * Update datasets.py
    
    * Update datasets.py
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- utils/datasets.py
+++ utils/datasets.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:75826b3389b48bf9e2371b342143c8eadb8cd1fb5f37574a2ec4c034e5d4c8aa
-size 46632
+oid sha256:4c2cd313c2dfec6f56121f0f4cf6274dd8dbeffa51903d6350e4adf673878bce
+size 46781
