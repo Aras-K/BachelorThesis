f879840354d9674db5252bef25e4634d6ce1b30d
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-05-16 00:56:44 +0200

    YOLOv5 OpenVINO PyTorch Hub inference fix (#7826)
    Resolves bug report #7817

--- hubconf.py
+++ hubconf.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4bc3a20f5b250f922194179004462bd8a7464734ba7d11f0adaebfa5c4fb6ba6
-size 6443
+oid sha256:9636bde0ed9f34fba077beba3562e884618a7bcb982f709ca02416a94f4d468c
+size 6465
