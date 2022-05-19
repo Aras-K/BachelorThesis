86ba5531a3c5e5d550a9cebae455f7cd92c6f942
Jaewon Lee <ljay189@gmail.com>
2022-05-19 18:49:13 +0900

    Removed shell=True from subprocess commands that require user inputs (#7875)
    * Removed shell=True from subprocess commands that require user inputs. Also removed unused arguments
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Added check=True
    
    * Revert line add
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- export.py
+++ export.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1170a15b8e14dbb01b5b054f07a2999ebec46968d95573d8da413162dff20fe6
-size 29672
+oid sha256:6ac9a61c7eacd1e244ae4d0822e837c1f322dfc4ad2d5b2c97645951d5003106
+size 29572
