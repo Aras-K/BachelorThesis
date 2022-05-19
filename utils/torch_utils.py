942be1adb02f341aa72b3551ac81242eeedc6937
xylieong <61135607+xylieong@users.noreply.github.com>
2022-05-19 21:48:44 +0800

    Added Windows cmd to count GPU devices (#7891)
    * Added Windows cmd to count GPU devices
    
    * Cleanup
    
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- utils/torch_utils.py
+++ utils/torch_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dfc9f3f72f463a159b8ae9787d6a04d2d8fc3f822579bde6b7f53e24117c2289
-size 13330
+oid sha256:82c979fba59c4cde8fb576b11788cd83da26b4bab1c867f1e7c8f429cf1ebd9c
+size 13435
