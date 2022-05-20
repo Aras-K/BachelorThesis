799dc05c25e45bfb67d49fb4f13148b4722cc991
Anton Lebedev <lebedevdes@gmail.com>
2022-05-20 12:33:10 +0300

    Bug fix mAP0.5-0.95 (#6787)
    * Improve mAP0.5-0.95
    
    Two changes provided
    1. Added limit on the maximum number of detections for each image likewise pycocotools
    2. Rework process_batch function
    
    Changes #2 solved issue #4251
    I also independently encountered the problem described in issue #4251 that the values for the same thresholds do not match when changing the limits in the torch.linspace function.
    These changes solve this problem.
    
    Currently during validation yolov5x.pt model the following results were obtained:
    from yolov5 validation
                   Class     Images     Labels          P          R     mAP@.5 mAP@.5:.95: 100%|██████████| 157/157 [01:07<00:00,  2.33it/s]
                     all       5000      36335      0.743      0.626      0.682      0.506
    from pycocotools
     Average Precision  (AP) @[ IoU=0.50:0.95 | area=   all | maxDets=100 ] = 0.505
     Average Precision  (AP) @[ IoU=0.50      | area=   all | maxDets=100 ] = 0.685
    
    These results are very close, although not completely pass the competition issue #2258.
    I think it's problem with false positive bboxes matched ignored criteria, but this is not actual for custom datasets and does not require an additional solution.
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Remove line to retain pycocotools results
    
    * Update val.py
    
    * Update val.py
    
    * Remove to device op
    
    * Higher precision int conversion
    
    * Update val.py
    
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- val.py
+++ val.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:47f6d68a3efe855b7f3a86442cda953435e1317cfb4e5244f0f3422ab3cc8fd7
-size 19404
+oid sha256:543da9ebcda2ff0ddc90000be9695b62a6f73591b4763de23e4eb09e3bbbc3f6
+size 19420
