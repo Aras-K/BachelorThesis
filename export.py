abbf74a33c39440a39c15db6a7bf6bbd0edfd7a3
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-05-22 13:33:36 +0200

    Add `--keras` argument for TF exports (#7921)
    Resolves https://github.com/ultralytics/yolov5/issues/7911#issuecomment-1133671255

--- export.py
+++ export.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ac9a61c7eacd1e244ae4d0822e837c1f322dfc4ad2d5b2c97645951d5003106
-size 29572
+oid sha256:e9bedd19782070a991980b4bf5a8c800a4ba6fcd8724ee3e5eba778ea00dedf8
+size 29723
