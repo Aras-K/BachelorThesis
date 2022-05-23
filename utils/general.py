c3e81f0d2d1d88da21c4108593b27d7b2ffb063f
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-05-23 16:15:38 +0200

    Windows `check_file()` fix (#7938)
    Resolves Ultralytics HUB CI errors.

--- utils/general.py
+++ utils/general.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:000a8cb00b2337b013f6535e0c1a98db097c754e04fa1ced4b6a4fc9f1123199
-size 41593
+oid sha256:e1fec4eb4f1fac18298091da44130d1cc1a042db807a521da6486323ee943e90
+size 41570
