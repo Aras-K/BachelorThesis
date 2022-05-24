dcf6f1255c941d973bee6d80bda94fe54de083ae
xylieong <61135607+xylieong@users.noreply.github.com>
2022-05-24 17:30:36 +0800

    Add OpenVINO metadata to export (#7947)
    * Write .yaml file when exporting model to openvino
    
    Write .yaml file automatically when exporting model to openvino to be used during inference
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update export.py
    
    * Update export.py
    
    * Load metadata on inference
    
    * Update common.py
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- export.py
+++ export.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e9bedd19782070a991980b4bf5a8c800a4ba6fcd8724ee3e5eba778ea00dedf8
-size 29723
+oid sha256:4fc65a276ea6ba6aaea02ce8fe2479d90b6fa962acb38c27d26938e795643f60
+size 29915
--- models/common.py
+++ models/common.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1093d5415f3dd035133dcb2dae25fe2b7e5e1a9f05b6c2cea74dbdd7e45b2fd2
-size 34971
+oid sha256:100751fd3260428adef663654e24a65dc8cd03f8b1222a45a8f426200fb0a12f
+size 35331
