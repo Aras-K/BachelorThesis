df83b9a74b76488a19f9f4483c3dd22129c7ad5d
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-05-24 13:58:10 +0200

    Increase NMS time limit to 50 ms/img (#7956)

--- utils/general.py
+++ utils/general.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e1fec4eb4f1fac18298091da44130d1cc1a042db807a521da6486323ee943e90
+oid sha256:4fbaf7f8a6b8928efdd8d9a0ae596bc8d7ffeb31e0638883f7c9da26243d0955
 size 41570
