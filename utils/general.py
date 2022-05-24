ff8ca2eb8e812baebba909e35841f41a1a6a0749
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-05-24 15:33:35 +0200

    AMP check improvements backup YOLOv5n pretrained (#7959)
    * Reduce AMP check to detections verification
    
    More robust and faster
    
    * Update general.py
    
    * Update general.py

--- utils/general.py
+++ utils/general.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:99e52bac3f5132c68e7d1ec4a7c855ccba249b8c0c0b40b4d19dbb22ecdeaca0
-size 41570
+oid sha256:73333e7e28306d565fbbbf03b52fd52ae78a8872cfcc88fe602ab7fb68503d02
+size 41686
