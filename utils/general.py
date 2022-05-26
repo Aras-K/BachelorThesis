5f3834ba0e161c5ea1088ccd83d3c4eb719fa0a8
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-05-26 20:37:48 +0200

    cURL download double quotes (#7999)
    Attempt to resolve Windows issues

--- utils/general.py
+++ utils/general.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f7b610cc2b7ba60bb5915e745baf9435ae534f8949bf281a0c841f1e1665fbaa
-size 41684
+oid sha256:e786e1a2979f0f4a67a9c7fd00ec89f1b5fa4ef2db675d8ee4a8ab68261409ff
+size 41705
