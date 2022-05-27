b1022795a0ac8493ca745bec0c5b6b297ea0c956
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-05-27 11:49:30 +0200

    Add warning emoji ⚠️ on `--conf > 0.001` (#8005)
    * Add warning emoji on `--conf > 0.001`
    
    * Update val.py

--- val.py
+++ val.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:543da9ebcda2ff0ddc90000be9695b62a6f73591b4763de23e4eb09e3bbbc3f6
-size 19420
+oid sha256:2f55b97afd0c90da40902d691e7e8f213ff3504ce836d1ad1ec93cadec895fef
+size 19434
