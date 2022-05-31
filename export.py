01d2985c1ef4d196af21b11cff6ec79485964a41
Sahil Chachra <37156032+SahilChachra@users.noreply.github.com>
2022-05-31 16:11:09 +0530

    Fix FP32 TensorRT model export (#8046)
    Fixed FP32 TRT  model export
    
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- export.py
+++ export.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:894ed1cd45e59be8ccd804247dfa697ce22037bc2336c1bb96a0a3c6985b6e01
-size 29923
+oid sha256:ce11aecf924490def03abc2ded33596f2d8c40a5da4830f9786572574c78514e
+size 29941
