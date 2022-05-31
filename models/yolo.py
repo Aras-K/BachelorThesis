81bb6b652a90c4ec618b9107f3fb6036620a72df
tcmyxc <1282494272@qq.com>
2022-05-31 15:53:36 +0800

    `.detach()` on bias init (#8044)
    make init bias better, 
    I think `detach` is more safer than `data`

--- models/yolo.py
+++ models/yolo.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:483ba01700cae39833391f55ba64a1c3c05f8bbc83f88aee499aae11470ba443
-size 15355
+oid sha256:c02399a9827aad31830d333b7bd281588424ce6173e26b12063cacc1bb59dc52
+size 15354
