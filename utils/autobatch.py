919566b5b2a59bec6e2d1e7eb9d3b61c77c1dfad
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-06-09 17:15:49 +0200

    AutoBatch checks against failed solutions (#8159)
    * AutoBatch checks against failed solutions
    
    @kalenmike this is a simple improvement to AutoBatch to verify that returned solutions have not already failed, i.e. return batch-size 8 when 8 already produced CUDA out of memory.
    
    This is a halfway fix until I can implement a 'final solution' that will actively verify the solved-for batch size rather than passively assume it works.
    
    * Update autobatch.py
    
    * Update autobatch.py

--- utils/autobatch.py
+++ utils/autobatch.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5ec84abedc5785add2ba25ed6a0d51e3262403981ff672c9cbeb9c052a2b9b1b
-size 2199
+oid sha256:97587e385edb32badd923058e2220d74d7d7072bff5fd15b2c8744036a5e64c0
+size 2600
