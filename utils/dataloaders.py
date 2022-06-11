a5e63febd7d74ed3cbc2a8fd48798aa51babd7e3
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-06-11 19:30:54 +0200

    Improved `dataset_stats()` YAML checks (#8125)
    * Update dataloaders.py
    
    * Update dataloaders.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- utils/dataloaders.py
+++ utils/dataloaders.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d37d490f153dbee82f2e507fd12e95b5d733dba66dbe364ec9bd59ded0c8a326
-size 46652
+oid sha256:5c59d07174428879c554d572d026acf00570086b992bf06df00bb1cae7df1d52
+size 47301
