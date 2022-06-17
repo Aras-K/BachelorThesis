a6b6a7d9dfc6cbd07f4bd81e122527b900a9a4a2
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-06-17 18:37:08 +0200

    HUB dataset_stats() error reporting (#8192)
    * HUB dataset_stats() error reporting
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update dataloaders.py
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- utils/dataloaders.py
+++ utils/dataloaders.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3032d17e5983573e84165dbfda88384b73c1a4befaca2a875fbda9e4d12e3792
-size 47573
+oid sha256:bac7c291cf54063e04995c1bc7e4dee82948da4ad17b91cc2dc30074ac48437f
+size 47683
