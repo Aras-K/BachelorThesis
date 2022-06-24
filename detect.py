9e032d0712ca2be2b73121abeef72564f631bff2
Ryan Hirasaki <4690732+RyanHir@users.noreply.github.com>
2022-06-24 13:30:44 +0000

    Allow preview resize in detect.py (#8318)
    * Initial implementation of --viewsz
    
    * use mouse resize
    
    * Add comment
    
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- detect.py
+++ detect.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a823718b2cf6dcdb1c43d3e22bd49fcafb183f87221cd4b27660b5a54c1bce06
-size 13296
+oid sha256:067beb975e4e75d9883c68c993a9d269f4bcf5f93c7658aa78da47f9a69ae5ca
+size 13409
