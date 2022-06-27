15f8d83f6761be9eb037d87b7884355c6fb3d2f8
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-06-27 16:11:24 +0200

    Fix bias warmup LR init (#8356)
    Per https://github.com/ultralytics/yolov5/issues/8352

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c6f57d71e9b49e74640fbe062f96f5d8e4f5ff361f8c7bbf557cc6b1f5642a8f
+oid sha256:e3c546fbe1b40e4f29120f9f104810d395623231de23bd0de2e14d2c096995ba
 size 34919
