5cb4a4c3b70745ab8a6071e156c564da34cff3d8
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-06-30 16:19:22 +0200

    Update cache comments (#8414)
    * Update cache comments
    
    For better readability
    
    * Update dataloaders.py

--- utils/dataloaders.py
+++ utils/dataloaders.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bac7c291cf54063e04995c1bc7e4dee82948da4ad17b91cc2dc30074ac48437f
-size 47683
+oid sha256:7fff2bd80b38ff39ab5ba1c083a157b206058af2b95a87d7193724346bf0f0de
+size 47707
