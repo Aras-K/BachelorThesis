612cafaf268f7302d3523382a40f3dd2d1492370
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-07-01 15:41:14 +0200

    TRT `--half` fix autocast images to FP16 (#8435)
    * TRT `--half` fix autocast images to FP16
    
    Resolves bug raised in https://github.com/ultralytics/yolov5/issues/7822
    
    * Update common.py

--- models/common.py
+++ models/common.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aaadc385af8d0162351bf8af6257e19157f2a33571c2225c9c3c86803675d265
-size 35362
+oid sha256:eb3818a93b0a41abf23d9865434cc02db62eda85048e7c9e57c10ec43c09bd0b
+size 35453
