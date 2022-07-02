544b045febe893a67538bcd018b8bf503a609e32
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-07-02 18:35:45 +0200

    Do not prefer Apple MPS (#8446)
    Require explicit request for MPS, i.e.
    ```bash
    python detect.py --device mps
    ```
    
    Reverts https://github.com/ultralytics/yolov5/pull/8210 for preferring MPS if available. 
    
    Note that torch MPS is experiencing ongoing compatibility issues in https://github.com/pytorch/pytorch/issues/77886

--- utils/torch_utils.py
+++ utils/torch_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e361ca3a115bc7dec057626cb7d9a3f2f7319117da6a48eacdf48e15cfb02894
-size 13717
+oid sha256:251391c4b4e6d9ffb7471efafeedc91495aa7b98b57f853108ab65bb4d4e1461
+size 13722
