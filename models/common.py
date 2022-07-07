34f6fb86231c24c0e0627409207c5c302d101842
Colin Wong <cwongmath@gmail.com>
2022-07-07 16:53:09 -0500

    Properly expose `batch_size` from OpenVINO similarly to TensorRT (#8514)
    Properly expose `batch_size` from OpenVINO
    
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- models/common.py
+++ models/common.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eb3818a93b0a41abf23d9865434cc02db62eda85048e7c9e57c10ec43c09bd0b
-size 35453
+oid sha256:89664a7681e081adafaef765d0bd6476a198efac2a0993d0b8e008d68040f774
+size 35737
