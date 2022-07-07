3f2c81936b23c602875a1b3332dbe1de73900c86
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-07-07 23:41:34 +0200

    Add `--hard-fail` argument to benchmarks for CI errors (#8513)
    * Add `--hard-fail` list argument to benchmarks for CI
    
    Will cause CI to fail on a benchmark failure for given indices.
    
    * Update ci-testing.yml
    
    * Attempt Failure (CI should fail)
    
    * Update benchmarks.py
    
    * Update export.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update benchmarks.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update ci-testing.yml
    
    * Update benchmarks.py
    
    * Update benchmarks.py
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- utils/benchmarks.py
+++ utils/benchmarks.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:33c2bd903b6dd4614e7a0c246a517bbfd2c343528fb900ac1dc54cd59ac39e3c
-size 6397
+oid sha256:82372bb54705d36fd118ae60273b7db3ae9580ab5f4280104f089dd7069f3c0c
+size 6980
