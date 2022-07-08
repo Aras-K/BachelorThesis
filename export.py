bf8c87fa4742f7608a3c952a7b694ccfa8d36880
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-07-08 13:49:20 +0200

    XML export `--half` fix (#8522)
    Improved error reporting for https://github.com/ultralytics/yolov5/issues/8519

--- export.py
+++ export.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d9efdc8bd3f11b74f9f19ed97b93b376947987a05a88a204c928e3a2c4fc3849
-size 30170
+oid sha256:3ebba422899c039116a763df3433f531e18f98662fb973512635cf3a320ec6e8
+size 30163
