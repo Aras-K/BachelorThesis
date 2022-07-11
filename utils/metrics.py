c0dc4af40512919835520d6dce10f1a1e2fd8275
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-07-12 01:58:25 +0200

    Update metrics.py with IoU protected divides (#8550)

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6f9e829cf1184e765439157bc9e9b01d6ee369a7fc1225374951ff10de85d4c5
-size 14365
+oid sha256:907f4bab8bc809c608ee8fc68793c11a8eb311f94f3de4064fc5725be5f70d12
+size 14397
