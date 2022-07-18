9afdb379664b45018b86bb7d47b8f4904dd6ac5d
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-07-18 15:05:58 +0200

    assert torch!=1.12.0 for DDP training (#8621)
    * assert torch!=1.12.0 for DDP training
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:13fd8a62e03a5a78cf8e72db65447fbe5b61977ba5a4a8eed36ccf0a0ae70b55
-size 33829
+oid sha256:9eebb02d339c821555d1dd76c3d0a7df0a17f6f296b854188222488e590c877e
+size 33545
