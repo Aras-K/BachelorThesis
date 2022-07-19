d83c8836b27d258b7ec7d702a80a99dc6af237cd
daquexian <daquexian566@gmail.com>
2022-07-20 01:01:24 +0800

    Upgrade onnxsim to v0.4.1 (#8632)
    * upgrade onnxsim to v0.4.1
    
    Signed-off-by: daquexian <daquexian566@gmail.com>
    
    * Update export.py
    
    * Update export.py
    
    * Update export.py
    
    * Update export.py
    
    * Update export.py
    
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- export.py
+++ export.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:684d43e0389064ec8fd3aa173af95ea5c6408f4a82f285f7dfe6893606bbccdd
-size 30287
+oid sha256:409505d0ebdfd95fca99087f07241b41c5db071545968c444e46074c037325a3
+size 30190
