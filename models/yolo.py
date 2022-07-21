67f8ce17d6bcc8d2cda11d326c03e92270b3ac6a
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-07-21 23:12:49 +0200

    Use contextlib's suppress method to silence an error (#8668)

--- models/yolo.py
+++ models/yolo.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c02399a9827aad31830d333b7bd281588424ce6173e26b12063cacc1bb59dc52
-size 15354
+oid sha256:e5979d3edffcf861a4fcce1a7736086327cfea1b383ea2b85187049de4b0d28e
+size 15353
