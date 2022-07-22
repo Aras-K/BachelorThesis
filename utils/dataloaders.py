8b6bbffb6c8e62d97e1cb527f4a9b05bd2895889
UnglvKitDe <100289696+UnglvKitDe@users.noreply.github.com>
2022-07-23 01:25:17 +0200

    Add generator and worker seed (#8602)
    * Add generator and worker seed
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update dataloaders.py
    
    * Update dataloaders.py
    
    * Update dataloaders.py
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- utils/dataloaders.py
+++ utils/dataloaders.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0eb866182499e02d4cd2f9964e233fe922b3fec0ac1d8d6e32932cc76792c99b
-size 47833
+oid sha256:2d4910590bba9d788393cd624cbd26d3f8ff0d1f97c46eed633705268bd1c993
+size 48219
