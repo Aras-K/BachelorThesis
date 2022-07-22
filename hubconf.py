70951626fc0d7ecbe3d6bf301f1c130ee46390d9
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-07-22 19:01:16 +0200

    Update hubconf.py to reset logging level to INFO (#8680)

--- hubconf.py
+++ hubconf.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7834c112b1fc3a13d7e83f950b7d5fc517389fa982462d56c12c2446de8bc0d7
-size 6467
+oid sha256:e73811f786fda4392e429f1114d552f9bf507ef90d731df019fa3d3550efe922
+size 6497
