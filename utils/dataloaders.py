4f3d34be17fbcdadb639df69db467bb404f809a6
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-07-25 18:20:01 +0200

    New `HUBDatasetStats()` class (#8716)
    * New `HUBDatasetStats()` class
    
    Usage examples:
    ```
    from utils.dataloaders import *
    
    stats = HUBDatasetStats('coco128.yaml', autodownload=True)  # method 1
    stats = HUBDatasetStats('path/to/coco128_with_yaml.zip')  # method 1
    
    stats.get_json(save=False)
    stats.process_images()
    ```
    
    @kalenmike
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update dataloaders.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update dataloaders.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update dataloaders.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update dataloaders.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- utils/dataloaders.py
+++ utils/dataloaders.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:855a4b5c5f1ba5697bbfaa476cdb7e0772a3d58fade4bf619e4924dfa850e559
-size 48094
+oid sha256:c09ff108c48921088df2a8178dfc60ee7a0047f90cba2ad2df3bd6f0b523339c
+size 48060
