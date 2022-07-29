f955189ff1826a580cb58723a9f82eeec380eaff
jbutle55 <justin.butler12@hotmail.ca>
2022-07-29 06:06:23 -0600

    Fix confusion matrix update when no predictions are made (#8748)
    * Fix confusion matrix update when no predictions are made
    
    * Update metrics.py
    
    * Simply confusion matrix changes
    
    * Simply confusion matrix fix
    
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:907f4bab8bc809c608ee8fc68793c11a8eb311f94f3de4064fc5725be5f70d12
-size 14397
+oid sha256:4a50cd08691498ed0ba2d87b1288ec940d16b1408a9c7167ef649968ce98af95
+size 14597
--- val.py
+++ val.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:79d8727fbb1787204180b808eb7ecc74988725a8f30327b780606807e0b0339f
-size 19471
+oid sha256:1c36eaa02474fa5f6d046cc443047b2acb65142a27ba553a12bb5f30e615cdba
+size 19594
