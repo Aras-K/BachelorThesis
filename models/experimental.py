9cff5630fb988b312cbe5c96062903c57edfc4b3
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-07-30 21:25:16 +0200

    Deprecate torch 1.6.0 `compat _non_persistent_buffers_set` (#8797)
    Deprecate torch 1.6.0 compat _non_persistent_buffers_set

--- models/experimental.py
+++ models/experimental.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9a0007eb10513bd2832b6c148b4e0d9c34c16c7ec11bbf5aeb4d52815baf9de0
-size 4146
+oid sha256:dd50595a21195da37a3a43bf16a968da672498cb7856f972a6c67b8f3280fc10
+size 4043
