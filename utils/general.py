2092128e973f0729413b41a5d773a2c9b39eb239
Amir Pourmand <pourmand1376@gmail.com>
2022-07-31 20:47:38 +0430

    Check git status on upstream `ultralytics` or `origin` dynamically (#8694)
    * Add remote ultralytics and check git status with that
    
    * Simplify
    
    * Update general.py
    
    * Update general.py
    
    * s fix
    
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- utils/general.py
+++ utils/general.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5c6379b416f31a74f161530034541a90c3fd1a84afe066e31061fccaee0375d4
-size 42489
+oid sha256:630db3b0d10814ba5f1023a3f183a31f6eb45347d8c5c595655e18bb8d19b8e7
+size 42860
