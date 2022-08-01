925fe36546f3e1a4b786ad27d0726f337bf6b77a
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-08-01 03:01:44 +0200

    Remove `assert not is_docker()` from GitHub checks (#8813)
    * Update
    
    * Update

--- utils/general.py
+++ utils/general.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:630db3b0d10814ba5f1023a3f183a31f6eb45347d8c5c595655e18bb8d19b8e7
-size 42860
+oid sha256:13ad63bc3618cb8e7e1cc29c98a7ac64090878e79fe8632bb37d0b7fd5ed6b0f
+size 42790
