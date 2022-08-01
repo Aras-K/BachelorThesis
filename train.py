adaca8a3f6e37305ee702465e4b0c101084f3e9b
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-08-01 21:39:04 +0200

    Remove hook `torch.nan_to_num(x)` (#8826)
    * Remove hook `torch.nan_to_num(x)`
    
    Observed erratic training behavior (green line) with the nan_to_num hook in classifier branch. I'm going to remove it from master.
    
    * Update train.py

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:87806acca1dc959690bd18f86bff9f1f85fe2afe0f70d3d8b553320761ca8999
-size 33889
+oid sha256:ec0f1a69fd94a8f26a351798a2c3549110a2af440424a726360cd39deba46bbf
+size 33930
