74b2d187308f86d4d24c185b385eaf95db12ed36
UnglvKitDe <100289696+UnglvKitDe@users.noreply.github.com>
2022-08-01 12:08:46 +0200

    Add tensor hooks and 10.0 gradient clipping (#8598)
    * Add tensor hooks and gradient clipping https://github.com/ultralytics/yolov5/issues/8578
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Remove retain_grad(), because its not necessary
    
    * Update train.py
    
    * Simplify
    
    * Update train.py
    
    * Update train.py
    
    * Update train.py
    
    * Update train.py
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:df03c22247a3196514d413dab54d6343f89a7ea29dd49ff96cb26992fb046698
-size 33598
+oid sha256:87806acca1dc959690bd18f86bff9f1f85fe2afe0f70d3d8b553320761ca8999
+size 33889
