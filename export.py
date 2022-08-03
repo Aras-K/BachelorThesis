bf1ebeaee761eeacc6036dad0fb26571a8bc750f
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-08-03 23:38:36 +0200

    export.py replace `check_file` -> `check_yaml` (#8852)
    * export.py replace `check_file` -> `check_yaml`
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- export.py
+++ export.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dd555d268547aed413115d3120ccec0b613b5773073dab318fd264db85a4093b
+oid sha256:d58e2efd62c51c39a2bbb46234f9247490408d8d1acde89bc46ced9993863cf3
 size 30658
