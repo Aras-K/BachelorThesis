cf9f868a7e045e6918b7a284ae5f8a8ed4ec8977
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-08-04 23:26:30 +0200

    Fix TensorRT --dynamic excess outputs bug (#8869)
    * Fix TensorRT --dynamic excess outputs bug
    
    Potential fix for https://github.com/ultralytics/yolov5/issues/8790
    
    * Cleanup
    
    * Update common.py
    
    * Update common.py
    
    * New fix

--- models/common.py
+++ models/common.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a9ec57d56e803502373728b1c66f5fe3b01606006a82aceee42c0cc57b1272b5
-size 36872
+oid sha256:2d20396e6598098538e4f627cebf9392ab3df185cdb87e64d0cff569add5e947
+size 36953
