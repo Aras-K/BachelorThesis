4b491175d6feacee8413201fafc761ff911385f2
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-08-05 14:45:41 +0200

    HUBDatasetStats() preview images to 50 quality (#8880)
    @kalenmike should represent a 30% filesize reduction vs 75 quality

--- utils/dataloaders.py
+++ utils/dataloaders.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:29dd7552ae85754ddcefe47b1c94fd60ea7b43b0fe301fc7dadbdcacc28e8192
+oid sha256:a4fa7938f66bfc988c0484855ad5f6a14b37117e8c3848032da1a77901c79a3d
 size 48042
