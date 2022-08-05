edd13c358a9ac284c6e8bdff96dc02f6d9998ff9
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-08-05 16:27:28 +0200

    `--resume` training from URL weights (#8882)
    @kalenmike

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6742a77f19f6bfe71545c36be317ac30cb9248c4e8407b9cd35af4dbcceb3bfb
-size 33458
+oid sha256:66b404544c0d9f6bb81fa7dd10582561240f383f5ae14535bff27367651b96e8
+size 33370
