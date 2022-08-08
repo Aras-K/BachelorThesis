11db29f98290b7ea9e75e2ec5e53adcd8c3bb481
黄不盈 <33193090+YellowAndGreen@users.noreply.github.com>
2022-08-08 23:15:18 +0800

    Fix anchor incorrectly initialized (#8891)
    * Fix anchor incorrectly initialized
    
    Using --noautoanchor and --evolve simultaneously leads to anchor incorrectly initialized. --noautoanchor denotes anchors don't need to evolve, thus removing anchors from hyp will fix it.
    
    * Update train.py
    
    * Update train.py
    
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8cda6d84f7a2db6ef14ab50444f58a10f8e668c0e484c25bbc7da1d1f3ce52d7
-size 33445
+oid sha256:10d90a154ad518359e085a4b8169bdd8ad89f473b7aebd1da78ce22305511ed7
+size 33522
