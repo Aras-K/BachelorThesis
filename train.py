30972b128cbd39734544694257d259064518715b
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-08-13 03:57:31 +0200

    Remove DDP `destroy_process_group()` on train end (#8935)
    May resolve https://github.com/ultralytics/yolov5/issues/7307

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:10d90a154ad518359e085a4b8169bdd8ad89f473b7aebd1da78ce22305511ed7
-size 33522
+oid sha256:0ce68fdc7529816485eef3419a76ef957443fc27c67e0f4951f7c70df45c4276
+size 33384
