044304ece87ac18e194505cb0553fce54d06c8c7
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-08-14 00:12:09 +0200

    Fix windows LOGGER with emojis output (#8958)
    * Fix duplicate LOGGER output
    
    * Update general.py

--- utils/general.py
+++ utils/general.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b8e7be811c618f8ea9909cca823f132ee794ddbeb9a76558ccc1a6eaa09ae3b8
-size 43059
+oid sha256:2e0fa9f89417774336c533ab849aa5c33fcc8876fca280de8ee3351a91792eae
+size 43117
