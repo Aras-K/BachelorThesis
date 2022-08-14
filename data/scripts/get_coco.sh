dcf9d047311401a53fa9d6cb385ae9bd35771e15
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-08-14 22:33:55 +0200

    Add bash script arguments (#8962)

--- data/scripts/get_coco.sh
+++ data/scripts/get_coco.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:58210cb73c9d681e01324d2d1796120fc767f0526eb3bd9b21b76ec8e5a7bf26
-size 900
+oid sha256:70746595310a7dcd020d1afb1807fcdc0f00cf4b8cc89b3da6a5e67b999eeb79
+size 1555
