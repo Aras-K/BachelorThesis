71d687a320bb098dee0182088b06c65f12e17e97
Ayush Chaurasia <ayush.chaurarsia@gmail.com>
2022-08-18 02:11:43 +0530

    [Classify]: Allow inference on dirs and videos (#9003)
    * allow image dirs
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update predict.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update dataloaders.py
    
    * Update predict.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update predict.py
    
    * Update predict.py
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- classify/predict.py
+++ classify/predict.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ef6343a0039da41055c4bb5759e23a37e06f930713d17adc8450934ec4653fed
-size 4053
+oid sha256:28c023809249acfda20e2a99fc91fea19ac6087141f7c310c4bb48734c34d109
+size 4385
--- utils/dataloaders.py
+++ utils/dataloaders.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ccedba3f80c72493bf6fa912d89546b56a139ee935d99dff119e65d3cd12c175
-size 51162
+oid sha256:f78f57ab072b041285bb7bea1372ca1c535c482cea5d5294773f047ca84d98ce
+size 51352
