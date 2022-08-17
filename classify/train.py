f5961d042d6f5ea210406e51db4487d62a0cbe5f
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-08-17 22:57:55 +0200

    Add weight `decay` to argparser (#9006)
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- classify/train.py
+++ classify/train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f0a68111657e5c8bf38abae86ca44cd561112aa2420ea9f06f8ac8b33bec0c1b
-size 16012
+oid sha256:ab65bf465e21024ac4a2dfc6a4f60b65c8525da6518005807925d7ff569be165
+size 16099
