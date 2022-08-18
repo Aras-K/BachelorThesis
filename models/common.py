966457aac0343d9b9899a5166526f816f165ec80
cher-liang <88578531+cher-liang@users.noreply.github.com>
2022-08-18 20:18:02 +0800

    Faster pre-processing for gray image input (#9009)
    * faster 1 channel to 3 channels image conversion
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- models/common.py
+++ models/common.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3cf4c2ade12b48d1685ebeac301bad20683cffcedd502f3546d9bcc2a0b28e40
-size 37412
+oid sha256:97aecb1f2fe50bb3faed660b0ba459d0ddc622f3cd70c29aae189ea417c8e04d
+size 37423
