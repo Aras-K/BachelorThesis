9750a77e298e5c3e2da78d1a1b15593062f166d5
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-08-18 21:45:11 +0200

    Add PyTorch Hub classification CI checks (#9027)
    * Add PyTorch Hub classification CI checks
    
    Add PyTorch Hub loading of official and custom trained classification models to CI checks. 
    
    May help resolve https://github.com/ultralytics/yolov5/issues/8790#issuecomment-1219840718
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    * Update hubconf.py
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- hubconf.py
+++ hubconf.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aef03f0530ad68397e8cdffda5ad3f178bfea58d65e72cffbb5d4ce57265db43
-size 6842
+oid sha256:0de55d27ae7dacfb101f476f7e316f4f428c3b51fd264823e77fec4e695be0d4
+size 6904
