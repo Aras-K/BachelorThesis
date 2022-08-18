2b8faa79275024eaa4bfc8539ddea740b38b121c
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-08-18 11:50:24 +0200

    Fix TorchScript JSON string key bug (#9015)
    * Fix TorchScript JSON string key bug
    
    Resolves https://github.com/ultralytics/yolov5/issues/9011
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- models/common.py
+++ models/common.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c5d0a97a3e5dd6cf5e59cb5740909605dfcd5e58ef55ee9487c4b5de4ec2ffce
-size 37246
+oid sha256:3cf4c2ade12b48d1685ebeac301bad20683cffcedd502f3546d9bcc2a0b28e40
+size 37412
