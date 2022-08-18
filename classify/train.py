554133285b8190d4007dd773ecd6d38aa7522927
glennjocher <glenn.jocher@ultralytics.com>
2022-08-18 02:44:50 +0200

    requires grad after reset params

--- classify/train.py
+++ classify/train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ab65bf465e21024ac4a2dfc6a4f60b65c8525da6518005807925d7ff569be165
+oid sha256:a0350f54ac188022a002ed3c58dd6bc9bcd5d7bf66598334d01b95da698c5d47
 size 16099
