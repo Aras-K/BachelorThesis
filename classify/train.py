da851731f1f8aa6c77a0d87b37faf48470b2e2c3
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-08-19 01:59:51 +0200

    Attach transforms to model (#9028)
    * Attach transforms to model
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    * Update val.py
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    * Update train.py
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- classify/train.py
+++ classify/train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a0350f54ac188022a002ed3c58dd6bc9bcd5d7bf66598334d01b95da698c5d47
-size 16099
+oid sha256:d3094cc0cc005fe87f77d710c8e9cfe746e97520618dcdb3407f07112e5f948e
+size 16139
