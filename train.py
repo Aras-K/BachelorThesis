fef22a3a25480d6e9eb2b9d4adbc4a9a12619849
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-08-19 16:54:58 +0200

    Update train.py `import val as validate` (#9037)
    * Update train.py `import val as validate`
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:431b4a386489ec91291cd8aa2f0bb5fa29bf5937c0950b11e69843dcc06a99c7
-size 33223
+oid sha256:2671c917bef4bc6890ae44f0e9d22c4d82d71077c9387991d4a3508ae2a5ffcb
+size 33291
