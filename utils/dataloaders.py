d2ea41546c660f73617ec3037fb0a9d835e22eb4
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-08-19 15:07:04 +0200

    VOC `names` dictionary fix (#9034)
    * VOC names dictionary fix
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    * Update dataloaders.py
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- utils/dataloaders.py
+++ utils/dataloaders.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f78f57ab072b041285bb7bea1372ca1c535c482cea5d5294773f047ca84d98ce
-size 51352
+oid sha256:3e1453dfce7af6e1e95cee5c2df1ee17865300bfffaea21b097b93cef4cfb70a
+size 51526
