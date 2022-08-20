191271d0054cd6527789d9d10d98f16d039bf752
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-08-20 16:29:08 +0200

    AutoBatch protect from negative batch sizes (#9048)
    * AutoBatch protect from negative batch sizes
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * cleanup
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- utils/autobatch.py
+++ utils/autobatch.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e2f012c400453a636413c732d82c4f6b7a068723824ca2927644ea24b477e044
-size 2584
+oid sha256:980ca1a325be99ba75de3eeddf4a56472237031fc439da7120370f7d9401fbee
+size 2767
