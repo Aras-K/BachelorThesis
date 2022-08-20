1e9f58605db8f619bd52196d142437855340acf5
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-08-20 17:17:35 +0200

    Add `--save-hybrid` mAP warning (#9050)
    * Add `--save-hybrid` mAP warning
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    * Update val.py
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- val.py
+++ val.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4e55ca5b44e6336961f7fa6c96ff06cab7166308864fa518428eccc60226a546
-size 19723
+oid sha256:2fee8cf65bbc2f41a63e94fbfcfc5ac06a01ad290ba5236c87f59004e602ccfd
+size 19876
