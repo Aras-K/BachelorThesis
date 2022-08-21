f1261d88373594ab1f5abb5a026c822dbcfb57a6
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-08-21 23:26:07 +0200

    Two dimensional `size=(h,w)` AutoShape support (#9072)
    * Two dimensional `size=(h,w)` AutoShape support
    
    May resolve https://github.com/ultralytics/yolov5/issues/9039
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    * Update hubconf.py
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- hubconf.py
+++ hubconf.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0de55d27ae7dacfb101f476f7e316f4f428c3b51fd264823e77fec4e695be0d4
-size 6904
+oid sha256:f86bd163fcda3bd8abf73802c6a04096b862a17ffb7e0b017ac954936fa8491b
+size 7227
--- models/common.py
+++ models/common.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7c441e6a9c1a1f638e8c5929f7858f9110b90ae1912fba9ed88c9b89349e4446
-size 37531
+oid sha256:3dbcfe2c2bcd6dfe6e23a037915fcf07179029ab154693d7e19141448fc8e0b2
+size 37624
