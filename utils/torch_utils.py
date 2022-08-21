89e32f553bf0d77e4ebbc25f32f402273de3eb21
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-08-21 02:23:39 +0200

    zero-mAP fix return `.detach()` to EMA (#9056)
    Resolves https://github.com/ultralytics/hub/issues/82
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- utils/torch_utils.py
+++ utils/torch_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a646613e5c65ee51c7e2abe067038bd95fc90c0f1fcad3aae1806f1d365394be
-size 19547
+oid sha256:19653da2b4dfe8c7f6c19aa8665360c100a1ce49dd6a56ccee406a4dd12ea676
+size 19556
