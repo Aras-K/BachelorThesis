20b2a05eb8f685da55f7dd68a0eaa5c0fcc85d34
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-08-21 17:07:56 +0200

    De-thread TensorBoard graph logging (#9071)
    * De-thread TensorBoard graph logging
    
    Issues with Classification models
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    * Update __init__.py
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- utils/loggers/__init__.py
+++ utils/loggers/__init__.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:62c272cb1f8eac23ff96b3cc3dc67ca1f6fc42dd52b380771234fffe7b88c551
-size 13280
+oid sha256:e48dad9dff6d05157954e217bd37da135015c2015ab5ceab7470ecca3575dad4
+size 13260
