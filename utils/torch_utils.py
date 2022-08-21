2301b66e3535c6f93d54b0c24b3aba8ad627ce58
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-08-21 03:22:24 +0200

    zero-mAP fix 3 (#9058)
    * zero-mAP fix 3
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    * Update torch_utils.py
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update torch_utils.py
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- utils/torch_utils.py
+++ utils/torch_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:19653da2b4dfe8c7f6c19aa8665360c100a1ce49dd6a56ccee406a4dd12ea676
-size 19556
+oid sha256:f80cee1a76ac7670676e6cb188457d083af2e5ec457947ed5dac96b1abce97c9
+size 19531
