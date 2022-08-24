3453c655cae2dfb136ec2e141b951818c994ebb5
Ayush Chaurasia <ayush.chaurarsia@gmail.com>
2022-08-24 15:45:37 +0530

    Infer Loggers project name  (#9117)
    * smart project name inference
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update __init__.py
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- utils/loggers/__init__.py
+++ utils/loggers/__init__.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e48dad9dff6d05157954e217bd37da135015c2015ab5ceab7470ecca3575dad4
-size 13260
+oid sha256:61f3094df01d4c8e4f647d8e7eece6c3b28a60b90a8b8365ec82d285244e8f0a
+size 13515
