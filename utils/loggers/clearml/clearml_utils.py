4201e1e493c67da9dee2e392b6d4fa212c1b6749
HighMans <42877729+HighMans@users.noreply.github.com>
2022-08-26 19:39:11 -0400

    Fix confidence threshold for ClearML debug images (#9174)
    * Fix confidence threshold
    
    The confidence is converted to a percentage on line 144, but it is being compared to a default conf_threshold value of a decimal value instead of percent value.
    
    Signed-off-by: HighMans <42877729+HighMans@users.noreply.github.com>
    
    * Revert "Fix confidence threshold"
    
    This reverts commit f84a09967f83d70626ca8dfe7625dce60fb0102e.
    
    * Fix confidence comparison
    
    Fix the confidence percentage is being compared to a decimal value.
    
    Signed-off-by: HighMans <42877729+HighMans@users.noreply.github.com>

--- utils/loggers/clearml/clearml_utils.py
+++ utils/loggers/clearml/clearml_utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4f612d06191c7f978f53c456f741f7c7d9d391980163631ef85ec2c60d87eee5
-size 7453
+oid sha256:17fd04bc2bbe23fe021cafc6c2e4486bc64478076ab31ae3d8d5d63945ec9059
+size 7469
