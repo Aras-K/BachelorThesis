76736efe41a87fc2d6d1f79d9250b80be41333e3
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-08-29 20:41:54 +0200

    Default AutoBatch 0.8 fraction (#9212)

--- hubconf.py
+++ hubconf.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:84862cb803b6159ce1f3e9db40453dee1abd38ec377d2880804c6fd5ba93d646
-size 7234
+oid sha256:bc8c3fb3da165b8b35d8389a596b272727b6f5f9ac9c9dc04bfc1d91d4c772a8
+size 7241
--- utils/autobatch.py
+++ utils/autobatch.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:737b53a9ad131beb6c24db21865bf4e755757569b4a13a9ebaf1a1213dd507c1
+oid sha256:f49a003a4bcf5e04b34ff394a7784af29a07997086b4c8602cd15339aed93859
 size 2783
