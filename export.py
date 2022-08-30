83d0f18a5f70a976c3e9ae3e370dad1a35de44bf
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-08-30 16:18:01 +0200

    Add ClassificationModel TF export assert (#9226)
    * Add ClassificationModel TF export assert
    
    Export to TF not yet supported, warning alerts users.
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- export.py
+++ export.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ee21524d738b6c4b64f3f0a362e8de6cb2f9450b569ab3491b12438e79e56368
-size 28494
+oid sha256:21fc1afee1d3e4fa9430c64e2c9dd05e7f08a2d38a6ea813757aacd6bd165001
+size 28636
