f2c5821977d90a8bfe8e8eef6db7cf92bf5b8fef
Ayush Chaurasia <ayush.chaurarsia@gmail.com>
2022-09-01 22:47:36 +0530

    Refactor Loggers : Move code outside train.py (#9241)
    * update
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * update
    
    * update
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f1086190b7636851d74ebd58d0915d039c396fee0931656318e5c0143e27fca3
-size 33410
+oid sha256:5a714a9debf37fb62177e0ed954c9a2b3ebfc642799e8f13397ff0ed9e2753dd
+size 33328
--- utils/loggers/__init__.py
+++ utils/loggers/__init__.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f3fffe4d07d7f5dec1be00d6ff85ab31d9a1e0a3ac2f9fdd14108fd0c619048c
-size 14125
+oid sha256:142fcb21085ac443ac78a04cb0fff38100b47bed49301cae6bbd9f9e0c3fe9a9
+size 14429
