de38e10554ac6141b30e9da12349aa2e341a2d5b
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-09-01 19:36:27 +0200

    Update general.py (#9252)
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- utils/general.py
+++ utils/general.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3e2463191413e776749aa777780d6ad167778999ccbd056827ebde30e466b58d
-size 43542
+oid sha256:9bbcba17e8b0105e106688c1d720debfd27fcff837c017bcb285a1c3eb366a07
+size 43550
