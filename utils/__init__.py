9029b9c52b7155923eddddfd423da6856c558232
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-09-02 16:24:30 +0200

    Update `TryExcept(msg='...')`` (#9261)

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9663625c8a062e33d89a7fb658de308d740debf3459fd4dacfc4cce8ab34ac64
-size 1874
+oid sha256:87954665d9af7e38f6c7314b77c7fb069d57eaa91b86b647a7b9d2130e3c1274
+size 1852
--- utils/metrics.py
+++ utils/metrics.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eb47462e9d72ff2038b8d1768ef959a91c35480d02748184c4202d27eb74c32b
-size 14659
+oid sha256:01aab4c9bc2d9ede295f452097c4aa63de05d0d1da5f7d400606e9b9d7ba2c7e
+size 14661
