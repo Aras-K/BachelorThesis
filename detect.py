964d145ac3cda7d837bf026ca82cb4119c50ab28
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-09-02 14:12:10 +0200

    Move `cudnn.benchmarks(True)` to LoadStreams (#9258)
    * Move cudnn.benchmarks(True) to LoadStreams
    
    * Update dataloaders.py
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    * Move cudnn.benchmarks(True) to LoadStreams
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- detect.py
+++ detect.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:903276a0eed4739383cef82459fed0f93581c807693b204a0320ed4007ddcbcc
-size 13637
+oid sha256:842d163a75d76b9f1e33ad007e327ee85e22969d68e3542a1537cc76edf784aa
+size 13515
--- utils/dataloaders.py
+++ utils/dataloaders.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ab653f9df97c2b5ce78680eeab1c2aa9875e0da78d39ef6ac6f01222aea53a4
-size 52428
+oid sha256:378aa49a85e6e33acf1520703fb579209270f95b8ca6a560d5a6b2e39b5b9802
+size 51137
