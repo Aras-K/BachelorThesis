44957e9890d38f5b17b4b64ac18454133b987588
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-09-02 16:05:23 +0200

    `cudnn.benchmark = True` on Seed 0 (#9259)
    * `cudnn.benchmark = True` on Seed 0
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    * Update general.py
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    * Update general.py
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- utils/general.py
+++ utils/general.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9bbcba17e8b0105e106688c1d720debfd27fcff837c017bcb285a1c3eb366a07
-size 43550
+oid sha256:6b700530cc4c9f2062e15f23132ad55c3150ae27a80d403034e0011e2023a775
+size 43437
