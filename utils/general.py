b88b0a802501a43225d327624bc46bb9fd81ddbe
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-09-10 12:14:31 +0300

    Update `check_requirements()` multiple string (#9356)
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- utils/general.py
+++ utils/general.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0ad0a46f5e1832c380f86ed677a856cf52fcd4c2f5adcdf1f8cb9f2ee85bd504
-size 43365
+oid sha256:888ff3f70c4733962221a13f5156ccf4b821236337ea342ba1e0cefdbbf54158
+size 43379
