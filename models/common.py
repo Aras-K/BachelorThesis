2101db94a96a68b624efd246f80aa553838a597d
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-09-10 23:55:18 +0300

    `torch.jit.trace()` fix (#9363)
    * Update common.py
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    * Update ci-testing.yml
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- models/common.py
+++ models/common.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:713514e6e5655849af976c6c9f86fde552ef8d8a4f1372a5914fd26de0f54eed
-size 38946
+oid sha256:54b463585ebd26f755f6ed57d7a1d028592487a58df606e8efc7a5de514caf92
+size 39003
