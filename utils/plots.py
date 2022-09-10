26093039d422d41be3c59b96e6ec36287c3b2d1d
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-09-10 22:24:46 +0300

    labels.jpg names fix (#9361)
    Partially resolves https://github.com/ultralytics/yolov5/issues/9360
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- utils/plots.py
+++ utils/plots.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6704e2e8652e4b23cbd6e18dbb5555028ff21610c69d22ffc52e67fcd1dffec5
-size 22457
+oid sha256:1cc5b8dc16df3128c42f2f6ad1a5897ad9bfc64865830f3d4420364333993b24
+size 22472
