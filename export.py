dd3bbd71aaf74f30fef210717f440fb22fc771c1
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-09-15 19:05:10 +0200

    Remove `.train()` mode exports (#9429)
    * Remove `.train()` mode exports
    
    No common use cases.
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- export.py
+++ export.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cbc8ce28a07c3f1d17799e76f415a259bcd73c233892c1d9020a01cf2925db48
-size 29616
+oid sha256:40e26d4a1c7d1c39ffacc5240d460b15b5d8459fff9a22b8b578d701b9c12c4c
+size 29294
