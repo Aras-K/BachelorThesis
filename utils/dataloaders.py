49896608827bfb8c2f259f1575cd5dc28146016c
mucunwuxian <mucun.wuxian@gmail.com>
2022-09-19 21:01:46 +0900

    Accelerate video inference (#9487)
    * The following code is slow, "self.cap.set(cv2.CAP_PROP_POS_FRAMES, self.vid_stride * (self.frame + 1)) # read at vid_stride".
    
    * adjust...
    
    * Update dataloaders.py
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- utils/dataloaders.py
+++ utils/dataloaders.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f3e36709f5eca132f53b697ac875cd88d31cf03a0a19b0650ae6462a4d45531c
-size 51503
+oid sha256:4a57a28d3da44a29bd20b5f25cf1fc79a3ac78f3b7d0f7bab26f3da5b243ad35
+size 51476
