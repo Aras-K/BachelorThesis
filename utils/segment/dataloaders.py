e2437cf55467e6c15fda089c60fcb2e53d23c7d9
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-09-20 12:22:02 +0200

    Segmentation `polygons2masks_overlap()` in `np.int32` (#9493)
    * Segmentation `polygons2masks_overlap()` in `np.int32`
    
    May resolve https://github.com/ultralytics/yolov5/issues/9461
    
    WARNING: Masks should be uint8 for fastest speed, change needs profiling results to determine impact.
    
    @AyushExel @Laughing-q 
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    * Update dataloaders.py
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- utils/segment/dataloaders.py
+++ utils/segment/dataloaders.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:419b1f215f8122ece8e2f443d4b18023abcdf1b65b0e05733e8c906a975c78d6
-size 13457
+oid sha256:ffd433193f880eac7d6ba18f06e33bf6ca496b135c6ae28c8eb58d75ef09cf6e
+size 13515
