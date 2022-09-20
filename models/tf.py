3a43fe4cb8d43cbdfb004f7a231b96a5d3d5fdeb
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-09-20 19:01:03 +0200

    TFLite fix 2 (#9518)
    * TFLite fix 2
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- models/tf.py
+++ models/tf.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3fd3d0cbd5a55f416aeeff98eb33c1a0686b7b3159b623fb67dfeb12000bc30a
-size 26880
+oid sha256:8d0ce58a5ce04f8c4670d95d63f0205e1b3718bfa844eebe28c57f1632cc6c5b
+size 26878
