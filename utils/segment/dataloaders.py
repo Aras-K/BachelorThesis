991da626eaedfc3556d5eec90058316ebafc12da
FeiGeChuanShu <774074168@qq.com>
2022-09-20 19:04:45 +0800

    Fix `random_perspective` param bug in segment (#9512)
    * fix random_perspective param bug when mosaic=False
    
    Signed-off-by: FeiGeChuanShu <774074168@qq.com>
    
    * Update dataloaders.py
    
    * Update dataloaders.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Signed-off-by: FeiGeChuanShu <774074168@qq.com>
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- utils/segment/dataloaders.py
+++ utils/segment/dataloaders.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ffd433193f880eac7d6ba18f06e33bf6ca496b135c6ae28c8eb58d75ef09cf6e
-size 13515
+oid sha256:b450338bdbdaad4fa323f6a274a8bef8dd87b49fc41395af15451702c20fd8b3
+size 13712
