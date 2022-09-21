127e323587cad3c9313b2fa36222bab34c8547e2
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-09-22 00:34:35 +0200

    TF.js fix (#9536)
    * TF.js fix
    
    May resolve https://github.com/ultralytics/yolov5/issues/9534
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update ci-testing.yml
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    * Update ci-testing.yml
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    * Update ci-testing.yml
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- models/tf.py
+++ models/tf.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8d0ce58a5ce04f8c4670d95d63f0205e1b3718bfa844eebe28c57f1632cc6c5b
-size 26878
+oid sha256:b1ed4177b01ab7939767894c9bba5012cea5a7686696addb0a9db31181b6bd25
+size 26875
