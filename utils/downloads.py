86f8995d9e60beb68c1101b216e6c0e39e22b629
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-09-23 23:20:19 +0200

    Update `is_url()` (#9566)
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- utils/downloads.py
+++ utils/downloads.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d2eef4a3220f3a3a2b7287cef4e17f54898810222aa7cd24da9a4a4c33c0fedb
-size 7640
+oid sha256:0a74688e4763f01f277b07a7f03950f3ce4aefa2a02496bca3336b434b305dcf
+size 7659
