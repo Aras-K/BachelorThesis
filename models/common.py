b52f0ba66fbfd4555194b1cd542381e702069f60
Forever518 <1423429527@qq.com>
2022-09-25 01:35:07 +0800

    generator seed fix for DDP mAP drop (#9545)
    * Try to fix DDP mAP drop by setting generator's seed to RANK
    
    * Fix default activation bug
    
    * Update dataloaders.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update dataloaders.py
    
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- models/common.py
+++ models/common.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba29550a777868f1f32a6334348bd276a0e45c24c4cfe8b360ea649ca7c7e091
-size 40916
+oid sha256:12804474a0cea8edaa23f1da1a123c8f012e52b962c2a35a3350b55157004583
+size 40932
--- utils/dataloaders.py
+++ utils/dataloaders.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:84a1b84e361b28cb2402814124e5f6c1670333ac5e6612da735dc0c9edb50c11
-size 53642
+oid sha256:67f7eb780641f88d8d7a4d04d9aeb9a19227b64cf020683834192d829fa8c23e
+size 53726
