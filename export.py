98b11815bef0ddf23d664f756325709733e33e76
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-09-26 20:10:24 +0200

    Updated Segmentation and Classification usage (#9607)
    * Updated Segmentation and Classification usage
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    * Update export.py
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    * Update export.py
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    * Update export.py
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- export.py
+++ export.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:94139c1301ba52c1db1b0818dbb8afb31335a4bf74d9a559ce3d330cabbd9e74
-size 29564
+oid sha256:4a40bd46710c05e276c534455126d634b384ed8a782965776a83bedf31b89524
+size 30097
