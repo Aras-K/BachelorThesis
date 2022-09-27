4283065f9390ddd1c1402865fb032a98408888ff
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-09-27 18:02:48 +0200

    NMS MPS device wrapper (#9620)
    * NMS MPS device wrapper
    
    May resolve https://github.com/ultralytics/yolov5/issues/9613
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    * Update general.py
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- utils/general.py
+++ utils/general.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:01493ac00f8d4660840e49e2d913c50d07ad211fd424029c421ebdd66f983867
-size 44737
+oid sha256:24bc2e0a9cca6ce4ea6266b94bce9290b04bf27b0a5fd4400328957b3559dab5
+size 44847
