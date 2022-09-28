600119bbdeed3e7b2f069129329a26f1fcc5ce4e
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-09-28 16:43:11 +0200

    Add SegmentationModel unsupported warning (#9632)
    * Add SegmentationModel unsupported warning
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    * Update hubconf.py
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    * Update hubconf.py
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- hubconf.py
+++ hubconf.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1a15a514669cb3fb52613ead5305ca6ffa6419f39a462ea93d4c02e0da1d399e
-size 7251
+oid sha256:4dcfd719b6d1fa0df4c73a80a10859ae748c9ff052d29d36aa7d8a06d04fc54c
+size 7559
