c1d183b14700c4e3a243cb0dca83ed62c9e7c2b5
Glenn Jocher <glenn.jocher@ultralytics.com>
2022-10-04 16:32:19 +0200

    TensorRT `--dynamic` fix (#9691)
    * Update export.py
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    * Update export.py
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- export.py
+++ export.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:529b6ea4d9b8cb883b563290aa0d4f65b45c63a56112be3f62d25d111d8103c4
-size 30042
+oid sha256:cf4dee2ec04ee334b32cf62438b972306afce0c34e314ad08497942a757056ac
+size 30029
