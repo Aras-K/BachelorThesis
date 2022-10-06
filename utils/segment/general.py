4d2abb6c4606206534d80882248d3fd8b47a7ace
Paul Guerrie <97041392+paulguerrie@users.noreply.github.com>
2022-10-06 14:55:15 -0600

    Error in utils/segment/general `masks2segments()` (#9724)
    When running segmentation predict on gpu, the conversion from tensor to numpy fails. Calling `.cpu()` solves this problem. 
    
    Signed-off-by: Paul Guerrie <97041392+paulguerrie@users.noreply.github.com>
    
    Signed-off-by: Paul Guerrie <97041392+paulguerrie@users.noreply.github.com>

--- utils/segment/general.py
+++ utils/segment/general.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:522e91bc656aafcf7ee5dbdbe9deeca79ec34c409ebb2cc8ce4c1cb741faea11
-size 4830
+oid sha256:d51d5cba0cfba894af5250a7a139d78b1a2daa7d3d4f55aeab13565ec420886e
+size 4836
